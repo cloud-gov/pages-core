3c123472befc60a921c4070346ce698ea85649fa
Jeremia Kimelman <jbkimelman@gmail.com>
2015-06-09 14:32:15 -0400

    wire it all together

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,13 +1,17 @@
 console.log('yo from up top');
 
 var UserModel = require('./models/User');
-var user = new UserModel();
-
+var UserView = require('./views/UserView');
 var SiteListView = require('./views/SiteListView');
 var SiteCollection = require('./models/Site').collection;
 
+var user = new UserModel();
+var loginView = new UserView({model: user});
+
 var sites = new SiteCollection();
-var listView = new SiteListView({collection: sites})
+var listView = new SiteListView({collection: sites, user: user})
+
+listView.render();
 
 sites.on('add', function(e) {
   listView.render();
@@ -18,3 +22,4 @@ sites.on('destroy', function(e) {
 
 window.u = user;
 window.l = listView;
+window.v = loginView;
