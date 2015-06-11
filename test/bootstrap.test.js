9b594fb8c78784bea794ce2f8be6f51188bf89ad
Dave Cole <david.cole@gsa.gov>
2015-06-11 19:19:02 -0400

    Set log level for tests

--- test/bootstrap.test.js
+++ test/bootstrap.test.js
@@ -5,7 +5,8 @@ before(function(done) {
   Sails.lift({
     // configuration for testing purposes
     // Use memory for data store
-    models: { connection: 'memory' }
+    models: { connection: 'memory' },
+    log: { level: 'info' }
   }, function(err, server) {
     sails = server;
     if (err) return done(err);
