91149a83b695964444f8cd1fdf2a9eac01fd616c
Dave Cole <david.cole@gsa.gov>
2015-06-11 19:18:33 -0400

    Add User model test

--- test/unit/models/User.test.js
+++ test/unit/models/User.test.js
@@ -1,6 +1,15 @@
-var sinon = require('sinon'),
-    assert = require('assert');
+var assert = require('assert');
 
 describe('User Model', function() {
+
+  describe('record.toJSON', function() {
+    it('should not include passports', function(done) {
+      User.create({}, function(err, user) {
+        if (err) throw err;
+        assert.equal(user.toJSON().passports, undefined);
+        done();
+      });
+    });
+  });
 
 });
