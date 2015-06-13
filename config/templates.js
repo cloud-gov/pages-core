27d6afcc680ff91cd011224de6cf42bfa2ce6e01
Jeremia Kimelman <jbkimelman@gmail.com>
2015-06-13 02:23:46 -0400

    set up templates

---
+++ config/templates.js
@@ -0,0 +1,12 @@
+module.exports = {
+  'microsite': {
+    repo: 'https://github.com/USG-Website-Templates/microsite-template',
+    example: 'https://usg-website-templates.github.io/microsite-template/',
+    description: 'A quick little site template'
+  },
+  'developer': {
+    repo: 'https://github.com/USG-Website-Templates/developer-hub',
+    example: 'http://usg-website-templates.github.io/developer-hub/',
+    description: 'A template for building developer hubs and API docs'
+  }
+}
