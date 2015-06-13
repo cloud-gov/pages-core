07e560ae30c4bfc1b7d23b059c6cd07d51127ee8
Jeremia Kimelman <jbkimelman@gmail.com>
2015-06-13 01:29:31 -0400

    abstract relationships for pub/ob model

--- assets/app/app.js
+++ assets/app/app.js
@@ -1,26 +1,49 @@
 var Backbone = require('backbone');
+var _ = require('underscore');
 var $ = window.jQuery = window.$ = Backbone.$;
 
+var AddSiteView = require('./views/AddSiteView');
 var UserModel = require('./models/User');
 var UserView = require('./views/UserView');
 var SiteListView = require('./views/SiteListView');
 var SiteCollection = require('./models/Site').collection;
 
+var dispatcher = _.clone(Backbone.Events);
+
 var user = new UserModel();
 var loginView = new UserView({model: user});
-
+var newSiteView = new AddSiteView({user: user});
 var sites = new SiteCollection();
-var listView = new SiteListView({collection: sites, user: user});
+var listView = new SiteListView({collection: sites});
 
-listView.render();
+listView.render({authenticated: user.isAuthenticated()});
 
-sites.on('add', function(e) {
-  listView.render();
-});
-sites.on('destroy', function(e) {
-  listView.render();
+dispatcher.listenTo(user, 'change', function() {
+  renderListView();
+  renderNewView();
 });
 
+dispatcher.listenTo(sites, 'change', function () {
+  renderListView();
+});
+
+dispatcher.listenTo(newSiteView, 'success', function () {
+  listView.collection.fetch();
+})
+
+dispatcher.listenTo(listView, 'newsite', function () {
+  newSiteView.toggleDisplay();
+});
+
+function renderListView() {
+  listView.render({authenticated: user.isAuthenticated()});
+}
+
+function renderNewView() {
+  newSiteView.render({user: user});
+}
+
+window.d = dispatcher;
 window.u = user;
 window.l = listView;
-window.v = loginView;
+window.v = newSiteView;
