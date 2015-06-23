2e6a2c31701ad59dedec46de43085efac399a302
Dave Cole <david.cole@gsa.gov>
2015-06-23 02:41:13 -0400

    More specific rollback script

--- migrations/20150623034530-initial.js
+++ migrations/20150623034530-initial.js
@@ -1,11 +1,11 @@
 var dbm = global.dbm || require('db-migrate');
 var type = dbm.dataType;
 var fs = require('fs');
+var async = require('sails/node_modules/async');
 
 exports.up = function(db, callback) {
   fs.readFile(__dirname + '/initial.sql', { encoding: 'utf-8' }, function(err, data) {
     if (err) throw err;
-    console.log(data);
     db.runSql(data, function() {
       if (err) throw err;
       callback();
@@ -14,5 +14,11 @@ exports.up = function(db, callback) {
 };
 
 exports.down = function(db, callback) {
-  db.runSql('drop schema public cascade; create schema public;', callback);
+  async.series([
+    db.dropTable.bind(db, 'build'),
+    db.dropTable.bind(db, 'passport'),
+    db.dropTable.bind(db, 'site'),
+    db.dropTable.bind(db, 'site_users__user_sites'),
+    db.dropTable.bind(db, 'user')
+  ], callback);
 };
