8076aa62a59c61ed3976f0b1e721ebcf96ed8ee8
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2015-07-08 22:34:26 -0400

    Main view to handle subviews

--- assets/app/app.js
+++ assets/app/app.js
@@ -3,11 +3,7 @@ var _ = require('underscore');
 var $ = window.jQuery = window.$ = Backbone.$;
 
 var MainContainerView = require('./views/MainContainerView');
-var AuthenticateView = require('./views/AuthenticateView');
-var SiteListView = require('./views/SiteListView');
-var AddSiteView = require('./views/AddSiteView');
-var UserView = require('./views/UserView');
-var EditView = require('./views/EditView');
+var NavbarView = require('./views/NavbarView');
 
 var UserModel = require('./models/User');
 var SiteCollection = require('./models/Site').collection;
@@ -19,37 +15,28 @@ var Router = Backbone.Router.extend({
     this.sites = new SiteCollection();
     this.user = window.u = new UserModel();
 
-    this.navbarView = new UserView({ model: this.user });
+    this.navbarView = new NavbarView({ model: this.user });
     this.app = new MainContainerView({ user: this.user, collection: this.sites });
 
-    this.listenTo(this.user, 'change', function () {
-      var authed = this.user.isAuthenticated();
-      if(authed) {
-        var listView = new SiteListView({ collection: this.sites });
-        this.app.pageSwitcher.set(listView);
-      }
-    });
   },
   routes: {
     '': 'home',
     'new': 'new',
-    'edit/*path': 'edit'
+    'edit(/)*path': 'edit'
   },
   home: function () {
-    var authView = new AuthenticateView();
-    this.app.pageSwitcher.set(authView);
+    this.app.home();
+    return this;
   },
   new: function () {
-    var addSiteView = new AddSiteView({ user: user });
-    this.app.pageSwitcher.set(addSiteView);
+    this.app.new();
+    return this;
   },
   edit: function (path) {
-    console.log('editoring for', path);
-    var editView = new EditView({ path: path });
-    this.app.pageSwitcher.set(editView);
+    this.app.edit(path);
+    return this;
   }
 });
 
 var router = window.r = new Router();
-
 Backbone.history.start();
--- assets/app/views/MainContainerView.js
+++ assets/app/views/MainContainerView.js
@@ -1,14 +1,56 @@
 var Backbone = require('backbone');
 var ViewSwitcher = require('ampersand-view-switcher');
 
+var AuthenticateView = require('./AuthenticateView');
+var SiteListView = require('./SiteListView');
+var AddSiteView = require('./AddSiteView');
+var EditView = require('./EditView');
+
 var AppView = Backbone.View.extend({
   el: 'main',
   initialize: function (opts) {
+    this.user = opts.user;
+    this.sites = opts.collection;
+
+    this.listenTo(this.user, 'change', function() {
+      this.home();
+    }.bind(this));
+
     this.render();
   },
   render: function () {
-    this.pageContainer = this.el;
-    this.pageSwitcher = new ViewSwitcher(this.pageContainer);
+    this.pageSwitcher = this.pageSwitcher || new ViewSwitcher(this.el);
+  },
+  home: function () {
+    var authed = this.user.isAuthenticated();
+    if(authed) {
+      var listView = new SiteListView({ collection: this.sites });
+      this.pageSwitcher.set(listView);
+
+      return this;
+    }
+
+    var authenticateView = new AuthenticateView();
+    this.pageSwitcher.set(authenticateView);
+    return this;
+  },
+  new: function () {
+    var addSiteView = new AddSiteView({ user: this.user });
+    this.pageSwitcher.set(addSiteView);
+
+    this.listenToOnce(addSiteView, 'site:save:success', function () {
+      this.home();
+    }.bind(this));
+
+    return this;
+  },
+  edit: function (path) {
+    console.log('editoring for', path);
+
+    var editView = new EditView({ path: path });
+    this.pageSwitcher.set(editView);
+
+    return this;
   }
 });
 
