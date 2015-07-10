4e7e7bed9fbc93cb04fea3262da1ffa1c69a76f2
Dave Cole <david.cole@gsa.gov>
2015-07-10 18:03:49 -0400

    Build default and preview sites separately based on branch.

--- api/services/buildEngines.js
+++ api/services/buildEngines.js
@@ -16,7 +16,7 @@ module.exports = {
       'mkdir -p ${source}',
       'git clone -b ${branch} --single-branch ' +
         'https://${token}@github.com/${owner}/${repository}.git ${source}',
-      'echo "baseurl: /${root}/${owner}/${repository}/${branch}" > ' +
+      'echo "baseurl: /${root}/${owner}/${repository}${branchURL}" > ' +
         '${source}/_config_base.yml',
       'jekyll build --safe --config ${source}/_config.yml,${source}/_config_base.yml ' +
         '--source ${source} --destination ${source}/_site',
@@ -36,7 +36,7 @@ module.exports = {
       'mkdir -p ${source}',
       'git clone -b ${branch} --single-branch ' +
         'https://${token}@github.com/${owner}/${repository}.git ${source}',
-      'hugo --baseUrl=/${root}/${owner}/${repository}/${branch} ' +
+      'hugo --baseUrl=/${root}/${owner}/${repository}${branchURL} ' +
         '--source=${source}',
       'rm -rf ${destination}',
       'mkdir -p ${destination}',
@@ -77,10 +77,12 @@ module.exports = {
    * @param {Function} callback function
    */
   _run: function(cmd, model, done) {
-
-    var tokens = {
+    var service = this,
+        defaultBranch = model.branch === model.site.defaultBranch,
+        tokens = {
           branch: model.branch,
-          root: sails.config.build.destinationRoot.replace('assets/', '')
+          branchURL: defaultBranch ? '' : '/' + model.branch,
+          root: defaultBranch ? 'site' : 'preview'
         },
         template = _.template(cmd.join(' && '));
 
@@ -105,19 +107,64 @@ module.exports = {
       tokens.token = model.user.passport.tokens.accessToken;
 
       // Set up source and destination paths
-      tokens.source = sails.config.build.sourceRoot + '/' +
+      tokens.source = sails.config.build.tempDir + '/source/' +
         tokens.owner + '/' + tokens.repository + '/' + tokens.branch;
-      tokens.destination = sails.config.build.destinationRoot + '/' +
+      tokens.destination = sails.config.build.tempDir + '/destination/' +
         tokens.owner + '/' + tokens.repository + '/' + tokens.branch;
+      tokens.publish = sails.config.build.publishDir + '/' + tokens.root + '/' +
+        tokens.owner + '/' + tokens.repository + tokens.branchURL;
 
       // Run command in child process and
       // call callback with error and model
       exec(template(tokens), function(err, stdout, stderr) {
         if (stdout) sails.log.verbose('stdout: ' + stdout);
         if (stderr) sails.log.verbose('stderr: ' + stderr);
-        done(err, tokens);
+        if (err) return done(err, model);
+        service.publish(tokens, model, done);
       });
 
+    }
+
+  },
+
+  /*
+   * Publish a built site by copying it to its publish directory
+   * or syncing it to an S3 bucket.
+   *
+   * @param {Object} tokens from the _run command
+   * @param {Build} build model to parse
+   * @param {Function} callback function
+   */
+  publish: function(tokens, model, done) {
+
+    // If an S3 bucket is defined, sync the site to it
+    if (sails.config.build.s3Bucket) {
+      var syncConfig = {
+            prefix: tokens.root + '/' +
+              tokens.owner + '/' +
+              tokens.repository +
+              tokens.branchURL,
+            directory: tokens.destination
+          };
+      sails.log.verbose('Publishing job: ', model.id,
+        ' => ', sails.config.build.s3Bucket);
+      S3(syncConfig, function(err) {
+        done(err, model);
+      });
+
+    // Or else copy the site to a local directory
+    } else {
+      var cmd = _.template(['rm -r ${publish} || true',
+            'mkdir -p ${publish}',
+            'cp -r ${destination}/ ${publish}',
+          ].join(' && '));
+      sails.log.verbose('Publishing job: ', model.id,
+        ' => ', tokens.publish);
+      exec(cmd(tokens), function(err, stdout, stderr) {
+        if (stdout) sails.log.verbose('stdout: ' + stdout);
+        if (stderr) sails.log.verbose('stderr: ' + stderr);
+        done(err, model);
+      });
     }
 
   }
--- config/build.js
+++ config/build.js
@@ -2,7 +2,7 @@
  * Settings the build process
  */
 module.exports.build = {
-  sourceRoot: '/tmp/federalist',
-  destinationRoot: 'assets/site',
-  s3Bucket: process.env.S3_BUCKET
+  tempDir: process.env.FEDERALIST_TEMP_DIR || './.tmp',
+  publishDir: process.env.FEDERALIST_PUBLISH_DIR || './assets',
+  s3Bucket: process.env.FEDERALIST_S3_BUCKET
 };
