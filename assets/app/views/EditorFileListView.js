8140ed911fce18d617a4e9f075931e83de87c907
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2015-07-29 14:30:09 -0400

    New view for listing files in directory

---
+++ assets/app/views/EditorFileListView.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+
+var Backbone = require('backbone');
+var _ = require('underscore');
+
+var templateHtml = fs.readFileSync(__dirname + '/../templates/AuthenticateTemplate.html').toString();
+
+var EditorFileListView = Backbone.View.extend({
+  tagName: 'ul',
+  className: 'collection',
+  template: _.template('<li class="collection-item avatar"><a href="<%- fileUrl %>"><i class="material-icons circle"><%- type %></i><p class="title"><%- fileName %></p></a></li>'),
+  initialize: function initializeEditorFileListView(opts) {
+    this.path = opts.path;
+    this.files = opts.files || [];
+  },
+  render: function renderUserView() {
+    var self        = this,
+        owner       = this.path.owner,
+        repo        = this.path.repo,
+        branch      = this.path.branch;
+
+    if (this.files.length === 0) {
+      self.$el.append('<strong>No files in this folder</strong>');
+      return this;
+    }
+
+    this.files.forEach(function(file) {
+      var newHash = ['#edit', owner, repo, branch, file.path].join('/'),
+          type    = (file.type == 'dir') ? 'folder' : 'insert_drive_file',
+          li      = self.template({ fileUrl: newHash, fileName: file.name, type: type });
+
+      self.$el.append(li);
+    });
+
+    return this;
+  }
+});
+
+module.exports = EditorFileListView;
