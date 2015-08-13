a0cb9333d666323dfaa462faa1d035d36871a945
Dave Cole <david.cole@gsa.gov>
2015-08-13 18:52:39 -0400

    Use env var for app name

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -4,12 +4,11 @@ set -e
 
 wget http://go-cli.s3-website-us-east-1.amazonaws.com/releases/v6.12.1/cf-cli_amd64.deb -qO temp.deb && sudo dpkg -i temp.deb
 
-
 rm temp.deb
 
 cf api $CF_API
 cf login --u $CF_USERNAME --p $CF_PASSWORD --o $CF_ORGANIZATION --s $CF_SPACE
 
-./scripts/deploy.sh federalist
+./scripts/deploy.sh $CF_APP
 
 cf logout
