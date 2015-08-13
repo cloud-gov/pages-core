36d4a95d64e54af2c14fb3df2b6e796a022256cf
Dave Cole <david.cole@gsa.gov>
2015-08-13 17:41:43 -0400

    Update deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,15 +2,17 @@
 
 # Blue-green deployment script. Usage:
 #
-#   ./script/deploy <appname>
-#
-# Based on
-#   http://www.cloudfoundry.rocks/blue-green-deployment-with-cloudfoundry/
-#   http://docs.pivotal.io/pivotalcf/devguide/deploy-apps/blue-green.html
-#   https://github.com/dlapiduz/step-cloud-foundry-deploy/blob/master/run.sh
+#   ./cf-blue-green <appname>
 
 set -e
 set -x
+
+
+if [ $# -ne 1 ]; then
+	echo "Usage:\n\n\t./cf-blue-green <appname>\n"
+	exit 1
+fi
+
 
 BLUE=$1
 GREEN="${BLUE}-B"
@@ -28,8 +30,15 @@ on_fail () {
 }
 
 
+CF_VERSION=$(cf --version)
+# http://stackoverflow.com/a/229606/358804
+if [[ "$CF_VERSION" != *"6.12.0-"* ]] && [[ "$CF_VERSION" != *"6.12.1-"* ]]; then
+  echo "CF CLI version is not compatible. See\n\n\thttps://github.com/18F/cf-blue-green#cf-cli\n"
+  exit 1
+fi
+
 # pull the up-to-date manifest from the BLUE (existing) application
-MANIFEST=$(mktemp -t "${BLUE}_manifest")
+MANIFEST=$(mktemp -t "${BLUE}_manifest.XXXXXXXXXX")
 cf create-app-manifest $BLUE -p $MANIFEST
 
 # set up try/catch
