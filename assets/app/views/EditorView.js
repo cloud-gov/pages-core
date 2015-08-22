e561dd7503756e35f83216e8222d0be5237118f5
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2015-08-21 23:28:08 -0400

    Show and accept changes to meta data

--- assets/app/views/EditorView.js
+++ assets/app/views/EditorView.js
@@ -21,10 +21,13 @@ var templateHtml = fs.readFileSync(__dirname + '/../templates/EditorTemplate.htm
 var EditorView = Backbone.View.extend({
   tagName: 'div',
   events: {
-    'click #save-content-action': 'saveDocument'
+    'click [data-show-area]': 'toggleAreas',
+    'click #save-content-action': 'saveDocument',
+    'click .front-matter-delete': 'deleteMetaDataRow',
+    'click #add-front-matter-row': 'addMetaDataRow'
   },
   initialize: function (opts) {
-    this.doc = new Document({ markdown: opts.content });
+    this.doc = window.d = new Document({ markdown: opts.content });
     this.fileName = opts.file;
     return this;
   },
@@ -33,7 +36,7 @@ var EditorView = Backbone.View.extend({
         blocks  = [],
         editor, mdTree;
 
-    this.$el.html(_.template(templateHtml)({ fileName: this.fileName }));
+    this.$el.html(_.template(templateHtml)({ fileName: this.fileName, frontMatter: this.doc.frontMatter }));
     this.editor = new SirTrevor.Editor({
       el: this.$('.js-st-instance'),
       blockTypes: ["H1", "H2", "H3", "Text", "Unordered", "Ordered"]
@@ -41,16 +44,45 @@ var EditorView = Backbone.View.extend({
 
     this.$('.js-st-instance').text(doc.toSirTrevorJsonString());
     this.editor.reinitialize();
-
+    $('form#metadata').hide();
     return this;
   },
+  toggleAreas: function () {
+    $('form#metadata').toggle();
+    $('form#content').toggle();
+  },
   saveDocument: function (e) {
+    var formFrontMatter = {};
     SirTrevor.onBeforeSubmit();
+    $('form#metadata .row').each(function(index, row) {
+      var key   = $(row).find('.front-matter-key').val(),
+          value = $(row).find('.front-matter-value').val();
+
+      if (key) {
+        formFrontMatter[key] = value;
+      }
+    });
+
+    this.doc.updateFrontMatter(formFrontMatter);
     this.doc.updateContentFromSirTrevorJson(this.editor.store.retrieve());
+
     this.trigger('edit:save', {
         md: this.doc.toMarkdown(),
         msg: this.$('#save-content-message').val()
     });
+    return this;
+  },
+  deleteMetaDataRow: function (e) {
+    e.preventDefault();
+    $(e.target).parents('.meta-data-rows .row').remove();
+
+    return this;
+  },
+  addMetaDataRow: function (e) {
+    e.preventDefault();
+    var rowTemplate = _.template('<div class="row"><div class="col s5"><input type="text" class="front-matter-key" placeholder="key"></div><div class="col s5"><input type="text" class="front-matter-value" placeholder="value"></div><div class="col s2"><button class="front-matter-delete">Delete</button></div></div>')();
+    $('.meta-data-rows').append(rowTemplate);
+
     return this;
   }
 });
