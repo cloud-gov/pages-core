ce02e8321bdf6b6c37ecd9d6d8aa711fa161cc5c
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2015-12-03 23:52:51 -0500

    Add image gallery picker and upload controls

---
+++ assets/app/views/editor/add-image.js
@@ -0,0 +1,98 @@
+var fs = require('fs');
+
+var _ = require('underscore');
+var Backbone = require('backbone');
+
+var html = _.template(fs.readFileSync(__dirname + '/../../templates/editor/add-image.html').toString());
+
+var AddImageView = Backbone.View.extend({
+  events: {
+    'change [type=file]': 'uploadFileSelected',
+    'click .form-group.image': 'existingSelected',
+    'click [data-action=image-confirm]': 'useAsset',
+    'click [data-action=image-cancel]': 'remove'
+  },
+  initialize: function () {
+    var github = window.federalist.github,
+        assets = github.assets.filter(function(a) {
+          var isImage = a.name.match(/\.jpg|\.jpeg|\.png|\.gif/);
+          return isImage;
+        });
+
+    this.selectedImage = false;
+    this.el.innerHTML = html({ assets: assets });
+  },
+  uploadNewAsset: function (file) {
+    var sizeLimit = (1024 * 1024) * 5; // 5 megabytes
+
+    if (file.size < sizeLimit) {
+      window.federalist.dispatcher.trigger('asset:upload:selected', file);
+    }
+    else {
+      alert('the file is too big, look at console for more info');
+      console.log('size limit', sizeLimit);
+      console.log('file size', file.size);
+    }
+  },
+  useAsset: function (e) {
+    e.preventDefault(); e.stopPropagation();
+
+    var self = this,
+        selectedImage = $(this.selectedImage).children('img'),
+        selectedFiles = $(this.selectedImage).children('[type=file]');
+
+    if (selectedFiles.length > 0) {
+      var file = $('#asset')[0].files[0];
+      this.uploadNewAsset(file);
+      window.federalist.dispatcher.once('asset:upload:uploaded', function(json){
+        var path = json.content['download_url'];
+        self.trigger('asset:selected', {
+          src: path,
+          title: 'Freshly uploaded'
+        });
+      });
+    }
+    else {
+      this.trigger('asset:selected', {
+        src: selectedImage.attr('src'),
+        title: selectedImage.attr('title')
+      });
+    }
+
+  },
+  triggerFilePicker: function (e) {
+    e.preventDefault(); e.stopPropagation();
+    //this.$('[type=file]').click();
+  },
+  uploadFileSelected: function (e) {
+    e.preventDefault(); e.stopPropagation();
+    var uploadCard = $('#upload-card')[0];
+    this.deselectCurrentCard();
+    this.selectCard(uploadCard);
+  },
+  existingSelected: function (e) {
+    e.preventDefault(); e.stopPropagation();
+    var tagName = e.target.tagName.toLowerCase(),
+        target = e.target; window.z = e.target;
+
+    if (tagName === 'img') target = $(z).parents('.image')[0];
+
+    if (this.selectedImage == target) {
+      this.deselectCurrentCard();
+      return;
+    }
+
+    this.selectCard(target);
+  },
+  deselectCurrentCard: function () {
+    $(this.selectedImage).removeClass('selected');
+    this.selectedImage = false;
+  },
+  selectCard: function (target) {
+    if (this.selectedImage) this.deselectCurrentCard();
+    $(target).addClass('selected');
+    this.selectedImage = target;
+  }
+});
+
+module.exports = AddImageView;
