f0549fdf03a7939c098351d599795f32da0da871
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2015-12-09 16:28:33 -0500

    Use sass to compile css

--- tasks/config/sync.js
+++ tasks/config/sync.js
@@ -17,7 +17,7 @@ module.exports = function(grunt) {
 		dev: {
 			files: [{
 				cwd: './assets',
-				src: ['**/*.!(coffee)'],
+				src: ['**/*.!(coffee|sass|scss)'],
 				dest: '.tmp/public'
 			}]
 		}
