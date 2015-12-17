22005a3a55c003b7f67b1e596e1611e0878c23f2
Dave Cole <david.cole@gsa.gov>
2015-12-17 17:45:47 -0500

    Add templates, comments, linting

--- api/controllers/SiteController.js
+++ api/controllers/SiteController.js
@@ -23,26 +23,41 @@ module.exports = {
 
     if (!req.isSocket || !roomName) return res.badRequest();
 
+    // If the socket disconnects (page refresh, browser close), notify others
     req.socket.on('disconnect', function() {
       broadcast(roomName);
     });
 
+    // Join a socket.io room based on the file name
     sails.sockets.join(req.socket, roomName);
 
-    res.json({ id: req.socket.id });
+    // Send the current state of the room to the user
+    res.json({
+      id: req.socket.id,
+      room: roomName,
+      subscribers: sails.sockets.subscribers(roomName)
+    });
+
+    // Notify all other users that someone else joined
     broadcast(roomName);
   },
 
   unlock: function unlockEditing(req, res) {
-    var roomName = req.param('file'),
-        data = {};
+    var roomName = req.param('file');
     if (!req.isSocket || !roomName) return res.badRequest();
+
+    // Remove the user from the room (used when a user navigates away without
+    // breaking the socket connection)
     sails.sockets.leave(req.socket, roomName);
+
+    // Notify others
     broadcast(roomName);
   }
 
 };
 
+// General function to broadcast the current users
+// of a room to all users in the room
 function broadcast(roomName) {
   sails.io.to(roomName).emit('change', {
     room: roomName,
