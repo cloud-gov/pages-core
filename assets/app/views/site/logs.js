5d3c95ce00a58102a00db71db4f1e946b1500ee9
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-02-22 16:37:22 -0500

    Refactor view code into smaller functions

--- assets/app/views/site/logs.js
+++ assets/app/views/site/logs.js
@@ -12,32 +12,46 @@ var SiteLogsView = Backbone.View.extend({
   template: _.template(templateHtml, { variable: 'site' }),
   render: function() {
     if (!this.model) return this;
-    var data = this.model.toJSON(),
-        view = this;
+    this.fetchUsernames();
+    return this;
+  },
+  fetchUsernames: function () {
+    var view = this;
     $.getJSON('/v0/user/usernames', function(users) {
-      data.builds = _(data.builds).chain().map(function(build) {
-        var completedAt = build.completedAt && new Date(build.completedAt),
-            createdAt = build.createdAt && new Date(build.createdAt),
-            base = completedAt || new Date(),
-            duration = moment.duration(moment(base).diff(createdAt)),
-            item = _.clone(build);
-
-        item.username = users[build.user];
-        item.duration = duration.seconds();
-        item.durationFormatted = duration.humanize();
-        item.panelClass = build.state === 'error' ? 'error' :
-          build.state === 'success' ? '' : 'info';
-
-        if (completedAt) {
-          item.completedAt = moment(completedAt).format('L LT');
-          item.completedAtFormatted = moment(completedAt).fromNow();
-        }
-
-        return item;
-      }).sortBy('createdAt').value().reverse();
+      var data = view.onFetchUsernames(users, view.model);
       view.$el.html(view.template(data));
     });
-    return this;
+  },
+  onFetchUsernames: function (users, model) {
+    model = model || this.model;
+    var data = this.model.toJSON(),
+        view = this;
+
+    data.builds = _(data.builds).chain().map(function(build) {
+      return view.processBuild(build, users);
+    }).sortBy('createdAt').value().reverse();
+
+    return data;
+  },
+  processBuild: function (build, users) {
+    var completedAt = build.completedAt && new Date(build.completedAt),
+        createdAt = build.createdAt && new Date(build.createdAt),
+        base = completedAt || new Date(),
+        duration = moment.duration(moment(base).diff(createdAt)),
+        item = _.clone(build);
+
+    item.username = users[build.user];
+    item.duration = duration.seconds();
+    item.durationFormatted = duration.humanize();
+    item.panelClass = build.state === 'error' ? 'error' :
+      build.state === 'success' ? '' : 'info';
+
+    if (completedAt) {
+      item.completedAt = moment(completedAt).format('L LT');
+      item.completedAtFormatted = moment(completedAt).fromNow();
+    }
+
+    return item;
   }
 });
 module.exports = SiteLogsView;
