66964007298be89a7ecf0aac4b6b3efc7498094e
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-04-06 12:53:36 -0400

    Decode b64 build status message
    To prevent problems with character encoding, the status message from
    the docker builder will be base 64 encoded.

--- api/controllers/BuildController.js
+++ api/controllers/BuildController.js
@@ -5,14 +5,18 @@
  * @help        :: See http://links.sailsjs.org/docs/controllers
  */
 
+function decodeb64 (str) {
+  return new Buffer(str, 'base64').toString('utf8');
+}
+
 module.exports = {
 
   // Endpoint for status updates for builds by external builders
   status: function(req, res) {
-    var message = req.body.message;
+
+    var message = decodeb64(req.body.message);
 
     Build.findOne(req.param('id')).exec(function(err, build) {
-
       if (err) res.send(err);
 
       Build.completeJob(message, build);
