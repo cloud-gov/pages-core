8b49a9e75971f9ac6b29b62748673f7046a4f11f
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-06-20 12:39:59 -0400

    Clean up some git comments in main.js

--- assets/app/main.js
+++ assets/app/main.js
@@ -34,25 +34,5 @@ userActions.fetchUser();
 siteActions.fetchSites();
 // buildActions.fetchBuilds();
 
-<<<<<<< 0d9c0b67a0066c17d69ce32efd779d471c655b8b
 window.router = router;
-=======
-store.subscribe(() => {
-  const state = store.getState();
-  if (state === previousState) return;
-  if (state.user && !authed) {
-    authed = true;
-    updateLoginElement();
-    if (state.currentView.id === viewTypes.HOME) router.setRoute('/dashboard');
-  }
-  previousState = state;
-  render(state, mainEl);
-  return;
-});
-
-const updateLoginElement = (loggedIn = true) => {
-  if (loggedIn) return loginEl.innerHTML = `<a>Logout</a>`;
-};
-
->>>>>>> Adds fetch, wrapper code
 window.store = store;
