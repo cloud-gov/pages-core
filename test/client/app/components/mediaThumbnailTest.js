b5e13f2bbb7b31e1c4e43953cfeac15dba350a9d
Brandon Kirby <brandon@escapepod.com>
2016-08-29 14:47:33 -0400

    Refactor test to extract common code.

--- test/client/app/components/mediaThumbnailTest.js
+++ test/client/app/components/mediaThumbnailTest.js
@@ -6,56 +6,36 @@ import { spy } from "sinon";
 import MediaThumbnail from "../../../../assets/app/components/mediaThumbnail";
 
 describe("mediaThumbnail", () => {
-  it("renders an img if it's a jpg extension", () => {
-    const downloadUrl = "https://nowhere.gov/fake/url/here/hiiiiiiiiiPeople.fake_extension_ignored";
-    const asset = {
-      name: "hellooooWorld.jpg",
-      download_url: downloadUrl
-    };
-    
-    const reactWrapper = shallow(<MediaThumbnail asset={ asset }/>);
+  const downloadUrl = "https://nowhere.gov/fake/url/here/hiiiiiiiiiPeople.fake_extension_ignored";
 
-    expect(reactWrapper.contains(<img src={downloadUrl}/>)).to.be.true;
+  it("renders an img if it's a jpg extension", () => {
+    verifyAssetWithExtensionIsRenderedAsAnImgTag("jpg");
   });
 
   it("renders an img if it's a jpeg extension", () => {
-    const downloadUrl = "https://nowhere.gov/fake/url/here/hiiiiiiiiiPeople.fake_extension_ignored";
-    const asset = {
-      name: "hellooooWorld.jpeg",
-      download_url: downloadUrl
-    };
-    
-    const reactWrapper = shallow(<MediaThumbnail asset={ asset }/>);
-
-    expect(reactWrapper.contains(<img src={downloadUrl}/>)).to.be.true;
+    verifyAssetWithExtensionIsRenderedAsAnImgTag("jpeg");
   });
   
   it("renders an img if it's a gif extension", () => {
-    const downloadUrl = "https://nowhere.gov/fake/url/here/hiiiiiiiiiPeople.fake_extension_ignored";
-    const asset = {
-      name: "hellooooWorld.gif",
-      download_url: downloadUrl
-    };
-    
-    const reactWrapper = shallow(<MediaThumbnail asset={ asset }/>);
-
-    expect(reactWrapper.contains(<img src={downloadUrl}/>)).to.be.true;
+    verifyAssetWithExtensionIsRenderedAsAnImgTag("gif");
   });
-  
+
   it("renders an img if it's a png extension", () => {
-    const downloadUrl = "https://nowhere.gov/fake/url/here/hiiiiiiiiiPeople.fake_extension_ignored";
+    verifyAssetWithExtensionIsRenderedAsAnImgTag("png");
+  });
+
+  const verifyAssetWithExtensionIsRenderedAsAnImgTag = (extension) => {
     const asset = {
-      name: "hellooooWorld.png",
+      name: `hellooooWorld.${extension}`,
       download_url: downloadUrl
     };
     
-    const reactWrapper = shallow(<MediaThumbnail asset={ asset }/>);
-
+    const reactWrapper = shallow(<MediaThumbnail asset={ asset }/>)
+    
     expect(reactWrapper.contains(<img src={downloadUrl}/>)).to.be.true;
-  });
-  
+  };
+
   it("renders the download url if it's not a jpg/jpeg/png/gif extension", () => {
-    const downloadUrl = "https://nowhere.gov/fake/url/here/hiiiiiiiiiPeople.fake_extension_ignored";
     const asset = {
       name: "hellooooWorld.aReallyGoofyExtension",
       download_url: downloadUrl
