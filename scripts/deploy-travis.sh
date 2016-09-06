f2f087b597fbcbfc29e557ec842508d4ee78a42f
Jeremia Kimelman <jeremia.kimelman@gsa.gov>
2016-09-06 19:25:32 -0400

    Use autopilot to deploy
    Using before_deploy values lifted from the openfec project
    and using the autopilot plugin to deploy automatically to
    cloud.gov from travis

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -2,13 +2,10 @@
 
 set -e
 
-wget https://s3.amazonaws.com/go-cli/releases/v6.12.4/cf-cli_amd64.deb -qO temp.deb && sudo dpkg -i temp.deb
-
-rm temp.deb
-
 cf api $CF_API
-cf login --u $CF_USERNAME --p $CF_PASSWORD --o $CF_ORGANIZATION --s $CF_SPACE
+cf auth $CF_USERNAME $CF_PASSWORD && cf target -o $CF_ORGANIZATION -s $CF_SPACE
 
-./scripts/deploy.sh $CF_APP
+# Run autopilot plugin
+cf zero-downtime-push $CF_APP -f manifest.yml -p .
 
 cf logout
