1c8b2ae0405b472a9d89a9c60a7a08b9ae043390
Adam Biagianti <adam.biagianti@gsa.gov>
2016-09-19 16:24:56 -0700

    Fixes issue where incorrect nav items are shown

--- assets/app/components/header.js
+++ assets/app/components/header.js
@@ -4,13 +4,17 @@ import Disclaimer from './disclaimer';
 import Nav from './nav';
 
 const propTypes = {
-  isLoggedIn: React.PropTypes.bool
+  isLoggedIn: React.PropTypes.bool,
+  username: React.PropTypes.oneOfType([
+    React.PropTypes.string,
+    React.PropTypes.object
+  ])
 };
 
-const Header = ({ isLoggedIn = false }) =>
+const Header = ({ isLoggedIn = false, username = null }) =>
   <header className="usa-site-header" role="banner">
     <Disclaimer />
-    <Nav isLoggedIn={ isLoggedIn } />
+    <Nav isLoggedIn={ isLoggedIn } username={ username } />
   </header>
 
 Header.propTypes = propTypes;
