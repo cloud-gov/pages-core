3127a4710017d09312a04614319006366614e36d
Adam Biagianti <adam.biagianti@gsa.gov>
2016-10-27 10:37:25 -0400

    incorporating changes from roger

--- config/passport.js
+++ config/passport.js
@@ -20,21 +20,6 @@ module.exports.passport = {
   local: {
     strategy: require('passport-local').Strategy
   },
-
-  // bearer: {
-  //   strategy: require('passport-http-bearer').Strategy
-  // },
-  //
-  // twitter: {
-  //   name: 'Twitter',
-  //   protocol: 'oauth',
-  //   strategy: require('passport-twitter').Strategy,
-  //   options: {
-  //     consumerKey: 'your-consumer-key',
-  //     consumerSecret: 'your-consumer-secret'
-  //   }
-  // },
-  //
   github: {
     name: 'GitHub',
     protocol: 'oauth2',
@@ -52,37 +37,5 @@ module.exports.passport = {
       14109682, // federalist-users
       14080592  // us-federal-sbst
     ]
-  },
-  //
-  // facebook: {
-  //   name: 'Facebook',
-  //   protocol: 'oauth2',
-  //   strategy: require('passport-facebook').Strategy,
-  //   options: {
-  //     clientID: 'your-client-id',
-  //     clientSecret: 'your-client-secret',
-  //     scope: ['email'] /* email is necessary for login behavior */
-  //   }
-  // },
-  //
-  // google: {
-  //   name: 'Google',
-  //   protocol: 'oauth2',
-  //   strategy: require('passport-google-oauth').OAuth2Strategy,
-  //   options: {
-  //     clientID: 'your-client-id',
-  //     clientSecret: 'your-client-secret'
-  //   }
-  // },
-  //
-  // cas: {
-  //   name: 'CAS',
-  //   protocol: 'cas',
-  //   strategy: require('passport-cas').Strategy,
-  //   options: {
-  //     ssoBaseURL: 'http://your-cas-url',
-  //     serverBaseURL: 'http://localhost:1337',
-  //     serviceURL: 'http://localhost:1337/auth/cas/callback'
-  //   }
-  // }
+  }a
 };
