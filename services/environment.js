92dd7816bbe1df3978395b541faf9eda615a8ce8
Adam Biagianti <adam.biagianti@gsa.gov>
2016-10-31 10:52:20 -0400

    Incorporates deploy cleanup from rogers PR

--- services/environment.js
+++ services/environment.js
@@ -1,6 +1,6 @@
 var cfenv = require('cfenv');
 var appEnv = cfenv.getAppEnv();
-var env = process.env.NODE_ENV || 'development';
+var env = process.env.APP_ENV || 'development';
 var serviceCredsKey = `federalist-${env.toLowerCase()}-env`;
 var federalistCreds = appEnv.getServiceCreds(serviceCredsKey) || {};
 
