b06152d09838c81d16cc5c585f04c21a9f3bbd4a
Jonathan Hooper <jonathan.hooper@gsa.gov>
2016-11-03 12:51:21 -0500

    Add tests for User API authentication
    This commit adds tests which verify that the endpoints for the Site API require authentication before granting access.
    It accomplishes this by sending unauthenticated requests and asserting that the API responds with a not authorized error.

--- test/api/requests/user.test.js
+++ test/api/requests/user.test.js
@@ -1,17 +1,59 @@
+var expect = require("chai").expect
+var request = require("supertest-as-promised")
+var sinon = require("sinon")
+var factory = require("../support/factory")
+var session = require("../support/session")
+
 describe("User API", () => {
   describe("GET /v0/user", () => {
-    it("should require authentication")
+    it("should require authentication", done => {
+      factory(User).then(user => {
+        return request("http://localhost:1337")
+          .get("/v0/user")
+          .expect(403)
+      }).then(response => {
+        expect(response.body).to.be.empty
+        done()
+      })
+    })
+
     it("should redirect to GET /v0/user/:id for the current user")
   })
 
   describe("GET /v0/user/:id", () => {
-    it("should require authentication")
+    it("should require authentication", done => {
+      factory(User).then(user => {
+        return request("http://localhost:1337")
+          .get(`/v0/user/${user.id}`)
+          .expect(403)
+      }).then(response => {
+        expect(response.body).to.be.empty
+        done()
+      })
+    })
+
     it("should show a JSON representation of the current user if the id matches theirs")
     it("should respond with a 403 if the requested user does not match the current user")
   })
 
   describe("POST /v0/user/add-site", () => {
-    it("should require authentication")
+    it("should require authentication", done => {
+      factory(User).then(user => {
+        return request("http://localhost:1337")
+          .post(`/v0/user/add-site`)
+          .send({
+            owner: "example-org",
+            repository: "example-repo",
+            engine: "jekyll",
+            defaultBranch: "master",
+            users: [user.id]
+          })
+          .expect(403)
+      }).then(response => {
+        expect(response.body).to.be.empty
+        done()
+      })
+    })
     it("should create a new site for a remote repository")
     it("should add a user to a site if a site already exists for the remote repository")
     it("should respond with a 400 if no owner or repo is specified")
