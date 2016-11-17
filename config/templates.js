54161e99d0a29beadfc2a9752238e1735451646c
Will Slack <wslack@users.noreply.github.com>
2016-11-17 14:41:54 -0500

    Adds landing page to the templates
    Questions:
    - [ ] Are there any other places this needs to be added?
    - [ ] Where is the image for the thumbnail stored? (Was added [here](https://github.com/18F/federalist/commit/d948f150b8f212483b12dd75bdf59642ff1d4257))
    - [ ] Is there any needed refactoring to make adding other templates easier in the future?

--- config/templates.js
+++ config/templates.js
@@ -22,5 +22,13 @@ module.exports = {
     example: 'https://pages.18f.gov/federalist-modern-team-template/',
     title: 'A clean template for teams and agencies',
     description: 'This is a Jekyll site. It is a simple site to showcase the work of an organization with a few different page types'
+  },
+   'landing': {
+    owner: '18f',
+    branch: 'master',
+    repo: 'federalist-landing-page-template',
+    example: 'http://federalist.18f.gov.s3-website-us-east-1.amazonaws.com/site/18F/federalist-landing-page-template/',
+    title: 'A clean simple landing page for a new report or program',
+    description: 'This site is a single page meant to host information about a new team or report'
   }
 };
