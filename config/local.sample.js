2ccc5b66a63a58b0a69a77388322e7111a8f8178
Jonathan Hooper <jonathan.hooper@gsa.gov>
2016-11-18 11:13:24 -0600

    Update sample config
    This commit updates the sample config to account for changes to the overall config that were made recently.

--- config/local.sample.js
+++ config/local.sample.js
@@ -1,29 +1,46 @@
-module.exports = {
-  //passport: {
-  //  github: {
-  //    options: {
-  //      clientID: '<<get from github>>',
-  //      clientSecret: '<<get from github>>',
-  //      callbackURL: 'http://localhost:1337/auth/github/callback'
-  //    },
-  //    organizations: [
-  //      << get id from https://api.github.com/orgs/<your-org-name> >>
-  //    ]
-  //  }
-  //},
-  //connections: {
-  //  postgres: {
-  //    adapter: 'sails-postgresql',
-  //    database: 'federalist'
-  //  }
-  //},
-  //models: {
-  //  connection: 'postgres'
-  //},
-  // // enable locally for development with redis to make sessions persist
-  // // between app restarts
-  // session: {
-  //   adapter: 'redis',
-  //   db: 0
-  // }
-};
+var AWS = require('aws-sdk')
+
+if (process.env.NODE_ENV != "test") {
+  module.exports = {
+    // passport: {
+    //  github: {
+    //    options: {
+    //      clientID: '123abc',
+    //      clientSecret: '456def',
+    //      callbackURL: 'http://localhost:1337/auth/github/callback'
+    //    },
+    //    organizations: [
+    //      << get id from https://api.github.com/orgs/<your-org-name> >>
+    //    ],
+    //  },
+    // },
+    // connections: {
+    //  postgres: {
+    //    adapter: 'sails-postgresql',
+    //    database: 'federalist',
+    //  },
+    // },
+    // models: {
+    //  connection: 'postgres',
+    // },
+    // build: {
+    //   awsBuildKey: "123abc",
+    //   awsBuildSecret: "123abc",
+    //   s3Bucket: "bucket-name",
+    //   awsRegion: "us-gov-west-1",
+    //   sqsQueue: "https://sqs.us-east-1.amazonaws.com/123abc/456def",
+    //   appName: "federalist-staging",
+    //   appDomain: "fr.cloud.gov",
+    // },
+    // sqs: {
+    //   accessKeyId: "123abc",
+    //   secretAccessKey: "456def",
+    //   region: "us-east-1",
+    // },
+    // S3: new AWS.S3({
+    //   accessKeyId: "123abc",
+    //   secretAccessKey: "456def",
+    //   region: "us-gov-west-1",
+    // }),
+  }
+}
