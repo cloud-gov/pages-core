02a101c38472b979473f0cf15e2c54862bb54cd6
Jonathan Hooper <jonathan.hooper@gsa.gov>
2016-11-28 10:46:33 -0600

    Correct spelling of repo:write_hook scope
    The `repo:write_hook` scope was misspelled in the previous commit.
    This commit fixes the typo.

--- config/passport.js
+++ config/passport.js
@@ -6,7 +6,7 @@ module.exports.passport = {
       clientID: env.GITHUB_CLIENT_ID || 'not_set',
       clientSecret: env.GITHUB_CLIENT_SECRET || 'not_set',
       callbackURL: env.GITHUB_CLIENT_CALLBACK_URL || 'not_set',
-      scope: ['user', 'repo', 'write:repo_hooks']
+      scope: ['user', 'repo', 'write:repo_hook']
     },
     organizations: [
       6233994,  // 18f
