82e1556ba47451fd1a05ebc911cf8ea1d7141359
Jonathan Hooper <jonathan.hooper@gsa.gov>
2016-12-02 13:43:44 -0600

    Fix option to cf login
    When we updated to cf-cli 6.22.2 we did not update the syntax for cf
    login which changed from:
    
    ```
    cf login --u username --p password
    ```
    
    to:
    
    ```
    cf login -u username -p password
    ```

--- scripts/deploy-travis.sh
+++ scripts/deploy-travis.sh
@@ -21,7 +21,7 @@ wget https://s3.amazonaws.com/go-cli/releases/v6.12.4/cf-cli_amd64.deb -qO temp.
 rm temp.deb
 
 cf api $CF_API
-cf login --u $CF_USERNAME --p $CF_PASSWORD --o $CF_ORGANIZATION --s $CF_SPACE
+cf login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
 
 cf zero-downtime-push $CF_APP -f $CF_MANIFEST
 
