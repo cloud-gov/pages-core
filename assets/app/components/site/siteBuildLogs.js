e9f454f0fadbded68d7eddd0297ecf79df51c88d
Jonathan Hooper <jonathan.hooper@gsa.gov>
2016-12-20 10:30:33 -0600

    Display build logs in frontend
    This commit enables the fronted to display build logs to the user. This
    is done with a new component called SiteBuildLogs.
    
    When the SiteBuildLogs component is mounted, it sends a request for
    build logs for the current build. When those builds are loaded, they are
    displayed in a table.

---
+++ assets/app/components/site/siteBuildLogs.js
@@ -0,0 +1,65 @@
+import React from "react";
+import buildLogActions from "../../actions/buildLogActions";
+
+class SiteBuildLogs extends React.Component {
+  componentDidMount() {
+    buildLogActions.fetchBuildLogs({ id: this.props.params.buildId });
+  }
+
+  buildLogs() {
+    if (!this.props.buildLogs) {
+      return [];
+    }
+
+    return this.props.buildLogs.filter(log => {
+      return log.build === parseInt(this.props.params.buildId);
+    }).sort((a, b) => {
+      return new Date(a.createdAt) - new Date(b.createdAt);
+    });
+  }
+
+  render() {
+    if (this.buildLogs().length > 0) {
+      return this.renderBuildLogsTable()
+    } else {
+      return this.renderEmptyState()
+    }
+  }
+
+  renderBuildLogsTable() {
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Source</th>
+            <th>Output</th>
+            <th>Timestamp</th>
+          </tr>
+        </thead>
+        <tbody>
+          { this.buildLogs().map(log => this.renderBuildLogRow(log)) }
+        </tbody>
+      </table>
+    )
+  }
+
+  renderBuildLogRow(log) {
+    return (
+      <tr key={log.id}>
+        <td>{ log.source }</td>
+        <td>
+          <output><pre
+            style={{ whiteSpace: "pre-wrap" }}
+          >{ log.output }</pre></output>
+        </td>
+        <td>{ log.createdAt }</td>
+      </tr>
+    )
+  }
+
+  renderEmptyState() {
+    return <p>This build does not have any build logs</p>;
+  }
+}
+
+export default SiteBuildLogs;
