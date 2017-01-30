54971c4a552f6913ac63f15218f5be6c50a76ede
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-01-30 08:56:48 -0600

    Add pending tests for models
    This commit adds pending unit tests for sequelize models. Currently,
    these tests and the code they are testing are unimplemented. These
    simple provide the examples to be fleshed out later.

---
+++ test/api/unit/modelss/build-log.test.js
@@ -0,0 +1,7 @@
+const expect = require("chai").expect
+
+describe("BuildLog model", () => {
+  describe("before validate hook", () => {
+    it("should sanitize secrets in the log output")
+  })
+})
---
+++ test/api/unit/modelss/site.test.js
@@ -0,0 +1,11 @@
+const expect = require("chai").expect
+
+describe("Site model", () => {
+  describe("before validate hook", () => {
+    it("should lowercase the owner and repository values")
+  })
+
+  describe("after create hook", () => {
+    it("should create a GitHub webhook for the site")
+  })
+})
