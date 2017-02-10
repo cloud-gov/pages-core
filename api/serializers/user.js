5e3260e2015eafd454663e73da147785f736f80a
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-02-10 09:21:22 -0600

    Apply sequelize changes to the User API
    This commit changes the code that support the User API to use the new
    sequelize models instead of the old Waterline models

---
+++ api/serializers/user.js
@@ -0,0 +1,28 @@
+const serialize = (serializable) => {
+  if (serializable.length !== undefined) {
+    const userIds = serializable.map(user => user.id)
+    const query = User.findAll({ where: { id: userIds }, include: [ Site, Build ] })
+
+    return query.then(users => {
+      return users.map(user => serializeObject(user))
+    })
+  } else {
+    const user = serializable
+    const query = User.findById(user.id, { include: [ Site, Build ] })
+
+    return query.then(user => {
+      return serializeObject(user)
+    })
+  }
+}
+
+const serializeObject = (user) => {
+  const json = user.toJSON()
+  json.sites = user.Sites.map(site => site.toJSON())
+  json.builds = user.Builds.map(build => build.toJSON())
+  delete json.Sites
+  delete json.Builds
+  return json
+}
+
+module.exports = { serialize }
---
+++ test/api/unit/serializers/user.test.js
@@ -0,0 +1,37 @@
+const expect = require("chai").expect
+const validateJSONSchema = require('jsonschema').validate
+
+const userSchema = require("../../../../assets/swagger/User.json")
+const factory = require("../../support/factory")
+
+const UserSerializer = require("../../../../api/serializers/user")
+
+describe("UserSerializer", () => {
+  describe(".serialize(serializable)", () => {
+    it("should serialize an object correctly", done => {
+      factory.user().then(user => {
+        return UserSerializer.serialize(user)
+      }).then(object => {
+        const result = validateJSONSchema(object, userSchema)
+        expect(result.errors).to.be.empty
+        done()
+      }).catch(done)
+    })
+
+    it("should serialize an array correctly", done => {
+      const users = Array(3).fill(0).map(() => factory.user())
+
+      Promise.all(users).then(users => {
+        return UserSerializer.serialize(users)
+      }).then(object => {
+        const arraySchema = {
+          type: "array",
+          items: userSchema,
+        }
+        const result = validateJSONSchema(object, arraySchema)
+        expect(result.errors).to.be.empty
+        done()
+      }).catch(done)
+    })
+  })
+})
