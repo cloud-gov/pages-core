3856c44959b0c092e362c4c9f4cec095ffb42894
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-02-22 10:33:00 -0600

    Extract Sails and setup Express middleware stack
    This commit removes Sails and a lot of the cruft that was growing alongside it. The new middleware stack includes middleware that does essentially what Sails did for us, plus a custom middleware to give us our response helpers and a custom middleware to give us routing.
    
    The routing middleware is currently in a TODO state.
    
    Ref #763

--- api/responses/forbidden.js
+++ api/responses/forbidden.js
@@ -1,8 +1,4 @@
-module.exports = function badRequest(error = {}, options) {
-  const res = this.res;
-
-  sails.log.verbose('Sending 403 ("Forbidden") response: \n', error)
-
+module.exports = (error, { req, res }) => {
   res.status(403)
   return res.json({
     message: error.message || "You are not authorized to perform that action",
--- api/responses/serverError.js
+++ api/responses/serverError.js
@@ -1,8 +1,4 @@
-module.exports = function badRequest(error = {}, options) {
-  const res = this.res;
-
-  sails.log.error('Sending 500 ("Server Error") response: \n', error)
-
+module.exports = (error, { req, res }) => {
   res.status(500)
   return res.json({
     message: "Internal server error",
