d3e240cf5a37a258d0c1d03c0f90603b036c28b3
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-03-01 15:19:17 -0600

    Wire up build log controller
    This commit sets up the build log router so that it will send requests to the build log contorller through the appropriate middlewares.

--- api/routers/build-log.js
+++ api/routers/build-log.js
@@ -1,6 +1,10 @@
 const router = require("express").Router()
 const BuildLogController = require("../controllers/build-log")
+const buildCallback = require("../policies/buildCallback")
+const passport = require("../policies/passport")
+const sessionAuth = require("../policies/sessionAuth")
 
-// TODO: Setup routes and middlewares
+router.get("/v0/build/:build_id/log", passport, sessionAuth, BuildLogController.find)
+router.post("/v0/build/:build_id/log/:token", buildCallback, BuildLogController.create)
 
 module.exports = router
