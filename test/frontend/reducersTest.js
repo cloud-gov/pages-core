619d0c7c5227ba844fa8d097eb54a5fa5f42791e
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-03-02 16:18:03 -0600

    Setup static assets directory
    This commit changes the way static assets are handled. It uses the
    express.static middleware to serve assets from the `public` directory.
    It moves static assets from the `assets` directory into the `public`
    directory and moves uncompiled javascript from the `assets` directory
    into the `frontend` directory.

---
+++ test/frontend/reducersTest.js
@@ -0,0 +1,38 @@
+import { expect } from "chai";
+import proxyquire from "proxyquire";
+
+proxyquire.noCallThru();
+
+describe("maps reducers", () => {
+  let fixture;
+  const BUILDS = "builds are here";
+  const ALERT = "alerts is here";
+  const SITES = "sites are here";
+  const USER = "user is here";
+
+  beforeEach(() => {
+    fixture = proxyquire("../../frontend/reducers.js", {
+      "./reducers/builds": BUILDS,
+      "./reducers/alert": ALERT,
+      "./reducers/sites": SITES,
+      "./reducers/user": USER
+    }).default;
+  });
+
+  it("maps builds reducer", () => {
+    expect(fixture.builds).to.equal(BUILDS);
+  });
+
+  it("maps alert reducer", () => {
+    expect(fixture.alert).to.equal(ALERT);
+  });
+
+  it("maps sites reducer", () => {
+    expect(fixture.sites).to.equal(SITES);
+  });
+
+  it("maps user reducer", () => {
+    expect(fixture.user).to.equal(USER);
+  });
+
+});
