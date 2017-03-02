560090177268811fb953916f8039e562aca65a0b
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-03-02 14:44:03 -0600

    Configure route for build API
    This commit configures the router for the build API so it routes
    requests to the correct BuildController action through the proper
    middlewares.

--- api/routers/build.js
+++ api/routers/build.js
@@ -1,6 +1,12 @@
 const router = require("express").Router()
 const BuildController = require("../controllers/build")
+const buildCallback = require("../policies/buildCallback")
+const passport = require("../policies/passport")
+const sessionAuth = require("../policies/sessionAuth")
 
-// TODO: Setup routes and middlewares
+router.get("/v0/build", passport, sessionAuth, BuildController.find)
+router.post("/v0/build", passport, sessionAuth, BuildController.create)
+router.get("/v0/build/:id", passport, sessionAuth, BuildController.findOne)
+router.post("/v0/build/:id/status/:token", buildCallback, BuildController.status)
 
 module.exports = router
