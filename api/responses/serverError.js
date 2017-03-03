9688ed891c9395f5c9421e30a94efce64190a87c
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-03-03 12:38:05 -0600

    Replace sails.log and morgan with winston
    This commit consolidates all logging functionality under winston, the
    same logging module we use in federalist-builder. It uses the same
    configurations to set the logging level, but uses a winston based
    logging middleware, and uses winston's logging functions in places where
    logging happens.

--- api/responses/serverError.js
+++ api/responses/serverError.js
@@ -1,4 +1,8 @@
+const logger = require("winston")
+
 module.exports = (error = {}, { req, res }) => {
+  winston.error("Sending 500: ", error)
+
   res.status(500)
   return res.json({
     message: "Internal server error",
