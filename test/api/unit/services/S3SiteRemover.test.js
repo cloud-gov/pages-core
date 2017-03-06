c1e7682ce960342c81a12db9acc087a6cb0f87a5
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-03-06 14:03:19 -0600

    Delete contents from S3 when a site is deleted
    This commit adds a S3SiteRemover service which deletes all of the objects related to a site from S3. This service is invoked on a site when that site is deleted by a user. It searches for previews and the live sites files and then deletes them.
    
    Ref #289

---
+++ test/api/unit/services/S3SiteRemover.test.js
@@ -0,0 +1,76 @@
+const AWS = require('aws-sdk-mock')
+const expect = require("chai").expect
+const proxyquire = require("proxyquire")
+const factory = require("../../support/factory")
+const config = require("../../../../config")
+
+const S3 = {
+  listObjects: () => Promise.resolve(),
+  deleteObjects: () => Promise.resolve(),
+}
+
+AWS.mock("S3", "listObjects", (params, cb) => S3.listObjects(params, cb))
+AWS.mock("S3", "deleteObjects", (params, cb) => S3.deleteObjects(params, cb))
+
+const S3SiteRemover = require("../../../../api/services/S3SiteRemover")
+
+describe("S3SiteRemover", () => {
+  describe(".removeSite(site)", () => {
+    it("should delete all objects in the `site/<org>/<repo>` and `preview/<org>/<repo> directories", done => {
+      const siteObjectsToDelete = []
+      const previewObjectsToDelete = []
+      let site
+      let objectsWereDeleted = false
+      let siteObjectsWereListed = false
+      let previewObjectsWereListed = false
+
+      S3.listObjects = (params, cb) => {
+        expect(params.Bucket).to.equal(config.s3.bucket)
+        if (params.Prefix === `site/${site.owner}/${site.repository}`) {
+          siteObjectsWereListed = true
+          cb(null, {
+            Contents: siteObjectsToDelete.map(Key => ({ Key }))
+          })
+        } else if (params.Prefix === `preview/${site.owner}/${site.repository}`) {
+          previewObjectsWereListed = true
+          cb(null, {
+            Contents: previewObjectsToDelete.map(Key => ({ Key }))
+          })
+        }
+      }
+      S3.deleteObjects = (params, cb) => {
+        expect(params.Bucket).to.equal(config.s3.bucket)
+
+        const objectsToDelete = siteObjectsToDelete.concat(previewObjectsToDelete)
+        expect(params.Delete.Objects).to.have.length(objectsToDelete.length)
+        params.Delete.Objects.forEach(object => {
+          const index = objectsToDelete.indexOf(object.Key)
+          expect(index).to.be.at.least(0)
+          objectsToDelete.splice(index, 1)
+        })
+        objectsWereDeleted = true
+        cb(null, {})
+      }
+
+      factory.site().then(model => {
+        site = model
+
+        const sitePrefix = `site/${site.owner}/${site.repository}`
+        siteObjectsToDelete.push(`${sitePrefix}/index.html`)
+        siteObjectsToDelete.push(`${sitePrefix}/redirect`)
+        siteObjectsToDelete.push(`${sitePrefix}/redirect/index.html`)
+        const previewPrefix = `preview/${site.owner}/${site.repository}`
+        previewObjectsToDelete.push(`${previewPrefix}/index.html`)
+        previewObjectsToDelete.push(`${previewPrefix}/redirect`)
+        previewObjectsToDelete.push(`${previewPrefix}/redirect/index.html`)
+
+        return S3SiteRemover.removeSite(site)
+      }).then(() => {
+        expect(siteObjectsWereListed).to.equal(true)
+        expect(previewObjectsWereListed).to.equal(true)
+        expect(objectsWereDeleted).to.equal(true)
+        done()
+      }).catch(done)
+    })
+  })
+})
