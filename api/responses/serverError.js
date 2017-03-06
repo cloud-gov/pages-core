c66930e3e4617c08d4b80691b9f4bbdc6d4782d1
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-03-06 14:03:19 -0600

    Delete contents from S3 when a site is deleted
    This commit adds a S3SiteRemover service which deletes all of the objects related to a site from S3. This service is invoked on a site when that site is deleted by a user. It searches for previews and the live sites files and then deletes them.
    
    Ref #289

--- api/responses/serverError.js
+++ api/responses/serverError.js
@@ -1,7 +1,7 @@
 const logger = require("winston")
 
 module.exports = (error = {}, { req, res }) => {
-  winston.error("Sending 500: ", error)
+  logger.error("Sending 500: ", error)
 
   res.status(500)
   return res.json({
