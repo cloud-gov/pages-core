3bd4b007460de37e48b7137fb8d744089b27173e
Shawn Allen <shawn.allen@gsa.gov>
2017-03-08 16:37:52 -0800

    refactor TemplateSiteList as flexible 3-up grid

--- assets/app/components/AddSite/TemplateSiteList/index.js
+++ assets/app/components/AddSite/TemplateSiteList/index.js
@@ -5,9 +5,33 @@ import TemplateSite from './templateSite';
 const propTypes = {
   templates: React.PropTypes.object.isRequired,
   handleSubmitTemplate: React.PropTypes.func.isRequired
-}
+};
+
+const MAX_CELLS_PER_ROW = 3;
+const CELL_WIDTHS = ['', 'whole', 'half', 'third', 'fourth'];
+
+/**
+ * Create a two-dimensional array of values that represent one or more rows
+ * of values, with `perRow` values per row.
+ *
+ * @param {array<*>} values
+ * @param {integer} perRow
+ * @return {array<array<*>>}
+ */
+const createRowsOf = (values, perRow) => {
+  let row = [];
+  const rows = [row];
+  values.forEach((val, index) => {
+    if (index === perRow) {
+      rows.push(row = []);
+    }
+    row.push(val);
+  });
+  return rows;
+};
 
 class TemplateList extends React.Component {
+
   constructor(props) {
     super(props);
 
@@ -27,6 +51,41 @@ class TemplateList extends React.Component {
   render() {
     const { templates } = this.props;
 
+    const templateKeys = Object.keys(templates);
+    // if there are fewer templates than cells per row,
+    // fill the space with them
+    const cellsPerRow = Math.min(templateKeys.length, MAX_CELLS_PER_ROW);
+    // generate a two-dimensional array of template keys
+    const templateRows = createRowsOf(templateKeys, cellsPerRow);
+    // i.e. 'whole', 'half', 'third', or 'fourth'
+    const cellSize = CELL_WIDTHS[cellsPerRow];
+
+    let index = 0;
+
+    const templateGrid = templateRows.map((row, rowIndex) => {
+      return (
+        <div className="usa-grid" key={rowIndex}>
+          {row.map((templateName, cellIndex) => {
+            const template = templates[templateName];
+            return (
+              <div className={`usa-width-one-${cellSize}`}>
+                <TemplateSite
+                  name={templateName}
+                  key={cellIndex}
+                  index={index++}
+                  thumb={templateName}
+                  active={this.state.activeChildId}
+                  handleChooseActive={this.handleChooseActive}
+                  handleSubmit={this.props.handleSubmitTemplate}
+                  defaultOwner={this.props.defaultOwner}
+                  {...template} />
+              </div>
+            );
+          })}
+        </div>
+      );
+    });
+
     return (
       <div>
         <div className="usa-grid">
@@ -34,24 +93,7 @@ class TemplateList extends React.Component {
             <h2>Choose from one of our templates</h2>
           </div>
         </div>
-        <div className="usa-grid">
-          {Object.keys(templates).map((templateName, index) => {
-            const template = templates[templateName];
-
-            return (
-              <TemplateSite
-                name={templateName}
-                key={index}
-                index={index}
-                thumb={templateName}
-                active={this.state.activeChildId}
-                handleChooseActive={this.handleChooseActive}
-                handleSubmit={this.props.handleSubmitTemplate}
-                defaultOwner={this.props.defaultOwner}
-                {...template} />
-            );
-          })}
-        </div>
+        {templateGrid}
       </div>
     );
   }
