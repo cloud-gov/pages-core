46641a7d732d6d5024a2c59d9f4e7b747740b297
Will Slack <wslack@users.noreply.github.com>
2017-03-09 23:31:37 -0500

    change template description text
    Not perfect, but better than before

--- config/templates.js
+++ config/templates.js
@@ -12,16 +12,16 @@ module.exports.templates = {
     repo: 'federalist-modern-team-template',
     branch: 'master',
     example: 'https://pages.18f.gov/federalist-modern-team-template/',
-    title: 'A clean template for teams and agencies',
-    description: 'This is a Jekyll site. It is a simple site to showcase the work of an organization with a few different page types'
+    title: 'Multi-page Template',
+    description: 'Simple Jekyll site to showcase the work of an organization'
   },
   'landing': {
     owner: '18f',
     repo: 'federalist-landing-page-template',
     branch: 'master',
     example: 'http://federalist.18f.gov.s3-website-us-east-1.amazonaws.com/site/18F/federalist-landing-page-template/',
-    title: 'A clean simple landing page for a new report or program',
-    description: 'This site is a single page meant to host information about a new team or report'
+    title: 'Single-page template',
+    description: 'Single page to host information about a specific topic or report'
   },
   'developer': {
     owner: 'USG-Website-Templates',
@@ -29,14 +29,14 @@ module.exports.templates = {
     branch: 'gh-pages',
     example: 'http://usg-website-templates.github.io/developer-hub/',
     title: 'A developer hub for API documentation',
-    description: 'A template for building developer hubs and API docs'
+    description: 'A template for developer hubs and API docs'
   },
   'microsite': {
     owner: 'USG-Website-Templates',
     repo: 'microsite-template',
     branch: 'gh-pages',
     example: 'https://usg-website-templates.github.io/microsite-template/',
-    title: 'A simple microsite',
-    description: 'A quick little site template'
+    title: 'Basic microsite',
+    description: 'A site template for a small site'
   },
 };
