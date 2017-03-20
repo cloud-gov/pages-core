e69244bca28992cc06543a8301811bf1d62a359a
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-03-20 14:26:10 -0500

    Add ability for published branch serializer to serialize a single branch
    Prior to this commit, the serializer for published branches only handled an array of branch names and had no concept of files for a given branch. This commit changes it so that it can serialize a single branch and include a list of files on that branch as part of its result.

--- api/serializers/published-branch.js
+++ api/serializers/published-branch.js
@@ -1,10 +1,23 @@
-const serialize = (site, branchNames) => {
-  const branches = branchNames.map(name => ({
+const serialize = (site, branch, files) => {
+  if (branch instanceof Array) {
+    const array = branch.map(name => serializeObject(site, name))
+    return Promise.resolve(array)
+  } else {
+    const object = serializeObject(site, branch, files)
+    return Promise.resolve(object)
+  }
+}
+
+const serializeObject = (site, name, files) => {
+  const object = {
     name,
     site: site.toJSON(),
     viewLink: site.viewLinkForBranch(name),
-  }))
-  return Promise.resolve(branches)
+  }
+  if (files) {
+    object.files = files
+  }
+  return object
 }
 
 module.exports = { serialize }
--- test/api/unit/serializers/published-branch.test.js
+++ test/api/unit/serializers/published-branch.test.js
@@ -7,7 +7,7 @@ const factory = require("../../support/factory")
 const PublishedBranchSerializer = require("../../../../api/serializers/published-branch")
 
 describe("PublishedBranchesSerializer", () => {
-  describe(".serialize(site, branchNames)", () => {
+  describe(".serialize(site, branch, files)", () => {
     it("should serialize a site with a list of published branches correctly", done => {
       let site
 
@@ -29,6 +29,25 @@ describe("PublishedBranchesSerializer", () => {
         expect(object[1].name).to.equal("preview")
         expect(object[1].viewLink).to.equal(previewViewLink)
         expect(object[1].site.id).to.equal(site.id)
+        done()
+      }).catch(done)
+    })
+
+    it("should serialize a site with a single published branch correctly", done => {
+      let site
+
+      factory.site({ defaultBranch: "default" }).then(model => {
+        site = model
+        return PublishedBranchSerializer.serialize(site, "default", ["abc", "abc/def", "ghi"])
+      }).then(object => {
+        const viewLink = site.viewLinkForBranch("default")
+        const result = validateJSONSchema(object, PublishedBranchSchema)
+
+        expect(result.errors).to.be.empty
+        expect(object.name).to.equal("default")
+        expect(object.viewLink).to.equal(viewLink)
+        expect(object.site.id).to.equal(site.id)
+        expect(object.files).to.deep.equal(["abc", "abc/def", "ghi"])
         done()
       }).catch(done)
     })
