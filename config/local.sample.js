17192794ab461e0e6b02fad5014b169e5ee33d0f
James Seppi <james.seppi@gsa.gov>
2017-05-25 11:11:25 -0500

    fix linting errors

--- config/local.sample.js
+++ config/local.sample.js
@@ -1,31 +1,31 @@
-if (process.env.NODE_ENV != "test") {
+if (process.env.NODE_ENV !== 'test') {
   module.exports = {
     passport: {
-     github: {
-       options: {
-         clientID: '123abc',
-         clientSecret: '456def',
-         callbackURL: 'http://localhost:1337/auth/github/callback'
-       },
-       organizations: [
-         1234567,
-       ],
-     },
+      github: {
+        options: {
+          clientID: '123abc',
+          clientSecret: '456def',
+          callbackURL: 'http://localhost:1337/auth/github/callback',
+        },
+        organizations: [
+          1234567,
+        ],
+      },
     },
     postgres: {
       database: 'federalist',
     },
     sqs: {
-      accessKeyId: "123abc",
-      secretAccessKey: "456def",
-      region: "us-gov-west-1",
-      queue: "https://sqs.us-east-1.amazonaws.com/123abc/456def",
+      accessKeyId: '123abc',
+      secretAccessKey: '456def',
+      region: 'us-gov-west-1',
+      queue: 'https://sqs.us-east-1.amazonaws.com/123abc/456def',
     },
     s3: {
-      accessKeyId: "123abc",
-      secretAccessKey: "456def",
-      region: "us-gov-west-1",
-      bucket: "cg-123-abc-456-def",
+      accessKeyId: '123abc',
+      secretAccessKey: '456def',
+      region: 'us-gov-west-1',
+      bucket: 'cg-123-abc-456-def',
     },
-  }
+  };
 }
