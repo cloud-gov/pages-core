cc9d2d75d333e5de68c8abb0fa2a50513575d736
James Seppi <james.seppi@gsa.gov>
2017-06-01 11:47:44 -0500

    remove beta notice from disclaimer

--- frontend/components/disclaimer.js
+++ frontend/components/disclaimer.js
@@ -7,7 +7,6 @@ const Disclaimer = () =>
         <img className="usa-flag_icon" alt="US flag signifying that this is a United States Federal Government website" src="/images/uswds/us_flag_small.png" />
         An official website of the United States Government
       </span>
-      <span className="usa-disclaimer-stage">This site is currently in beta. <a href="https://18f.gsa.gov/dashboard/stages/#beta">Learn more.</a></span>
     </div>
   </div>;
 
