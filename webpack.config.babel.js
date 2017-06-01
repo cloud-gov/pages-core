c02b808693ddea100268ab117dfe83100e323b87
James Seppi <james.seppi@gsa.gov>
2017-06-01 13:04:59 -0500

    almost working

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -4,15 +4,10 @@ import autoprefixer from 'autoprefixer';
 import ExtractTextPlugin from 'extract-text-webpack-plugin';
 import ManifestPlugin from 'webpack-manifest-plugin';
 
-const extractSass = new ExtractTextPlugin({
+const extractStyles = new ExtractTextPlugin({
   filename: 'styles/styles.[contenthash].css',
   allChunks: true,
 });
-
-// const extractUswds = new ExtractTextPlugin({
-//   filename: 'styles/uswds.[contenthash].css',
-//   allChunks: true,
-// });
 
 const manifestPlugin = new ManifestPlugin({
   fileName: '../webpack-manifest.json',
@@ -41,7 +36,7 @@ export default {
       },
       {
         test: /\.scss$/,
-        use: extractSass.extract([
+        use: extractStyles.extract([
           'css-loader?sourceMap',
           {
             loader: 'postcss-loader',
@@ -53,20 +48,16 @@ export default {
           'sass-loader?sourceMap',
         ]),
       },
-      // {
-      //   test: /\.css$/,
-      //   use: extractUswds.extract(['css-loader']),
-      // },
       {
         test: /\.(gif|png|jpe?g|svg|ttf|woff2?|eot)$/i,
         use: [{
           loader: 'file-loader',
           options: {
-            outputPath: 'styles/webpackAssets/',
+            outputPath: './webpackAssets/',
           },
         }],
       },
     ],
   },
-  plugins: [extractSass, /* extractUswds,*/ manifestPlugin],
+  plugins: [extractStyles, manifestPlugin],
 };
