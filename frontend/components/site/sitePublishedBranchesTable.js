c1c7510115ae77b81d7abb1f026143821b63f519
James Seppi <james.seppi@gsa.gov>
2017-06-19 15:30:45 -0500

    Improve styling of log tables to not overrun the page width
    Ref #844.
    
    Also filter out files with no names, which are redirect objects, from the SitePublishedFilesTable (ref #1015).

--- frontend/components/site/sitePublishedBranchesTable.js
+++ frontend/components/site/sitePublishedBranchesTable.js
@@ -1,30 +1,20 @@
-import React from "react";
+import React from 'react';
+import PropTypes from 'prop-types';
+
 import { Link } from 'react-router';
-import publishedBranchActions from "../../actions/publishedBranchActions";
-import LoadingIndicator from "../loadingIndicator"
+import publishedBranchActions from '../../actions/publishedBranchActions';
+import LoadingIndicator from '../loadingIndicator';
 
 class SitePublishedBranchesTable extends React.Component {
   componentDidMount() {
-    publishedBranchActions.fetchPublishedBranches({ id: this.props.params.id })
+    publishedBranchActions.fetchPublishedBranches({ id: this.props.params.id });
   }
 
   publishedBranches() {
     if (this.props.publishedBranches.isLoading || !this.props.publishedBranches.data) {
-      return []
-    } else {
-      return this.props.publishedBranches.data
+      return [];
     }
-  }
-
-  render() {
-    const branches = this.publishedBranches()
-    if (this.props.publishedBranches.isLoading) {
-      return this.renderLoadingState()
-    } else if (!branches.length) {
-      return this.renderEmptyState()
-    } else {
-      return this.renderPublishedBranchesTable()
-    }
+    return this.props.publishedBranches.data;
   }
 
   renderPublishedBranchesTable() {
@@ -40,7 +30,7 @@ class SitePublishedBranchesTable extends React.Component {
           { this.publishedBranches().map(this.renderPublishedBranchRow.bind(this)) }
         </tbody>
       </table>
-    )
+    );
   }
 
   renderPublishedBranchRow(branch) {
@@ -52,28 +42,52 @@ class SitePublishedBranchesTable extends React.Component {
           { this.renderBranchFilesLink(branch) }
         </td>
       </tr>
-    )
+    );
   }
 
   renderBranchViewLink(branch) {
-    return <a href={branch.viewLink} target="_blank">View</a>
+    return <a href={branch.viewLink} target="_blank" rel="noopener noreferrer">View</a>;
   }
 
   renderBranchFilesLink(branch) {
-    const href = `/sites/${branch.site.id}/published/${branch.name}`
-    return <Link to={href}>Files</Link>
+    const href = `/sites/${branch.site.id}/published/${branch.name}`;
+    return <Link to={href}>Files</Link>;
   }
 
   renderLoadingState() {
-    return <LoadingIndicator/>
+    return <LoadingIndicator />;
   }
 
   renderEmptyState() {
-    return <p>
+    return (<p>
       No branches have been published.
       Please wait for build to complete or check logs for error message.
-    </p>
+    </p>);
+  }
+
+  render() {
+    const branches = this.publishedBranches();
+    if (this.props.publishedBranches.isLoading) {
+      return this.renderLoadingState();
+    } else if (!branches.length) {
+      return this.renderEmptyState();
+    }
+    return this.renderPublishedBranchesTable();
   }
 }
 
-export default SitePublishedBranchesTable
+SitePublishedBranchesTable.propTypes = {
+  params: PropTypes.shape({
+    id: PropTypes.string.isRequired,
+  }).isRequired,
+  publishedBranches: PropTypes.shape({
+    isLoading: PropTypes.bool.isRequired,
+    data: PropTypes.array,
+  }),
+};
+
+SitePublishedBranchesTable.defaultProps = {
+  publishedBranches: null,
+};
+
+export default SitePublishedBranchesTable;
