ddb918bf67b159e93ca03ead38ce5e1d1a8ad733
James Seppi <james.seppi@gsa.gov>
2017-07-19 09:00:31 -0500

    setup scanjs in eslint and ignore it in Code Climate
    - Change .eslintrc files into JavaScript .eslintrc.js files
    - Only include scanjs and no-unsanitized plugins and rules when scanjs-rules is able to be `require`'d. This prevents eslint in Code Climate from attempting to run those plugins/rules, because they are not available there and will error out.

---
+++ test/.eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  env: {
+    mocha: true,
+  },
+  rules: {
+    'no-unused-expressions': [0], // because chai's expect is weird
+
+    /** ScanJS rule overrides from main .eslintrc **/
+    'scanjs-rules/assign_to_search': 0,
+  },
+};
