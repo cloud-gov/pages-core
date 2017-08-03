c9332d9e0d622fb52cbe414f1eb32828a0336db1
James Seppi <james.seppi@gsa.gov>
2017-08-03 09:33:58 -0500

    ignore 'import/no-named-as-default' rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ const finalRules = {
   /* airbnb config overrides */
   'react/jsx-filename-extension': [0],
   'import/no-extraneous-dependencies': [0],
+  'import/no-named-as-default': [0], // allow component to be the same as the default export
   'class-methods-use-this': [0],
   'no-throw-literal': [0],
   'comma-dangle': ['error',
