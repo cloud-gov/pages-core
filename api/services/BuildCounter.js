716b63c8112054e82fa5c38469f0754da3b36337
Jonathan Hooper <jonathan.hooper@gsa.gov>
2017-11-08 15:00:12 -0600

    PR feedback for build counter

--- api/services/BuildCounter.js
+++ api/services/BuildCounter.js
@@ -2,7 +2,7 @@ const moment = require('moment');
 const { Build } = require('../models');
 
 const countBuildsFromPastWeek = () => {
-  const oneWeekAgo = moment().subtract(1, 'week').startOf('day').format();
+  const oneWeekAgo = moment().subtract(1, 'week').startOf('day');
   return Build.count({ where: { createdAt: { $gt: oneWeekAgo } } });
 };
 
