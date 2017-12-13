109c7e1f54d2f1e0b67385451e68a3b891257439
Will Slack <wslack@users.noreply.github.com>
2017-12-13 16:54:24 -0500

    add explanatory text

--- frontend/components/site/SiteSettings/index.jsx
+++ frontend/components/site/SiteSettings/index.jsx
@@ -59,6 +59,11 @@ class SiteSettings extends React.Component {
 
     return (
       <div>
+        <p>
+          See our doucmentation site for more about
+          <a href="https://federalist-docs.18f.gov/pages/using-federalist/#managing-site-settings">these settings</a> or 
+          <a href="https://federalist-docs.18f.gov/pages/using-federalist/previews/">viewing site previews</a>.
+        </p>
         <h3>Basic Settings</h3>
         <BasicSiteSettings
           initialValues={basicInitialValues}
