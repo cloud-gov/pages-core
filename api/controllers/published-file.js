6adac3be51929acdb26a1585e4570567b3547177
James Seppi <james.seppi@gsa.gov>
2017-12-21 15:45:57 -0600

    linting fixes

--- api/controllers/published-file.js
+++ api/controllers/published-file.js
@@ -1,35 +1,35 @@
-const PublishedBranchSerializer = require("../serializers/published-branch")
-const S3PublishedFileLister = require("../services/S3PublishedFileLister")
-const siteAuthorizer = require("../authorizers/site")
-const { Site } = require("../models")
+const PublishedBranchSerializer = require('../serializers/published-branch');
+const S3PublishedFileLister = require('../services/S3PublishedFileLister');
+const siteAuthorizer = require('../authorizers/site');
+const { Site } = require('../models');
 
 module.exports = {
   find: (req, res) => {
-    let site
-    let files
-    const branch = req.params["branch"]
+    let site;
+    let files;
+    const branch = req.params.branch;
 
-    Promise.resolve(Number(req.params["site_id"])).then(id => {
+    Promise.resolve(Number(req.params.site_id)).then((id) => {
       if (isNaN(id)) {
-        throw 404
+        throw 404;
       }
-      return Site.findById(id)
-    }).then(model => {
+      return Site.findById(id);
+    }).then((model) => {
       if (model) {
-        site = model
+        site = model;
       } else {
-        throw 404
+        throw 404;
       }
-      return siteAuthorizer.findOne(req.user, site)
-    }).then(() => {
-      return S3PublishedFileLister.listPublishedFilesForBranch(site, branch)
-    }).then(resolvedFiles => {
-      files = resolvedFiles
-      return PublishedBranchSerializer.serialize(site, branch)
-    }).then(branchJSON => {
-      res.json(files.map(file => {
-        return Object.assign(file, { publishedBranch: branchJSON })
-      }))
-    }).catch(res.error)
-  }
-}
+      return siteAuthorizer.findOne(req.user, site);
+    })
+    .then(() => S3PublishedFileLister.listPublishedFilesForBranch(site, branch))
+    .then((resolvedFiles) => {
+      files = resolvedFiles;
+      return PublishedBranchSerializer.serialize(site, branch);
+    })
+    .then((branchJSON) => {
+      res.json(files.map(file => Object.assign(file, { publishedBranch: branchJSON })));
+    })
+    .catch(res.error);
+  },
+};
