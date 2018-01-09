04f1cef9b739ea0cd8443180c1281d2bedbfc2d1
Adam Biagianti <adam.biagianti@gsa.gov>
2018-01-09 10:33:18 -0500

    Add more robust UserAction support on front-end
    * Adds reducer, action creators and tests

---
+++ frontend/reducers/userActions.js
@@ -0,0 +1,28 @@
+import {
+  userActionFetchStartedType as USER_ACTIONS_FETCH_STARTED,
+  userActionReceivedType as USER_ACTIONS_RECEIVED,
+} from '../actions/actionCreators/userActions';
+
+const initialState = {
+  isLoading: false,
+  data: [],
+};
+
+export default function userActions(state = initialState, action) {
+  switch (action.type) {
+    case USER_ACTIONS_FETCH_STARTED:
+      return {
+        ...initialState,
+        isLoading: true,
+      };
+
+    case USER_ACTIONS_RECEIVED:
+      return {
+        isLoading: false,
+        data: action.userActions
+      };
+
+    default:
+      return state;
+  }
+};
