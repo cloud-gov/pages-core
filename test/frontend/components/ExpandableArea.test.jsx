117f3a7538aa11cddf13eb8bcdbb6157ae1dd44e
Scott Weber <stephen.weber@gsa.gov>
2018-01-17 11:13:41 -0500

    updating expandable area classes to use accordion

--- test/frontend/components/ExpandableArea.test.jsx
+++ test/frontend/components/ExpandableArea.test.jsx
@@ -13,12 +13,12 @@ describe('<ExpandableArea/>', () => {
     );
     expect(wrapper.exists()).to.be.true;
 
-    const content = wrapper.find('.expandable-area-content');
+    const content = wrapper.find('.usa-accordion-content');
     expect(content.exists()).to.be.true;
     expect(content.find('p').exists()).to.be.true;
     expect(content.find('p').text()).to.equal('hello');
 
-    const button = wrapper.find('.expandable-area-button');
+    const button = wrapper.find('.usa-accordion-button');
     expect(button.prop('aria-controls')).to.equal(content.prop('id'));
   });
 
@@ -30,8 +30,8 @@ describe('<ExpandableArea/>', () => {
     );
     expect(wrapper.exists()).to.be.true;
 
-    let button = wrapper.find('.expandable-area-button');
-    let content = wrapper.find('.expandable-area-content');
+    let button = wrapper.find('.usa-accordion-button');
+    let content = wrapper.find('.usa-accordion-content');
 
     // start collapsed
     expect(button.prop('aria-expanded')).to.equal(false);
@@ -39,15 +39,15 @@ describe('<ExpandableArea/>', () => {
 
     // click to toggle to expanded
     button.simulate('click');
-    button = wrapper.find('.expandable-area-button');
-    content = wrapper.find('.expandable-area-content');
+    button = wrapper.find('.usa-accordion-button');
+    content = wrapper.find('.usa-accordion-content');
     expect(button.prop('aria-expanded')).to.equal(true);
     expect(content.prop('aria-hidden')).to.equal(false);
 
     // click to toggle back to collapsed
     button.simulate('click');
-    button = wrapper.find('.expandable-area-button');
-    content = wrapper.find('.expandable-area-content');
+    button = wrapper.find('.usa-accordion-button');
+    content = wrapper.find('.usa-accordion-content');
     expect(button.prop('aria-expanded')).to.equal(false);
     expect(content.prop('aria-hidden')).to.equal(true);
   });
