af292ea7aaf2ba21c3acf4810ed0d0ff8bafd24f
Adam Biagianti <adam.biagianti@gsa.gov>
2018-01-17 10:08:03 -0500

    Fix users being unable to rebuild any build
    * Add a new build error to differentiate between a build not being found
    and a branch not being found on github
    
    * Adds build resolver to move finding branches out of an authorizer
    
    * Properly use siteAuthorizer and return 403s when user doesnt have
    access to a site
    
    * A build doesn't require authorization provided a user has access to a
    site, so remove those checks

---
+++ test/api/unit/services/BuildResolver.test.js
@@ -0,0 +1,152 @@
+const { expect } = require('chai');
+const { stub } = require('sinon');
+const factory = require('../../support/factory');
+const BuildResolver = require('../../../../api/services/BuildResolver');
+const GitHub = require('../../../../api/services/GitHub');
+const buildErrors = require('../../../../api/responses/buildErrors');
+
+describe('BuildResolver', () => {
+  let ghStub;
+  beforeEach(() => {
+    ghStub = stub(GitHub, 'getBranch');
+  });
+
+  afterEach(() => {
+    ghStub.restore();
+  });
+
+  it('returns a 404 when a build cannot be found by build id', (done) => {
+    const user = factory.user();
+    const site = factory.site({ users: Promise.all([user]) });
+    let values;
+
+    Promise.props({
+      user,
+      site,
+    })
+    .then((response) => {
+      values = response;
+      return BuildResolver.getBuild(values.user, {
+        buildId: 1,
+        siteId: values.site.id
+      })
+    })
+    .then(() => {
+      done();
+    })
+    .catch((err) => {
+      expect(err.status).to.equal(404);
+      expect(err.message).to.equal(buildErrors.BUILD_NOT_FOUND);
+      done()
+    });
+  });
+
+  it('finds a build when passed build and site ids', (done) => {
+    const user = factory.user();
+    const site = factory.site({ users: Promise.all([user]) });
+    const build = factory.build({ site });
+    let values;
+
+    Promise.props({
+      user,
+      site,
+      build,
+    })
+    .then((response) => {
+      values = response;
+      return BuildResolver.getBuild(values.user, {
+        buildId: values.build.id,
+        siteId: values.site.id
+      })
+    })
+    .then((build) => {
+      expect(build.id).to.equal(values.build.id);
+      done();
+    })
+    .catch(done);
+  });
+
+  it('finds a build by branch name', (done) => {
+    const branch = 'staging';
+    const sha = '123abc';
+    const user = factory.user();
+    const site = factory.site({ users: Promise.all([user]) });
+    const build = factory.build({ site, branch });
+
+    Promise.props({
+      user,
+      site,
+      build,
+    })
+    .then((values) =>
+      BuildResolver.getBuild(values.user, {
+        branch,
+        siteId: values.site.id,
+        sha,
+      })
+    )
+    .then((build) => {
+      expect(build.branch).to.equal(branch);
+      expect(build.commitSha).to.equal(sha);
+      done();
+    })
+    .catch(done);
+  });
+
+  describe('checking for branch on github', () => {
+    const branch = 'staging';
+    const sha = '123abc';
+    const user = factory.user();
+    const site = factory.site({ users: Promise.all([user]) });
+    let pValues;
+
+    beforeEach(() => {
+      pValues = Promise.props({
+        user,
+        site,
+      });
+    });
+
+    it('calls out to github if the branch cannot be found locally', (done) => {
+      pValues
+      .then((values) => {
+        ghStub.returns(Promise.resolve({
+          name: branch,
+          commit: {
+            sha,
+          },
+        }));
+
+        return BuildResolver.getBuild(values.user, {
+          branch,
+          siteId: values.site.id,
+          sha,
+        });
+      })
+      .then((build) => {
+        expect(build.branch).to.equal(branch);
+        expect(build.commitSha).to.equal(sha);
+        done();
+      })
+      .catch(done);
+    });
+
+    it('returns a 404 when a build cannot be started because branch does not exist', (done) => {
+      pValues
+      .then((values) => {
+        ghStub.returns(Promise.reject());
+
+        return BuildResolver.getBuild(values.user, {
+          branch,
+          siteId: values.site.id,
+          sha,
+        });
+      })
+      .catch((err) => {
+        expect(err.status).to.equal(404);
+        expect(err.message).to.equal(buildErrors.BRANCH_NOT_FOUND);
+        done();
+      });
+    });
+  });
+});
