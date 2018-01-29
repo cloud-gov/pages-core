e7241af24b993e67a0ea1968ecf38926a6f33b1b
Adam B <el-mapache@users.noreply.github.com>
2018-01-29 10:50:05 -0500

    Copy a site/repo clone functionality (#1543)
    * Adds view and basic site copy functionality
    
    * SiteCreator is getting really gnarly and needs to be refactored
    
    * Adds front end tests
    
    * CC fixes, slight change to SiteCreator
    
    * More changes to SiteCreator
    
    * Break out site validation into function
    * attempt to reduce more duplication
    
    * Adds additional tests for site copy feature
    
    * Updates for feedback
    
    * Remove uneeded comment
    * Add alias for githubRepoRegex
    
    * Add additional comment to BranchField reuse

--- api/utils/validators.js
+++ api/utils/validators.js
@@ -1,7 +1,9 @@
-const branchRegex = /^[a-zA-Z0-9._-]+$/;
+const branchRegex = /^[^-][a-zA-Z0-9._-]+$/;
+const githubUsernameRegex = /^[^-][a-zA-Z-]+$/;
 const shaRegex = /^[a-f0-9]{40}$/;
 
 module.exports = {
   branchRegex,
   shaRegex,
+  githubUsernameRegex,
 };
