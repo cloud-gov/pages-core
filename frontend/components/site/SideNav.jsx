685c9527e2b8c568370a987749c0ae6951b51713
Eric Ronne <eric.ronne@gsa.gov>
2018-01-29 11:33:02 -0600

    Tweaks new icon svgs sizes and removes sprite code

--- frontend/components/site/SideNav.jsx
+++ frontend/components/site/SideNav.jsx
@@ -10,7 +10,7 @@ const SideNav = ({ siteId }) => (
       {
         SITE_NAVIGATION_CONFIG.map(conf => (
           <li key={conf.route}>
-            <Link to={`/sites/${siteId}/${conf.route}`} className={`icon ${conf.icon}`}>
+            <Link to={`/sites/${siteId}/${conf.route}`} className={`${conf.icon}`}>
               {conf.display}
             </Link>
           </li>
