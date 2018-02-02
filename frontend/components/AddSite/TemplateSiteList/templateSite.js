5e850e7ebf9737c7559b03d2d997df0e326dfa1f
Scott Weber <stephen.weber@gsa.gov>
2018-02-02 15:15:32 -0500

    fixing br tag

--- frontend/components/AddSite/TemplateSiteList/templateSite.js
+++ frontend/components/AddSite/TemplateSiteList/templateSite.js
@@ -101,7 +101,7 @@ class TemplateSite extends React.Component {
               >
                 View sample
               </a>
-              <br/>
+              <br />
               <button
                 className="usa-button"
                 onClick={this.handleChooseActive}
