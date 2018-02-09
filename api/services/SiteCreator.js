d5d354abe7e1076173bd85890d57f25fb8213158
Will Slack <wslack@users.noreply.github.com>
2018-02-09 18:21:49 -0500

    update error message

--- api/services/SiteCreator.js
+++ api/services/SiteCreator.js
@@ -69,7 +69,7 @@ function checkGithubOrg({ user, owner }) {
     // Has this org authorized federalist as an oauth app?
     if (!federalistAuthorizedOrg) {
       throw {
-        message: `Organization '${owner}' hasn't approved access for Federalist. Ask an owner to authorize it.`,
+        message: `Federalist can't confirm org permissions for '${owner}'. Either '${owner}' hasn't approved access for Federalist or you aren't an org member. Ensure you are an org member and ask an org owner to authorize Federalist for the organization.`,
         status: 403,
       };
     }
