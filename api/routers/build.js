b748093b00e2b226e58b557d6f9535ee8e26adab
James Seppi <james.seppi@gsa.gov>
2018-02-15 11:15:32 -0600

    removes /build/:id route, controller method, and tests
    That route is not in use, and due to some authorizer simplifications, probably wouldn't work as expected if it ever did need to get used. Ref https://github.com/18F/federalist/issues/1467.
    No need to keep it around if we don't need it.

--- api/routers/build.js
+++ api/routers/build.js
@@ -6,7 +6,6 @@ const csrfProtection = require('../policies/csrfProtection');
 
 router.get('/site/:site_id/build', sessionAuth, BuildController.find);
 router.post('/build', sessionAuth, csrfProtection, BuildController.create);
-router.get('/build/:id', sessionAuth, BuildController.findOne);
 router.post('/build/:id/status/:token', buildCallback, BuildController.status);
 
 module.exports = router;
