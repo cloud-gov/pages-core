503a0a02ee3df4f3fb3125f1ae1c3f4c7466ef44
Adam Biagianti <adam.biagianti@gsa.gov>
2018-04-02 13:54:21 -0400

    Add case-studies route to controller
    * Add partial for case studies html blocks block
    * Add data include for case studies to ease updating
    * Start updating image grid in footer to use flexbox

---
+++ public/data/case-studies.js
@@ -0,0 +1,34 @@
+module.exports = [
+  {
+    heading: 'Vote.gov',
+    subheading: null,
+    copy: 'Vote.gov makes it easier for eligible voters participate in our democratic process by providing streamlined information on how to vote. Site visitors can register electronically in states that allow it, and download a printable application in a number of languages for those states where electronic registration isn\'t an option.',
+    image: '/images/thumbs/cs-preview-vote.png',
+    imageCaption: null,
+    agency: null,
+    date: null,
+    templateUsed: null,
+  },
+
+  {
+    heading: 'College Scorecard',
+    subheading: null,
+    copy: 'The U.S. Department of Education uses Federalist to host College Scorecard. Combined with an API server, College Scorecard provides important information on everything from financial aid to expected earnings. Federalist\'s cloud infrastructure helps manage site traffic during spikes in visitors, making sure College Scorecard is always available.',
+    image: '/images/thumbs/cs-preview-college.png',
+    imageCaption: null,
+    agency: null,
+    date: null,
+    templateUsed: null,
+  },
+  
+  {
+    heading: 'Department of Homeland Security',
+    subheading: null,
+    copy: 'DHS used a Federalist template to publish information about a new <a href="https://cyber.dhs.gov/">binding operational directive</a>. Federalist makes it easy to update and maintain the documentation, reducing e-mail queries to DHS.',
+    image: '/images/thumbs/cs-preview-dhs.png',
+    imageCaption: null,
+    agency: null,
+    date: null,
+    templateUsed: null,
+  },
+];
