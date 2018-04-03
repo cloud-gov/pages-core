3baff62f5d6b30ce5d9ef401e11b68a3ce2139c5
David Warren <davide.warren@gsa.gov>
2018-04-03 12:59:53 -0400

    Update case-studies.js
    switched order to put Performance.gov and CIO.gov first and second

--- public/data/case-studies.js
+++ public/data/case-studies.js
@@ -35,6 +35,16 @@ module.exports = {
   ],
   exampleSites: [
     {
+      image: '/images/example-sites/cards/performance-gov.png',
+      caption: 'Performance.gov',
+      link: 'https://www.performance.gov',
+    },
+    {
+      image: '/images/example-sites/cards/cio-gov.png',
+      caption: 'CIO.gov',
+      link: 'https://www.cio.gov',
+    },
+    {
       image: '/images/example-sites/cards/18f-gsa-gov.png',
       caption: '18F.gsa.gov',
       link: 'https://18f.gsa.gov',
@@ -63,16 +73,6 @@ module.exports = {
       image: '/images/example-sites/cards/tech-gsa-gov.png',
       caption: 'tech.gsa.gov',
       link: 'https://tech.gsa.gov',
-    },
-    {
-      image: '/images/example-sites/cards/performance-gov.png',
-      caption: 'Performance.gov',
-      link: 'https://www.performance.gov',
-    },
-    {
-      image: '/images/example-sites/cards/cio-gov.png',
-      caption: 'CIO.gov',
-      link: 'https://www.cio.gov',
     },
   ],
 };
