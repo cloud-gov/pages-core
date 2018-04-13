08fe2f43da77d1390ca48024a61aa9a50be1cb7c
Adam Biagianti <adam.biagianti@gsa.gov>
2018-04-13 11:57:48 -0400

    Adds basic glossary code
    * Basic initialization for app code
      * will probably need to be moved to a static build file
    * adds basic css
    * adds partial

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -7,13 +7,31 @@ import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 
+import Glossary from 'glossary-panel';
+
+const terms = [
+  {
+    term: "Federalist",
+    definition: "A cool app that does some cool stuff and is cool",
+  },
+  {
+    term: "Mammal",
+    definition: 'warm blooded animal with hair or fur that gives birth to live young',
+  },
+  {
+    term: 'Platypus',
+    definition: 'A mammal that breaks all the rules',
+  },
+];
+
+new Glossary(terms);
+
 import routes from './routes';
 import store from './store';
 
 import './sass/styles.scss';
 
 const history = syncHistoryWithStore(browserHistory, store);
-
 const mainEl = document.querySelector('#js-app');
 
 render((
