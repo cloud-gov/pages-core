5e313c26844787a247947188163f4fd2b6bb87af
Adam Biagianti <adam.biagianti@gsa.gov>
2018-04-19 16:54:19 -0400

    Adds standalone definitions file

--- frontend/glossary.js
+++ frontend/glossary.js
@@ -1,20 +1,8 @@
 import Glossary from 'glossary-panel';
+import definitions from 'public/data/glossary-definitions';
 
-const terms = [
-  {
-    term: "Federalist",
-    definition: "A cool app that does some cool stuff and is cool",
-  },
-  {
-    term: "Mammal",
-    definition: 'warm blooded animal with hair or fur that gives birth to live young',
-  },
-  {
-    term: 'Platypus',
-    definition: 'A mammal that breaks all the rules',
-  },
-];
-
-new Glossary(terms, {}, {
-  termClass: 'glossary__term accordion__button',
-});
+if (Object.keys(definitions).length) {
+  new Glossary(definitions, {}, {
+    termClass: 'glossary__term accordion__button',
+  });
+}
