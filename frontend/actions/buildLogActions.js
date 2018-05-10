56eb5ed1f7f23b73bc5bdd94593c0b2d1446b60d
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2018-05-09 20:35:10 -0400

    see if spaces will please linter

--- frontend/actions/buildLogActions.js
+++ frontend/actions/buildLogActions.js
@@ -2,8 +2,8 @@ import api from '../util/federalistApi';
 import { dispatch } from '../store';
 
 import {
-  buildLogsFetchStarted as createBuildLogsFetchStartedAction,
-  buildLogsReceived as createBuildLogsReceivedAction,
+	buildLogsFetchStarted as createBuildLogsFetchStartedAction,
+ 	buildLogsReceived as createBuildLogsReceivedAction,
 } from './actionCreators/buildLogActions';
 
 const dispatchBuildLogsFetchStartedAction = () => {
@@ -15,20 +15,20 @@ const dispatchBuildLogsReceivedAction = (logs) => {
 };
 
 const fetchNextBuildLogsPage = (build, page) =>
-	api.fetchBuildLogs(build, page)
-	.then((buildLogs) => {
-		dispatchBuildLogsReceivedAction(buildLogs);
-		if (buildLogs.length > 0) {
-			fetchNextBuildLogsPage(build, page + 1);
-		}
-		return Promise.resolve;
-	});
+  api.fetchBuildLogs(build, page)
+  .then((buildLogs) => {
+    dispatchBuildLogsReceivedAction(buildLogs);
+    if (buildLogs.length > 0) {
+      fetchNextBuildLogsPage(build, page + 1);
+    }
+    return Promise.resolve;
+  });
 
 const fetchBuildLogs = (build) => {
-	dispatchBuildLogsFetchStartedAction();
-	return fetchNextBuildLogsPage(build, 1);
+  dispatchBuildLogsFetchStartedAction();
+  return fetchNextBuildLogsPage(build, 1);
 }
 
 export default {
-	fetchBuildLogs,
+  fetchBuildLogs,
 };
