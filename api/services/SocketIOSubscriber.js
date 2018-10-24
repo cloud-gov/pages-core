8546cd08912c7c88d240a939d66b95ebcb94fbbd
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2018-10-24 11:09:52 -0400

    socket io to user websocket transport only

--- api/services/SocketIOSubscriber.js
+++ api/services/SocketIOSubscriber.js
@@ -3,8 +3,8 @@ const { User, Site } = require('../models');
 
 module.exports = {
   joinRooms: (_socket) => {
-    if (_socket.request && _socket.request.session && _socket.request.session.passport) {
-      const userId = _socket.request.session.passport.user;
+    if (_socket.handshake && _socket.handshake.session && _socket.handshake.session.passport) {
+      const userId = _socket.handshake.session.passport.user;
       if (userId) {
         return User.findOne({
           where: { id: userId },
