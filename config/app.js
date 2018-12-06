2650ad3cca91813e099dba0acadbb17b60b2cca0
Eddie A Tejeda <eddie.tejeda@gsa.gov>
2018-12-06 13:07:35 -0500

    use consistant naming 'Manage Sites' for login

--- config/app.js
+++ config/app.js
@@ -16,5 +16,5 @@ module.exports = {
   hostname: process.env.APP_HOSTNAME || 'http://localhost:1337',
   preview_hostname: process.env.FEDERALIST_PREVIEW_HOSTNAME || 'http://localhost:1338',
   app_env: process.env.APP_ENV || 'development',
-  homepageUrl: homepageUrl,
+  homepageUrl,
 };
