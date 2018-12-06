4562a283bf2462f4c39c89fc8ca2406bb045d14e
Eddie A Tejeda <eddie.tejeda@gsa.gov>
2018-12-06 14:25:47 -0500

    check redirect without waiting for response

--- test/api/requests/main.test.js
+++ test/api/requests/main.test.js
@@ -145,11 +145,8 @@ describe('Main Site', () => {
         .get('/examples')
         .set('Cookie', cookie)
         .expect(302)
+        .expect('Location', 'http://localhost:4000/examples')
       )
-      .then((response) => {
-        expect(response.headers.location).to.equal('http://localhost:4000/examples');
-        done();
-      })
       .catch(done);
     });
   });
