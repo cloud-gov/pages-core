b8e02646a5814695a6d3cf2b8ec9d56834731c88
David Corwin <david.corwin@gsa.gov>
2019-01-18 10:17:58 -0800

    Added auto refresh and toggle to build logs, slight refactoring to enhance UI when loading

--- test/frontend/components/site/siteBuildLogs.test.js
+++ test/frontend/components/site/siteBuildLogs.test.js
@@ -1,22 +1,27 @@
 import React from 'react';
 import { expect } from 'chai';
 import { shallow } from 'enzyme';
+import sinon from 'sinon';
 
-import { SiteBuildLogs } from '../../../../frontend/components/site/siteBuildLogs';
+import { SiteBuildLogs, REFRESH_INTERVAL } from '../../../../frontend/components/site/siteBuildLogs';
 import LoadingIndicator from '../../../../frontend/components/LoadingIndicator';
 
+let props;
+
 describe('<SiteBuildLogs/>', () => {
-  it('should render a button to download logs if builds exist', () => {
-    const props = {
+  beforeEach(() => {
+    props = {
       params: {
         buildId: '123',
       },
       buildLogs: {
         isLoading: false,
-        data: [{ source: 'theSource', createdAt: '2018-02-01', output: 'blahblah' }],
+        data: [{ id: 1, source: 'theSource', createdAt: '2018-11-05T13:15:30Z', output: 'blahblah' }],
       },
     };
+  });
 
+  it('should render a button to download logs if builds exist', () => {
     const wrapper = shallow(<SiteBuildLogs {...props} />);
     expect(wrapper.find('SiteBuildLogTable')).to.have.length(1);
     expect(wrapper.find('p')).to.have.length(0);
@@ -26,15 +31,8 @@ describe('<SiteBuildLogs/>', () => {
   });
 
   it('should render a loading state if builds are loading', () => {
-    const props = {
-      params: {
-        buildId: '123',
-      },
-      buildLogs: {
-        isLoading: true,
-        data: [],
-      },
-    };
+    props.buildLogs.isLoading = true;
+    props.buildLogs.data = [];
 
     const wrapper = shallow(<SiteBuildLogs {...props} />);
     expect(wrapper.find('SiteBuildLogTable')).to.have.length(0);
@@ -42,21 +40,80 @@ describe('<SiteBuildLogs/>', () => {
   });
 
   it('should render an empty state if there are no builds', () => {
-    const props = {
-      params: {
-        buildId: '123',
-      },
-      buildLogs: {
-        data: [],
-        isLoading: false,
-      },
-    };
+    props.buildLogs.isLoading = false;
+    props.buildLogs.data = [];
 
     const wrapper = shallow(<SiteBuildLogs {...props} />);
     expect(wrapper.find('SiteBuildLogTable')).to.have.length(0);
-    expect(wrapper.find('p')).to.have.length(1);
+    // expect(wrapper.find('p')).to.have.length(1);
     expect(wrapper.find('p').contains('This build does not have any build logs.')).to.be.true;
     expect(wrapper.find('RefreshBuildLogsButton')).to.have.length(1);
     expect(wrapper.find('DownloadBuildLogsButton')).to.have.length(0);
+  });
+
+  it('should fetch the builds on mount', () => {
+    const spy = sinon.spy();
+
+    props.actions = { fetchBuildLogs: spy };
+
+    const wrapper = shallow(<SiteBuildLogs {...props} />);
+    wrapper.instance().componentDidMount();
+    expect(spy.calledOnce).to.equal(true);
+  });
+
+  describe('Auto Refresh', () => {
+    const AUTO_REFRESH_SELECTOR = '[data-test="toggle-auto-refresh"]';
+
+    let clock;
+
+    beforeEach(() => {
+      clock = sinon.useFakeTimers();
+    });
+
+    afterEach(() => {
+      clock.restore();
+    });
+
+    it('should default to auto refresh: ON', () => {
+      const wrapper = shallow(<SiteBuildLogs {...props} />);
+      console.log(wrapper.html());
+      expect(wrapper.state('autoRefresh')).to.equal(true);
+      expect(wrapper.find(AUTO_REFRESH_SELECTOR).text()).to.equal('Auto Refresh: ON');
+    });
+
+    it('should toggle auto refresh when the `auto refresh` button is clicked', () => {
+      const wrapper = shallow(<SiteBuildLogs {...props} />);
+
+      wrapper.find(AUTO_REFRESH_SELECTOR).simulate('click');
+      expect(wrapper.state('autoRefresh')).to.equal(false);
+      expect(wrapper.find(AUTO_REFRESH_SELECTOR).text()).to.equal('Auto Refresh: OFF');
+
+      wrapper.find(AUTO_REFRESH_SELECTOR).simulate('click');
+      expect(wrapper.state('autoRefresh')).to.equal(true);
+      expect(wrapper.find(AUTO_REFRESH_SELECTOR).text()).to.equal('Auto Refresh: ON');
+    });
+
+    it('should refresh builds according to the refresh interval when `auto refresh` is on', () => {
+      const spy = sinon.spy();
+
+      props.actions = { fetchBuildLogs: spy };
+
+      const wrapper = shallow(<SiteBuildLogs {...props} />);
+      wrapper.instance().componentDidMount();
+      clock.tick(REFRESH_INTERVAL + 1000);
+      expect(spy.callCount).to.equal(2);
+    });
+
+    it('should NOT refresh builds when `auto refresh` is turned off', () => {
+      const spy = sinon.spy();
+
+      props.actions = { fetchBuildLogs: spy };
+
+      const wrapper = shallow(<SiteBuildLogs {...props} />);
+      wrapper.instance().componentDidMount();
+      wrapper.setState({ autoRefresh: false });
+      clock.tick(REFRESH_INTERVAL + 1000);
+      expect(spy.callCount).to.equal(1);
+    });
   });
 });
