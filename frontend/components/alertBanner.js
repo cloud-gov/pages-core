24dd447fd22c00d2533bc34ec611c0d9a0db02f4
David Corwin <david.corwin@gsa.gov>
2019-01-29 10:00:35 -0800

    Lint fix

--- frontend/components/alertBanner.js
+++ frontend/components/alertBanner.js
@@ -26,7 +26,7 @@ const AlertBanner = ({ children, header, message, status, alertRole }) => {
     return null;
   }
 
-  const errorMsg =
+  const errorMsg = (
     <span>
       We are experiencing an unexpected problem, please wait a few moments and try the following:
       <ol>
@@ -34,7 +34,7 @@ const AlertBanner = ({ children, header, message, status, alertRole }) => {
         <li>log out and back in to your account</li>
       </ol>
       If this error persists, please contact us, we apologize for the inconvenience.
-    </span>
+  </span>);
 
   const msg = status === 'error' ? errorMsg : message;
 
