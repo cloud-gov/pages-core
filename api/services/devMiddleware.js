ac380bdba0f4e70d5cae53a70c06f59e6f3d6eca
David Corwin <david.corwin@gsa.gov>
2019-03-07 14:17:08 -0800

    serve static files without session/auth

---
+++ api/services/devMiddleware.js
@@ -0,0 +1,11 @@
+/* eslint-disable global-require */
+module.exports = () => {
+  const webpack = require('webpack');
+  const webpackDevMiddleware = require('webpack-dev-middleware');
+  const webpackConfig = require('../../webpack.development.config.js');
+  const compiler = webpack(webpackConfig);
+
+  return webpackDevMiddleware(compiler, {
+    publicPath: webpackConfig.output.publicPath,
+  });
+}
