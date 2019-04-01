e9c1f57c84479836db729c046db8abd45df06670
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2019-04-01 14:24:05 -0400

    add test for repoLastVerified for user just logged in

--- test/frontend/components/siteList/repoLastVerified.test.js
+++ test/frontend/components/siteList/repoLastVerified.test.js
@@ -20,6 +20,14 @@ describe('<RepoLastVerified />', () => {
     expect(wrapper.find('p').text()).to.equal(VERIFIED_BASE);
   });
 
+  it('not display warning if not found and site older than threshold when just logged in', () => {
+    const daysNotVerified = 6; // greater than default threshold 5
+    const createdAt = new Date(new Date() - (daysNotVerified * 24 * 60 * 60 * 1000)).toString();
+    wrapper = shallow(<RepoLastVerified site={{ repoLastVerified: undefined, createdAt }} />);
+
+    expect(wrapper.find('p').length).to.equal(0);
+  });
+
   it('does not display warning if not found and site created less than threshold', () => {
     const daysNotVerified = 3; // greater than default threshold 5
     const createdAt = new Date(new Date() - (daysNotVerified * 24 * 60 * 60 * 1000)).toString();
@@ -34,6 +42,12 @@ describe('<RepoLastVerified />', () => {
     expect(wrapper.find('p').text()).to.equal(VERIFIED_BASE + MOST_RECENT_VERIFIED);
   });
 
+  it('not display most recent repo verification when user just logged in', () => {
+    wrapper = shallow(<RepoLastVerified site={{ repoLastVerified: MOST_RECENT_VERIFIED_TIME }} />);
+
+    expect(wrapper.find('p').length).to.equal(0);
+  });
+
   it('repoLastVerified today - under the threshold', () => {
     const repoLastVerified = new Date(new Date() - (3 * 24 * 60 * 60 * 1000)).toString();
     wrapper = shallow(<RepoLastVerified site={{ repoLastVerified }} userUpdated={userUpdated} />);
@@ -44,5 +58,11 @@ describe('<RepoLastVerified />', () => {
     const repoLastVerified = new Date(new Date() - (3 * 24 * 60 * 60 * 1000)).toString();
     wrapper = shallow(<RepoLastVerified site={{ repoLastVerified }} daysNotVerified={2} userUpdated={userUpdated} />);
     expect(wrapper.find('p').length).to.equal(1);
+  });
+
+  it('repoLastVerified under passed threshold but user just logged in', () => {
+    const repoLastVerified = new Date(new Date() - (3 * 24 * 60 * 60 * 1000)).toString();
+    wrapper = shallow(<RepoLastVerified site={{ repoLastVerified }} daysNotVerified={2} />);
+    expect(wrapper.find('p').length).to.equal(0);
   });
 });
