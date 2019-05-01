344974cc588fef3d8f96079d054b7066fd488817
David Corwin <david.corwin@gsa.gov>
2019-05-01 08:53:42 -0700

    Ignore ESLint global-require error

--- api/routers/index.js
+++ api/routers/index.js
@@ -10,6 +10,7 @@ mainRouter.use(require('./main'));
 
 // Temporary until we are ready to release
 if (config.app.app_env !== 'production') {
+  // eslint-disable-line global-require
   mainRouter.use(require('./external-auth'));
 }
 
