aafece55bcbae9b478d78a1edb9d748de614dd66
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2019-10-03 13:29:05 -0400

    more readable

--- api/services/SiteUserAuditor.js
+++ api/services/SiteUserAuditor.js
@@ -15,9 +15,8 @@ const auditUser = (user, auditor) => {
       const removed = [];
       sites.forEach((site) => {
         const fullName = [site.owner, site.repository].join('/').toUpperCase();
-        if (!(repos.find(repo => repo.full_name.toUpperCase() === fullName)) || // site not in repos
-          (repos.find(repo => repo.full_name.toUpperCase() === fullName &&
-          !repo.permissions.push))) { // site does not have push permissions
+        const repoFound = repos.find(repo => repo.full_name.toUpperCase() === fullName)
+        if (!repoFound || !repoFound.permissions.push) { // site does not have push permissions
           const r = site.removeUser(user)
             .then(() => UserActionCreator.addRemoveAction({
               userId: auditor.id,
