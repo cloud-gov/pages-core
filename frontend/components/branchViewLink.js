e379863afe5df18859015a8be465b307256f725e
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2019-10-25 14:30:12 -0400

    set preview links for old builds of  migrated sites to federalist proxy

--- frontend/components/branchViewLink.js
+++ frontend/components/branchViewLink.js
@@ -16,12 +16,12 @@ const getUrlAndViewText = (branchName, site, completedAt) => {
   }
 
   // temp for migration - should if block be removed by end of year 2019
-  // if (completedAt && ((new Date(completedAt) < new Date('2019-09-06'))) && site.createdAt && (new Date(site.createdAt) < new Date('2019-06-05'))) {
-  //   return {
-  //     url: `https://federalist-proxy.app.cloud.gov/preview/${site.owner}/${site.repository}/${branchName}/`,
-  //     viewText: 'Preview site',
-  //   };
-  // }
+  if (completedAt && ((new Date(completedAt) < new Date('2019-10-25T20:00Z'))) && site.createdAt && (new Date(site.createdAt) < new Date('2019-06-05T20:09Z'))) {
+    return {
+      url: `https://federalist-proxy.app.cloud.gov/preview/${site.owner}/${site.repository}/${branchName}/`,
+      viewText: 'Preview site',
+    };
+  }
 
   return {
     url: `https://${site.awsBucketName}.app.cloud.gov/preview/${site.owner}/${site.repository}/${branchName}/`,
