21649a1c2c3bf7eb1e079947787f2b30e1207f44
David Corwin <david.corwin@gsa.gov>
2020-01-07 11:50:18 -0800

    Add generic error handling for sequalize data conversion error, refactor find build log method

--- api/controllers/build-log.js
+++ api/controllers/build-log.js
@@ -1,4 +1,3 @@
-const siteAuthorizer = require('../authorizers/site');
 const buildLogSerializer = require('../serializers/build-log');
 const { Build, BuildLog } = require('../models');
 
@@ -12,71 +11,51 @@ function decodeb64(str) {
 module.exports = {
   create: (req, res) => {
     Promise.resolve(Number(req.params.build_id))
-    .then((id) => {
-      if (isNaN(id)) { throw 404; }
-      return Build.findByPk(id);
-    })
-    .then((build) => {
-      if (!build) {
-        throw 404;
-      } else if (build.token !== req.params.token) {
-        throw 403;
-      }
-
-      return BuildLog.create({
-        build: build.id,
-        output: decodeb64(req.body.output),
-        source: req.body.source,
-      });
-    })
-    .then(buildLog => buildLogSerializer.serialize(buildLog))
-    .then((buildLogJSON) => { res.json(buildLogJSON); })
-    .catch((err) => {
-      res.error(err);
-    });
-  },
-
-  find: (req, res) => {
-    let build;
-    const limit = 5;
-
-    const isPlaintext = req.query.format &&
-      req.query.format.toLowerCase() === 'text';
-
-    return Promise.resolve(Number(req.params.build_id))
       .then((id) => {
-        if (isNaN(id)) {
-          throw 404;
-        }
+        if (isNaN(id)) { throw 404; }
         return Build.findByPk(id);
-      }).then((model) => {
-        build = model;
+      })
+      .then((build) => {
         if (!build) {
           throw 404;
+        } else if (build.token !== req.params.token) {
+          throw 403;
         }
-        return siteAuthorizer.findOne(req.user, { id: build.site });
+
+        return BuildLog.create({
+          build: build.id,
+          output: decodeb64(req.body.output),
+          source: req.body.source,
+        });
       })
-      .then(() =>
-        Promise.resolve(Number(req.params.page) || 1)
-        .then(page => BuildLog.findAll({
-          attributes: ['id'],
-          where: { build: build.id },
-          order: [['id', 'ASC']],
-          offset: (limit * (page - 1)),
-          limit,
-        }))
-      )
-      .then(buildLogs => buildLogSerializer.serialize(buildLogs, { isPlaintext }))
-      .then((serializedBuildLogs) => {
-        if (isPlaintext) {
-          // .findAll always resolves to an array, so join is available
-          res.type('text').send(serializedBuildLogs.join('\n\n'));
-        } else {
-          res.json(serializedBuildLogs);
-        }
-      })
+      .then(buildLog => buildLogSerializer.serialize(buildLog))
+      .then((buildLogJSON) => { res.json(buildLogJSON); })
       .catch((err) => {
         res.error(err);
       });
+  },
+
+  find: async (req, res) => {
+    const limit = 5;
+    const { params, user } = req;
+    const { build_id: buildId, page = 1 } = params;
+
+    const build = await Build.forUser(user).findByPk(buildId);
+
+    if (!build) {
+      return res.status(404).send('Not Found');
+    }
+
+    const buildLogs = await BuildLog.findAll({
+      where: { build: build.id },
+      order: [['id', 'ASC']],
+      offset: (limit * (page - 1)),
+      limit,
+      include: [Build],
+    });
+
+    const serializedBuildLogs = await buildLogSerializer.serialize(buildLogs);
+
+    return res.json(serializedBuildLogs);
   },
 };
--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const ConnectSession = require('connect-session-sequelize');
 const nunjucks = require('nunjucks');
 const flash = require('connect-flash');
 const http = require('http');
+const { SequelizeDatabaseError } = require('sequelize');
 const io = require('socket.io');
 const redis = require('redis');
 const redisAdapter = require('socket.io-redis');
@@ -150,9 +151,12 @@ app.use((err, req, res, next) => {
 });
 
 // Generic error handler
-// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line
 app.use((err, _req, res, _next) => {
-  res.status(500).send({ message: `An unexpected error occurred: ${err.message}` });
+  if (err instanceof SequelizeDatabaseError) {
+    return res.status(404).send('Not Found');
+  }
+  return res.status(500).send({ message: `An unexpected error occurred: ${err.message}` });
 });
 
 socket.use((_socket, next) => {
