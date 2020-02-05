08d3a9a22b2614b910c77a0b80b00a7498ee5b06
David Corwin <david.corwin@gsa.gov>
2020-02-05 10:24:23 -0800

    Use original gatsby template repo

--- config/templates.js
+++ config/templates.js
@@ -21,10 +21,10 @@ module.exports = {
   },
   gatsby: {
     owner: '18f',
-    repo: 'federalist-uswds-gatsby2',
+    repo: 'federalist-uswds-gatsby',
     branch: 'master',
     engine: 'node.js',
-    example: 'https://federalist-proxy.app.cloud.gov/site/18f/federalist-uswds-gatsby2/',
+    example: 'https://federalist-proxy.app.cloud.gov/site/18f/federalist-uswds-gatsby/',
     title: 'Gatsby - U.S. Web Design System v2.0',
     description: 'A flexible multi-page setup intended to introduce an agency or program using Gatsby.js. It offers an engaging Hero section for a current priority and a landing page layout designed to provide clarity and context for first time visitors. This template also offers navigation options for internal pages.',
     thumb: '/images/federalist-gatsby-uswds-template.png',
