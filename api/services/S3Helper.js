45be1135317b2b81fc26315e6a0c042c8546df04
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2020-03-27 16:26:29 -0400

    Update S3Helper.js

--- api/services/S3Helper.js
+++ api/services/S3Helper.js
@@ -121,12 +121,13 @@ class S3Client {
   // ToDo refactor and move `putBucketWebsite` config in site creation flow
   putBucketWebsite(owner, repository, max = 10) {
     let attempt = 0;
+    let start;
     const { bucket, client } = this;
     const params = createWebsiteParams(owner, repository, bucket);
 
     return new Promise((resolve, reject) => {
       const request = () => {
-        const start = new Date().getTime();
+      start = new Date().getTime();
         client.putBucketWebsite(params, (err, data) => {
           if (err && attempt < max) {
             putBucketLogger('info', bucket, 'Retry', { start, attempt });
