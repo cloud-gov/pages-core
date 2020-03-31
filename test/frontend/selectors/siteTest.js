f86215bfcf145c64a2f16daba5db51ccb6bc3f62
David Corwin <david.corwin@gsa.gov>
2020-03-31 14:29:52 -0700

    Small refactor of test

--- test/frontend/selectors/siteTest.js
+++ test/frontend/selectors/siteTest.js
@@ -3,33 +3,20 @@ import { currentSite } from '../../../frontend/selectors/site';
 
 describe('siteSelectors', () => {
   describe('.currentSite', () => {
+    const state = {
+      data: [
+        { id: 2 },
+        { id: 3 },
+      ],
+    };
+
     it('returns the site for the given id', () => {
-      const state = {
-        data: [
-          { id: 2 },
-          { id: 3 },
-        ],
-      };
-
-      const id = '3';
-
-      const site = currentSite(state, id);
-
+      const site = currentSite(state, '3');
       expect(site).to.eq(state.data[1]);
     });
 
     it('returns the null if there is not site for the given id', () => {
-      const state = {
-        data: [
-          { id: 2 },
-          { id: 3 },
-        ],
-      };
-
-      const id = '4';
-
-      const site = currentSite(state, id);
-
+      const site = currentSite(state, '4');
       expect(site).to.be.undefined;
     });
   });
