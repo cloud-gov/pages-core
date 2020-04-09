e9130ab0fbfe5df46e56062dbdb3e00de5e18f51
Andrew Burnes <andrew.burnes@gsa.gov>
2020-04-09 09:20:21 -0700

    Simplify admin session auth

--- api/policies/adminSessionAuth.js
+++ api/policies/adminSessionAuth.js
@@ -3,28 +3,28 @@ const utils = require('../utils');
 
 module.exports = async function adminSessionAuth(req, res, next) {
   req.session.authRedirectPath = undefined;
-  if (req.session.authenticated) {
-    const lastAuthenticatedAt = req.session.authenticatedAt;
-    if (!lastAuthenticatedAt || utils.isPastAuthThreshold(lastAuthenticatedAt)) {
-      // check that the authenticated user is still an admin member
-      try {
-        await GitHub.validateAdmin(req.user.githubAccessToken);
-      } catch (error) {
-        req.logout();
-        req.session.destroy();
-        return res.forbidden({
-          message: 'You are not permitted to perform this action.',
-        });
-      }
 
+  if (!req.session.authenticated) {
+    return res.forbidden({
+      message: 'You are not permitted to perform this action. Are you sure you are logged in?',
+    });
+  }
+
+  const lastAuthenticatedAt = req.session.authenticatedAt;
+  if (!lastAuthenticatedAt || utils.isPastAuthThreshold(lastAuthenticatedAt)) {
+    // check that the authenticated user is still an admin member
+    try {
+      await GitHub.validateAdmin(req.user.githubAccessToken);
       req.session.authenticatedAt = new Date();
       return next();
+    } catch (error) {
+      req.logout();
+      req.session.destroy();
+      return res.forbidden({
+        message: 'You are not permitted to perform this action.',
+      });
     }
-    // else
-    return next();
   }
-  // else
-  return res.forbidden({
-    message: 'You are not permitted to perform this action. Are you sure you are logged in?',
-  });
+
+  return next();
 };
