855cfe691435aba60d42d6dcb6f90ec5a952cfeb
David Corwin <david.corwin@gsa.gov>
2020-06-04 14:27:15 -0700

    Update config, use deploy script

---
+++ .cloudgov/deploy.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+set -o pipefail
+
+CF_API="https://api.fr.cloud.gov"
+CF_ORGANIZATION="gsa-18f-federalist"
+
+curl -L -o cf7.deb 'https://packages.cloudfoundry.org/stable?release=debian64&version=v7&source=github' \
+  && sudo dpkg -i cf7.deb \
+  && rm cf7.deb
+
+cf7 api $CF_API
+
+echo "Logging in to $CF_ORGANIZATION org, $CF_SPACE space."
+cf7 login -u $CF_USERNAME -p $CF_PASSWORD -o $CF_ORGANIZATION -s $CF_SPACE
+
+echo "Deploying to $CF_SPACE space."
+cf7 push $CF_APP --strategy rolling --vars-file $CF_VARS_FILE -f $CF_MANIFEST
+
+cf7 logout
