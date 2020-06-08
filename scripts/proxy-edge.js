f0abdd05a43cdd0b961f436367ba1484ccde1af4
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2020-06-08 16:40:04 -0400

    remove comments

--- scripts/proxy-edge.js
+++ scripts/proxy-edge.js
@@ -2,12 +2,5 @@ const AWS = require('aws-sdk');
 const { saveSites, removeSite, saveSite } = require('../api/services/ProxyDataSync');
 const { Site } = require('../api/models');
  
-// let site;
 Site.findAll({ attributes: ['id', 'owner', 'repository', 'awsBucketName', 'awsBucketRegion', 's3ServiceName'] })
 	.then(saveSites);
-	// .then((sites) => {
-	// 	site = sites[0];
-	// 	console.log(`\n\nremove site:\t${JSON.stringify(site)}\n\n`)
-	// 	return removeSite(site)
-	// })
-	// .then(() => saveSite(site));
