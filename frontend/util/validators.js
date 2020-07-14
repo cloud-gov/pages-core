a224ff066161d1afa1383d229ee149e3a9b2a46b
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2020-07-14 15:37:22 -0400

    Update frontend/util/validators.js
    Co-authored-by: David Corwin <david.corwin@gsa.gov>

--- frontend/util/validators.js
+++ frontend/util/validators.js
@@ -4,7 +4,7 @@ const validBranchName = s => /^[\w._]+(?:[/-]*[\w._])*$/.test(s);
 
 const validBasicAuthUsername = s => /^(?!.*[:])(?=.*[a-zA-Z0-9]).{4,255}$/.test(s) ? undefined : 'Value is required and must be at least 4 characters. Allowed symbols: .-_@';
 
-const validBasicAuthPassword = s => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,255}$/.test(s) ? undefined : 'Value is required and must be at least 4 characters. At leaset 1 uppercase, 1 lowercase and 1 number is required';
+const validBasicAuthPassword = s => /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,255}$/.test(s) ? undefined : 'Value is required and must be at least 4 characters. At least 1 uppercase, 1 lowercase and 1 number is required';
 
 function isValidYaml(yamlString) {
   try {
