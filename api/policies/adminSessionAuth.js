9b3c999c3717af4b4233feccfdd1aa22813e8703
David Corwin <david.corwin@gsa.gov>
2020-07-17 15:04:04 -0700

    Restore session auth

--- api/policies/adminSessionAuth.js
+++ api/policies/adminSessionAuth.js
@@ -5,20 +5,20 @@ module.exports = async function adminSessionAuth(req, res, next) {
   req.session.authRedirectPath = undefined;
 
   // Commenting out to test sessionless token auth
-  // if (!req.session.adminAuthenticated) {
-  //   return res.forbidden({
-  //     message: 'You are not permitted to perform this action. Are you sure you are logged in?',
-  //   });
-  // }
+  if (!req.session.adminAuthenticated) {
+    return res.forbidden({
+      message: 'You are not permitted to perform this action. Are you sure you are logged in?',
+    });
+  }
 
   // Grab token header for sessionless testing
-  const token = req.header('token');
+  // const token = req.header('token');
 
   const lastAuthenticatedAt = req.session.adminAuthenticatedAt;
   if (!lastAuthenticatedAt || utils.isPastAuthThreshold(lastAuthenticatedAt)) {
     // check that the adminAuthenticated user is still an admin member
     try {
-      await GitHub.validateAdmin(token);
+      await GitHub.validateAdmin(req.user.githubAccessToken);
       req.session.adminAuthenticatedAt = new Date();
       return next();
     } catch (error) {
