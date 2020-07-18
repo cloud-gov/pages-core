7406392d6e3df5affadc0ef072a80bcad5b17074
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2020-07-17 20:40:16 -0400

    update proxydb item basicAuth to have capital username and passwork keys and add subdomain to proxyDB sync script

--- scripts/proxy-edge.js
+++ scripts/proxy-edge.js
@@ -2,6 +2,6 @@ const { saveSites } = require('../api/services/ProxyDataSync');
 const { Site } = require('../api/models');
 
 Site.findAll({
-  attributes: ['id', 'owner', 'repository', 'awsBucketName', 'awsBucketRegion', 's3ServiceName', 'updatedAt', 'config'],
+  attributes: ['id', 'owner', 'repository', 'awsBucketName', 'awsBucketRegion', 's3ServiceName', 'updatedAt', 'config', 'subdomain'],
 })
   .then(saveSites);
