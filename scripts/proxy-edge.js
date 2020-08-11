9eea63f3cc0ea04258d74874603b98d4fcf2baa9
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2020-08-11 18:25:55 -0400

    sync proxy table records one-by-one incase there is an issue with one record not all fail to sync - also displays more  meaningful error

--- scripts/proxy-edge.js
+++ scripts/proxy-edge.js
@@ -1,7 +1,11 @@
-const { saveSites } = require('../api/services/ProxyDataSync');
+const { saveSite } = require('../api/services/ProxyDataSync');
 const { Site } = require('../api/models');
 
 Site.findAll({
   attributes: ['id', 'owner', 'repository', 'awsBucketName', 'awsBucketRegion', 's3ServiceName', 'updatedAt', 'config', 'subdomain'],
 })
-  .then(saveSites);
+  .then(sites => Promise.all(sites.map(site => saveSite(site))))
+  .catch(err => {
+  	console.error(err);
+  	throw err;
+  });
