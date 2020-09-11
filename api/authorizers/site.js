2ca394ee27ec2ffc2829fe14b3ed182cca701843
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2020-09-11 11:20:34 -0400

    allow federalist operators to delete a site

--- api/authorizers/site.js
+++ api/authorizers/site.js
@@ -1,6 +1,7 @@
 const GitHub = require('../services/GitHub');
 const siteErrors = require('../responses/siteErrors');
 const { User, Site } = require('../models');
+const FederalistUsersHelper = require('../services/FederalistUsersHelper');
 
 const authorize = ({ id }, site) => (
   User.findByPk(id, { include: [Site] })
@@ -39,6 +40,23 @@ const authorizeAdmin = (user, site) => (
     })
 );
 
+const authorizeFederalistUsersAdmin = (user) =>
+  FederalistUsersHelper.federalistUsersAdmins(user.username)
+    .then((admins) => {
+      if (!admins.includes(user.username)) {
+        return Promise.reject({
+          message: siteErrors.ADMIN_ACCESS_REQUIRED,
+          status: 403,
+        });
+      }
+    })
+    .catch((error) => {
+      return Promise.reject({
+        message: siteErrors.ADMIN_ACCESS_REQUIRED,
+        status: 403,
+      });
+    });
+
 // create is allowed for all
 const create = () => Promise.resolve();
 const addUser = () => Promise.resolve();
@@ -51,10 +69,13 @@ const findOne = (user, site) => authorize(user, site);
 
 const update = (user, site) => authorize(user, site);
 
-const destroy = (user, site) => (
-  authorize(user, site)
+const destroy = (user, site) => {
+  return authorize(user, site)
     .then(() => authorizeAdmin(user, site))
-);
+    .catch(() => {
+      return authorizeFederalistUsersAdmin(user);
+    });
+}
 
 const removeUser = (user, site) => authorize(user, site);
 
--- test/api/unit/authorizers/site.test.js
+++ test/api/unit/authorizers/site.test.js
@@ -2,9 +2,11 @@ const crypto = require("crypto")
 const expect = require("chai").expect
 const factory = require("../../support/factory")
 const nock = require('nock');
+const sinon = require('sinon');
 const githubAPINocks = require('../../support/githubAPINocks');
 const authorizer = require("../../../../api/authorizers/site.js")
 const siteErrors = require('../../../../api/responses/siteErrors');
+const FederalistUsersHelper = require('../../../../api/services/FederalistUsersHelper');
 
 describe("Site authorizer", () => {
   describe(".create(user, params)", () => {
@@ -42,8 +44,6 @@ describe("Site authorizer", () => {
 
       Promise.props({ user, site }).then(({ user, site }) => {
         return authorizer.findOne(user, site)
-      }).then(() => {
-        done(new Error("Expected authorization error"))
       }).catch(err => {
         expect(err).to.equal(403)
         done()
@@ -79,60 +79,90 @@ describe("Site authorizer", () => {
   })
 
   describe(".destroy(user, site)", () => {
+    let stub;
+    beforeEach(() => {
+       stub = sinon.stub(FederalistUsersHelper, 'federalistUsersAdmins');
+    })
+    afterEach(() => {
+      stub.restore();
+    });
     it("should resolve if the user is associated with the site", done => {
       const user = factory.user();
       const site = factory.site({ users: Promise.all([user]) });
       nock.cleanAll();
-      githubAPINocks.repo({
-        owner: site.owner,
-        repository: site.repo,
-        response: [200, {
-          permissions: { admin: true, push: true },
-        }],
-      });
 
-      Promise.props({ user, site }).then(({ user, site }) => {
+      Promise.props({ user, site })
+      .then(({ user, site }) => {
+        githubAPINocks.repo({
+          owner: site.owner,
+          repository: site.repo,
+          response: [200, {
+            permissions: { admin: true, push: true },
+          }],
+        });
         return authorizer.destroy(user, site)
       }).then(() => {
         done()
-      }).catch(done)
+      });
     })
 
     it("should reject if the user is not associated with the site", done => {
       const user = factory.user();
       const site = factory.site();
       nock.cleanAll();
-      githubAPINocks.repo({
-        owner: site.owner,
-        repository: site.repo,
-        response: [200, {
-          permissions: { admin: true, push: true },
-        }],
-      });
+      stub.rejects();
 
-      Promise.props({ user, site }).then(({ user, site }) => {
-        return authorizer.destroy(user, site)
-      }).then(() => {
-        done(new Error('Expected authorization error'));
-      }).catch((err) => {
-        expect(err).to.equal(403);
+      Promise.props({ user, site })
+      .then(({ user, site }) => {
+        githubAPINocks.repo({
+          owner: site.owner,
+          repository: site.repo,
+          response: [200, {
+            permissions: { admin: true, push: true },
+          }],
+        });
+        return authorizer.destroy(user, site);
+      })
+      .catch((err) => {
+        expect(err.status).to.equal(403);
         done();
-      }).catch(done)
+      });
     })
+
+    it("should accept if user is not assoc with the site but is feralist-users admin", done => {
+      const user = factory.user();
+      const site = factory.site();
+      nock.cleanAll();
+
+      Promise.props({ user, site })
+      .then(({ user, site }) => {
+        stub.resolves([user.username]);
+        githubAPINocks.repo({
+          owner: site.owner,
+          repository: site.repo,
+          response: [200, {
+            permissions: { admin: true, push: true },
+          }],
+        });
+        return authorizer.destroy(user, site);
+      })
+      .then(done);
+    });
 
     it("should reject if the user is associated with the site but not an admin", done => {
       const user = factory.user()
       const site = factory.site({ users: Promise.all([user]) })
       nock.cleanAll();
-      githubAPINocks.repo({
-        owner: site.owner,
-        repository: site.repo,
-        response: [200, {
-          permissions: { admin: false, push: true },
-        }],
-      });
+      stub.rejects();
 
       Promise.props({ user, site }).then(({ user, site }) => {
+        githubAPINocks.repo({
+          owner: site.owner,
+          repository: site.repo,
+          response: [200, {
+            permissions: { admin: false, push: true },
+          }],
+        });
         return authorizer.destroy(user, site)
       }).then(() => {
         done(new Error("Expected authorization error"))
@@ -147,14 +177,14 @@ describe("Site authorizer", () => {
       const user = factory.user()
       const site = factory.site({ users: Promise.all([user]) })
       nock.cleanAll();
-      githubAPINocks.repo({
-        owner: site.owner,
-        repository: site.repo,
-        response: [404, {}],
-      });
 
       Promise.props({ user, site })
       .then(({ user, site }) => {
+        githubAPINocks.repo({
+          owner: site.owner,
+          repository: site.repo,
+          response: [404, {}],
+        });
         return authorizer.destroy(user, site)
       }).then(() => {
         done()
@@ -165,18 +195,18 @@ describe("Site authorizer", () => {
       const user = factory.user()
       const site = factory.site({ users: Promise.all([user]) })
       nock.cleanAll();
-      githubAPINocks.repo({
-        owner: site.owner,
-        repository: site.repo,
-        response: [400, {}],
-      });
+      stub.resolves([]);
 
       Promise.props({ user, site })
       .then(({ user, site }) => {
+        githubAPINocks.repo({
+          owner: site.owner,
+          repository: site.repo,
+          response: [400, {}],
+        });
         return authorizer.destroy(user, site)
-      }).then(() => {
-        done(new Error("Expected authorization error"))
-      }).catch(err => {
+      })
+      .catch(err => {
         expect(err.status).to.equal(403)
         expect(err.message).to.equal(siteErrors.ADMIN_ACCESS_REQUIRED)
         done()
