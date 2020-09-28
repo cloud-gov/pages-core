349f6eb19c71dc51b866683322e920151f405e2a
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2020-09-28 18:20:58 -0400

    update indexes for event table

--- migrations/20200917163300-create-events.js
+++ migrations/20200917163300-create-events.js
@@ -8,11 +8,8 @@ exports.up = (db, callback) => db.createTable('event', {
   body: { type: 'jsonb', defaultValue: '{}' },
 })
   .then(() => Promise.all([
-    db.runSql('CREATE INDEX IF NOT EXISTS "event_type_idx" ON event USING btree ("type");'),
-    db.runSql('CREATE INDEX IF NOT EXISTS "label" ON event USING btree ("label");'),
-    db.runSql('CREATE INDEX IF NOT EXISTS "model" ON event USING btree ("model");'),
-    db.runSql('CREATE INDEX IF NOT EXISTS "modelId" ON event USING btree ("modelId");'),
-    db.runSql('CREATE INDEX IF NOT EXISTS "idxEventBody" ON event USING gin ("body");'),
+    db.runSql('CREATE INDEX IF NOT EXISTS event_model_model_id_idx ON event (model, "modelId");'),
+    db.runSql('CREATE INDEX IF NOT EXISTS event_body_idx ON event USING gin (body);'),
   ]))
   .then(() => callback())
   .catch(callback);
