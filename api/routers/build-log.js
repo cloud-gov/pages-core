25bd5da0c8d44c30d70401e3bcb6988042c8c10c
David Corwin <david.corwin@gsa.gov>
2020-11-12 09:49:08 -0800

    Clean it up

--- api/routers/build-log.js
+++ api/routers/build-log.js
@@ -3,6 +3,5 @@ const BuildLogController = require('../controllers/build-log');
 const sessionAuth = require('../policies/sessionAuth');
 
 router.get('/build/:build_id/log(/page/:page)?', sessionAuth, BuildLogController.find);
-router.post('/build/:build_id/log/:token', BuildLogController.create);
 
 module.exports = router;
