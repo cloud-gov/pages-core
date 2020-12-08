6204708e0f906c9359988c99b32e85580ea18dcc
David Corwin <david.corwin@gsa.gov>
2020-12-08 09:06:09 -0800

    Update api/routers/auth.js
    Co-authored-by: Amir Reavis-Bey <amir.reavis-bey@gsa.gov>

--- api/routers/auth.js
+++ api/routers/auth.js
@@ -7,7 +7,7 @@ const idp = config.env.authIDP;
 const opts = {
   failureRedirect: '/',
   failureFlash:
-    'Apologies; you don\'t have access to Federalist! Please contact the Federalist team if this is in error.',
+    'Apologies; you are not authorized to access Federalist! Please contact the Federalist team if this is in error.',
 };
 
 function redirectIfAuthenticated(req, res, next) {
