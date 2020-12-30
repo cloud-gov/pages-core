05fab9afdcf81e0e9a8d4f540b3e489f304f96a1
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2020-12-30 15:53:19 -0500

    create events for handler errors

--- api/admin/controllers/site.js
+++ api/admin/controllers/site.js
@@ -1,10 +1,10 @@
 const { Op } = require('sequelize');
-const { Site } = require('../../models');
+const { Site, Event } = require('../../models');
 const SiteDestroyer = require('../../services/SiteDestroyer');
 const { fetchModelById } = require('../../utils/queryDatabase');
 const { pick, toInt, wrapHandlers } = require('../../utils');
 const { serializeNew, serializeMany } = require('../../serializers/site');
-const { logger } = require('../../../winston');
+const EventCreator = require('../../services/EventCreator');
 
 const updateableAttrs = [
   'containerConfig',
@@ -72,8 +72,15 @@ module.exports = wrapHandlers({
       await SiteDestroyer.destroySite(site);
       return res.json(serializeNew(site, true));
     } catch (error) {
-      // Todo - make use of the future audit event
-      logger.error([`site@id=${site.id}`, error.message, error.stack].join('\n\n'));
+      const errorBody = {
+        request: {
+          params: req.params,
+          path: req.patth,
+        },
+        error: error.stack,
+        message: 'Error encountered while destroying site',
+      };
+      EventCreator.error(Event.labels.ADMIN, errorBody);
       return res.error(error);
     }
   },
