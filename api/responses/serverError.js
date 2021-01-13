20abd71446ccdbc6b9174bbe3796ed4e6134eb4f
David Corwin <david.corwin@gsa.gov>
2021-01-13 14:55:33 -0800

    WIP

--- api/responses/serverError.js
+++ api/responses/serverError.js
@@ -1,9 +1,4 @@
-const { logger } = require('../../winston');
-
 module.exports = (error = {}, { res }) => {
-  // TODO - remove when refactoring error handling/logging
-  logger.error('Sending 500: ', error);
-
   res.status(500);
   return res.json({
     message: 'An unexpected error occurred',
--- api/services/EventCreator.js
+++ api/services/EventCreator.js
@@ -1,71 +1,42 @@
 const { logger } = require('../../winston');
 const { Event } = require('../models');
 
-const createEvent = async (obj) => {
-  let event;
-  try {
-    const {
-      label, type, model, body,
-    } = obj;
-    let modelId;
-    let modelName;
-
-    if (model) {
-      modelId = model.id;
-      modelName = model.constructor.name;
-    }
-
-    const atts = {
-      label,
-      type,
-      model: modelName,
-      modelId,
-      body,
-    };
-
-    event = await Event.create(atts);
-    if (event.type === Event.types.ERROR) {
-      logger.error(JSON.stringify(event));
-    } else if (event.type === Event.types.WARN) {
-      logger.warn(JSON.stringify(event));
-    } else {
-      logger.info(JSON.stringify(event));
-    }
-  } catch (err) {
-    logger.warn([`Failed to create Event(${JSON.stringify(obj)}`, err.stack].join('\n'));
-  }
-  return event;
-};
+const createEvent = (obj) => Event
+  .create(obj)
+  .catch((err) => {
+    logger.error([`Failed to create Event(${JSON.stringify(obj)}`, err.stack].join('\n'));
+  });
 
 const audit = (label, model, message, body = {}) => createEvent({
   type: Event.types.AUDIT,
   label,
-  model,
-  body: { ...body, message },
+  model: model.constructor.name,
+  modelId: model.id,
+  body: {
+    message,
+    ...body,
+  },
 });
 
 const error = (label, err, body = {}) => createEvent({
   type: Event.types.ERROR,
   label,
   body: {
-    ...{}, error: err.stack, message: err.message, ...body,
+    error: err.stack,
+    message: err.message,
+    ...body,
   },
 });
 
-const warn = (label, message, err, body = {}) => {
-  if (err) {
-    body.err = err.stack; // eslint-disable-line no-param-reassign
-    body.message = err.message; // eslint-disable-line no-param-reassign
-  }
-  if (message && message.length) {
-    body.message = message; // eslint-disable-line no-param-reassign
-  }
-  return createEvent({
-    type: Event.types.WARNING,
-    label,
-    body,
-  });
-};
+const warn = (label, err, body = {}) => createEvent({
+  type: Event.types.WARNING,
+  label,
+  body: {
+    error: err.stack,
+    message: err.message,
+    ...body,
+  },
+});
 
 const handlerError = async (request, err) => {
   const { path, params, body } = request;
