4f5392176a6a869f706d2c07ee4c4b55da01071a
David Corwin <david.corwin@gsa.gov>
2021-02-10 10:24:44 -0800

    Only report build status to Github is requestedCommitSha is present

--- api/controllers/build.js
+++ api/controllers/build.js
@@ -147,8 +147,12 @@ module.exports = wrapHandlers({
     await build.updateJobStatus(buildStatus);
 
     emitBuildStatus(build);
-
-    await GithubBuildHelper.reportBuildStatus(build);
+    
+    // The `requestedCommitSha` will not be present for initial builds
+    // and there is no need to report status to Github
+    if (build.requestedCommitSha) {
+      await GithubBuildHelper.reportBuildStatus(build);
+    }
 
     if (Features.enabled(Features.Flags.FEATURE_PROXY_EDGE_DYNAMO)) {
       if (buildStatus.status === Build.States.Success) {
