484952a030c0821af6cfd2bd5270059cd5db84bd
David Corwin <david.corwin@gsa.gov>
2021-03-08 12:04:06 -0800

    Initial test

---
+++ .concourse/test-api.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cd app
+
+curl -L https://codeclimate.com/downloads/test-reporter/test-reporter-latest-linux-amd64 > ./cc-test-reporter
+chmod +x ./cc-test-reporter
+./cc-test-reporter before-build
+
+yarn install
+status="$(yarn test:cover)"
+
+# Combine the test coverage reports in coverage/server/ and coverage/client/
+./cc-test-reporter format-coverage -t lcov -o coverage/codeclimate.client.json coverage/client/lcov.info
+./cc-test-reporter format-coverage -t lcov -o coverage/codeclimate.server.json coverage/server/lcov.info
+./cc-test-reporter sum-coverage coverage/codeclimate.*.json
+# Attempt to submit a report, but don't fail the build if this fails (`|| true`)
+./cc-test-reporter upload-coverage || true
+
+exit "$status"
