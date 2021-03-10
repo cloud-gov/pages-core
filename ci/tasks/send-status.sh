2baaee31d300d2588528cf20da1d505a38c068f8
David Corwin <david.corwin@gsa.gov>
2021-03-09 20:30:17 -0800

    testing

--- ci/tasks/send-status.sh
+++ ci/tasks/send-status.sh
@@ -2,9 +2,11 @@
 
 set -e
 
+build_url=$ATC_EXTERNAL_URL/teams/$BUILD_TEAM_NAME/pipelines/$BUILD_PIPELINE_NAME/jobs/$BUILD_JOB_NAME/builds/$BUILD_NAME
+
 curl \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
-  -d '{"state": "'"$GITHUB_STATE"'", "context": "concourse", "target_url": "'"$BUILD_URL"'"}' \
+  -d '{"state": "'"$GITHUB_STATE"'", "context": "concourse", "target_url": "'"$build_url"'"}' \
   "https://api.github.com/repos/18f/federalist/statuses/$(git rev-parse HEAD)"
