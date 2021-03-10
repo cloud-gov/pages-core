cd48608c3f89a64f604c71a3a2768ede87fb2589
David Corwin <david.corwin@gsa.gov>
2021-03-09 20:25:59 -0800

    testing

--- ci/tasks/send-status.sh
+++ ci/tasks/send-status.sh
@@ -6,5 +6,5 @@ curl \
   -X POST \
   -H "Accept: application/vnd.github.v3+json" \
   -H "Authorization: token $GITHUB_ACCESS_TOKEN" \
-  -d '{"state": "'$GITHUB_STATE'", "context": "concourse", "target_url": "'$BUILD_URL'"}' \
-  https://api.github.com/repos/18f/federalist/statuses/$(git rev-parse HEAD)
+  -d '{"state": "'"$GITHUB_STATE"'", "context": "concourse", "target_url": "'"$BUILD_URL"'"}' \
+  "https://api.github.com/repos/18f/federalist/statuses/$(git rev-parse HEAD)"
