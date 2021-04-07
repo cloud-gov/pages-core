3a1238e824ccdf960adb3b68dcdb79138d207b3e
Andrew Burnes <andrew.burnes@gsa.gov>
2021-04-07 11:35:02 -0700

    Refactor components for siteList and TemplateSiteList

--- frontend/components/AddSite/TemplateSiteList/index.jsx
+++ frontend/components/AddSite/TemplateSiteList/index.jsx
@@ -5,6 +5,35 @@ import PropTypes from 'prop-types';
 import { ORGANIZATION } from '../../../propTypes';
 import TemplateSite from './templateSite';
 
+const templateGrid = (
+  activeChildId,
+  defaultOwner,
+  handleChooseActive,
+  handleSubmit,
+  orgData,
+  templates
+) => (
+  Object.keys(templates).map((templateKey, index) => {
+    const template = templates[templateKey];
+
+    return (
+      <div className="federalist-template-list" key={templateKey}>
+        <TemplateSite
+          templateKey={templateKey}
+          index={index}
+          thumb={template.thumb}
+          active={activeChildId}
+          handleChooseActive={handleChooseActive}
+          handleSubmit={handleSubmit}
+          defaultOwner={defaultOwner}
+          orgData={orgData}
+          {...template}
+        />
+      </div>
+    );
+  })
+);
+
 export class TemplateList extends React.Component {
   constructor(props) {
     super(props);
@@ -26,32 +55,19 @@ export class TemplateList extends React.Component {
     const {
       defaultOwner, handleSubmitTemplate, orgData, templates,
     } = this.props;
-    const { activeChildId } = this.state;
-
-    const templateGrid = Object.keys(templates).map((templateKey, index) => {
-      const template = templates[templateKey];
-
-      return (
-        <div className="federalist-template-list" key={templateKey}>
-          <TemplateSite
-            templateKey={templateKey}
-            index={index}
-            thumb={template.thumb}
-            active={activeChildId}
-            handleChooseActive={this.handleChooseActive}
-            handleSubmit={handleSubmitTemplate}
-            defaultOwner={defaultOwner}
-            orgData={orgData}
-            {...template}
-          />
-        </div>
-      );
-    });
+    const { handleChooseActive, state: { activeChildId } } = this;
 
     return (
       <div>
         <h2>Or choose from one of our templates</h2>
-        {templateGrid}
+        {templateGrid(
+          activeChildId,
+          defaultOwner,
+          handleChooseActive,
+          handleSubmitTemplate,
+          orgData,
+          templates
+        )}
       </div>
     );
   }
