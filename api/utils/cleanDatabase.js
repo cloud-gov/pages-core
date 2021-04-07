9aa7f7b2e2b5f00a1eb65b4393af304a28a57ffa
David Corwin <david.corwin@gsa.gov>
2021-04-07 15:44:16 -0700

    Update dev data, add tests

--- api/utils/cleanDatabase.js
+++ api/utils/cleanDatabase.js
@@ -3,7 +3,7 @@ const { models } = require('../models').sequelize;
 function cleanDatabase() {
   const promises = Object.keys(models).map((name) => {
     const model = models[name];
-    const promise = model.destroy({ truncate: true, cascade: true });
+    const promise = model.destroy({ truncate: true, cascade: true, force: true });
 
     return promise;
   });
