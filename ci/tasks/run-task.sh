11bab668d8cb8c11a89cc58989cf6b4f8b8ee119
David Corwin <david.corwin@gsa.gov>
2021-04-08 15:12:24 -0700

    Add inputs to config and quote task command

--- ci/tasks/run-task.sh
+++ ci/tasks/run-task.sh
@@ -7,4 +7,4 @@ cf auth
 
 cf t -o $CF_ORG -s $CF_SPACE
 
-cf run-task $CF_APP_NAME --name $CF_TASK_NAME --command $CF_TASK_COMMAND
+cf run-task $CF_APP_NAME --name $CF_TASK_NAME --command "$CF_TASK_COMMAND"
