5297f858dc4a0cb28bf7ba4e43c7c0435e8986c3
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2021-05-28 14:58:36 -0400

    jobProcessors decoupled from worker

--- api/workers/scheduled.js
+++ api/workers/scheduled.js
@@ -1,135 +1,25 @@
 const { Worker, QueueScheduler, QueueEvents } = require('bullmq');
 const IORedis = require('ioredis');
-const moment = require('moment');
-const PromisePool = require('@supercharge/promise-pool');
-const { Op } = require('sequelize');
 const { logger } = require('../../winston');
-const { Build } = require('../models');
-const BuildLogs = require('../services/build-logs');
 const config = require('../../config');
-const TimeoutBuilds = require('../services/TimeoutBuilds');
-const ScheduledBuildHelper = require('../services/ScheduledBuildHelper');
-const RepositoryVerifier = require('../services/RepositoryVerifier');
 const { handleWorker, monitorQueue } = require('./utils');
-
-async function runTimeoutBuilds() {
-  const results = await TimeoutBuilds.timeoutBuilds();
-  const allBuildIds = results.map(r => r[0]);
-
-  const failedCancels = results.filter(([, result]) => result.status === 'rejected');
-
-  if (failedCancels.length === 0) {
-    return;
-  }
-
-  const details = failedCancels.map(([buildId, { reason }]) => `${buildId}: ${reason}`).join('\n');
-  throw new Error(`${failedCancels.length} build tasks could not be canceled:\n${details}`);
-}
-
-async function runArchiveBuildLogsDaily() {
-  const startDate = moment().subtract(1, 'days').startOf('day');
-  const endDate = startDate.clone().add(1, 'days');
-
-  logger.info(`Querying for all builds completed between ${startDate} and ${endDate}.`);
-
-  const builds = await Build.findAll({
-    attributes: ['id'],
-    where: {
-      completedAt: {
-        [Op.gte]: startDate.toDate(),
-        [Op.lt]: endDate.toDate(),
-      },
-    },
-  });
-
-  logger.info(`Found ${builds.length} builds.`);
-
-  const { errors } = await PromisePool
-    .withConcurrency(5)
-    .for(builds)
-    .process(({ id }) => BuildLogs.archiveBuildLogsForBuildId(id));
-
-  if (errors.length === 0) {
-    logger.info('All build logs archived successfully!');
-    return;
-  }
-
-  const errMsg = [
-    `Archive build logs for ${startDate.format('YYYY-MM-DD')} completed with the following errors:`,
-    errors.map(e => `  ${e.item.id}: ${e.message}`).join('\n'),
-  ].join();
-  logger.error(errMsg);
-  throw new Error(errMsg);
-}
-
-async function runNightlyBuilds() {
-    const results = await ScheduledBuildHelper.nightlyBuilds();
-
-    const successes = results
-    .filter(result => result.status === 'fulfilled')
-    .map(result => result.value);
-
-  const failures = results
-    .filter(result => result.status === 'rejected')
-    .map(result => result.reason);
-
-    const msg = [`Queued nightly builds with ${successes.length} successes and ${failures.length} failures.`];
-    if (successes.length) {
-      msg.push(`   Successes:\n      ${successes.join('\n      ')}`)
-    }
-    if (failures.length) {
-      msg.push(`   Failures:\n      ${failures.join('\n      ')}`);
-    }
-    
-    if(failures.length) {
-      logger.error(`Exiting with failed build errors.  ${msg.join('\n')}`);
-      throw new Error(msg.join('\n'));
-    }
-
-    logger.info(msg.join('\n'));
-}
-
-async function runVerifyRepos() {
-  const results = await RepositoryVerifier.verifyRepos();
-  const successes = results
-    .filter(result => result.status === 'fulfilled')
-    .map(result => result.value);
-
-  const failures = results
-    .filter(result => result.status === 'rejected')
-    .map(result => result.reason);
-
-  const msg = [`Repositories verified with ${successes.length} successes and ${failures.length} failures.`];
-  if (successes.length) {
-    msg.push(`   Successes:\n      ${successes.join('\n      ')}`)
-  }
-  if (failures.length) {
-    msg.push(`   Failures:\n      ${failures.join('\n      ')}`);
-  }
-  
-  if(failures.length) {
-    logger.error(`Exiting with failed repository verifications.  ${msg.join('\n')}`);
-    throw new Error(msg.join('\n'));
-  }
-
-  logger.info(msg.join('\n'));
-}
+const jobProcessor = require('./jobProcessor');
 
 async function processJob(job) {
   logger.info(`Processing Job: ${JSON.stringify(job)}`);
-  switch(job.name) {
+  switch (job.name) {
     case 'timeoutBuilds':
-      return this.runTimeoutBuilds();
+      return jobProcessor.runTimeoutBuilds();
     case 'archiveBuildLogsDaily':
-      return this.runArchiveBuildLogsDaily();
+      return jobProcessor.runArchiveBuildLogsDaily();
     case 'nightlyBuilds':
-      return this.runNightlyBuilds();
+      return jobProcessor.runNightlyBuilds();
     case 'verifyRepos':
-      return this.runVerifyRepos();
+      return jobProcessor.runVerifyRepos();
     default:
       throw new Error(`No processor found for job@name=${job.name}.`);
   }
-};
+}
 
 class ScheduledWorker {
   constructor(queueName = 'scheduled', { concurrency = 5 } = {}) {
@@ -148,8 +38,4 @@ class ScheduledWorker {
 module.exports = {
   ScheduledWorker,
   processJob,
-  runArchiveBuildLogsDaily,
-  runNightlyBuilds,
-  runTimeoutBuilds,
-  runVerifyRepos,
 };
--- api/workers/utils.js
+++ api/workers/utils.js
@@ -2,21 +2,21 @@ const { logger } = require('../../winston');
 
 function monitorQueue(queueEvents) {
   function log(job, err = null) {
-    if(err) {
+    if (err) {
       logger.error(`queue@name=${queueEvents.name} job=${JSON.stringify(job)}`, err);
       return;
     }
     logger.info(`queue@name=${queueEvents.name} job=${JSON.stringify(job)}`);
   }
 
-  queueEvents.on('completed', job => {
+  queueEvents.on('completed', (job) => {
     log(job);
   });
 
   queueEvents.on('failed', (job, failedReason) => {
     log(job, failedReason);
   });
-  queueEvents.on('waiting', job => {
+  queueEvents.on('waiting', (job) => {
     log(job);
   });
 
@@ -24,7 +24,7 @@ function monitorQueue(queueEvents) {
     log(job);
   });
 
-  queueEvents.on('progress', job => {
+  queueEvents.on('progress', (job) => {
     log(job);
   });
 
@@ -34,7 +34,7 @@ function monitorQueue(queueEvents) {
 }
 
 function handleWorker(worker) {
-  worker.on('error', ( err) => {
+  worker.on('error', (err) => {
     logger.error(`worker@name=${worker.QUEUE_NAME} error:`, err);
   });
 }
