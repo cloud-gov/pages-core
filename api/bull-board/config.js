d3ddfd92a8194503edcebea515bdf9cbdcbfc220
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2021-06-10 11:18:10 -0400

    use bull-board config file in production

--- api/bull-board/config.js
+++ api/bull-board/config.js
@@ -1,40 +1,34 @@
 const cfenv = require('cfenv');
-const apiConfig = require('../../config');
 
 const appEnv = cfenv.getAppEnv();
-
-if (process.env.NODE_ENV === 'production') { // only services needed for this bull-board in CF
   
-  const rdsCreds = appEnv.getServiceCreds(`federalist-${process.env.APP_ENV}-rds`);
-  if (rdsCreds) {
-    module.exports.postgres = {
-      database: rdsCreds.db_name,
-      host: rdsCreds.host,
-      user: rdsCreds.username,
-      password: rdsCreds.password,
-      port: rdsCreds.port,
-    };
-  } else {
-    throw new Error('No database credentials found.');
-  }
-
-  const redisCreds = appEnv.getServiceCreds(`federalist-${process.env.APP_ENV}-redis`);
-  if (redisCreds) {
-    module.exports.redis = {
-      url: redisCreds.uri,
-      tls: {},
-    };
-  } else {
-    throw new Error('No Redis credentials found');
-  }
-
-  const uaaCredentials = appEnv.getServiceCreds(`app-${process.env.APP_ENV}-uaa-client`);
-
-  module.exports.passport = {
-    uaa: {
-      options: uaaCredentials,
-    },
+const rdsCreds = appEnv.getServiceCreds(`federalist-${process.env.APP_ENV}-rds`);
+if (rdsCreds) {
+  module.exports.postgres = {
+    database: rdsCreds.db_name,
+    host: rdsCreds.host,
+    user: rdsCreds.username,
+    password: rdsCreds.password,
+    port: rdsCreds.port,
   };
 } else {
-  module.exports = apiConfig;
+  throw new Error('No database credentials found.');
 }
+
+const redisCreds = appEnv.getServiceCreds(`federalist-${process.env.APP_ENV}-redis`);
+if (redisCreds) {
+  module.exports.redis = {
+    url: redisCreds.uri,
+    tls: {},
+  };
+} else {
+  throw new Error('No Redis credentials found');
+}
+
+const uaaCredentials = appEnv.getServiceCreds(`app-${process.env.APP_ENV}-uaa-client`);
+
+module.exports.passport = {
+  uaa: {
+    options: uaaCredentials,
+  },
+};
