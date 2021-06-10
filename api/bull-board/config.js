16fcaf1b32ae83334450b0251677872d0a4cf74d
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2021-06-10 13:10:10 -0400

    stop passport and sessionConfig from using api config in production

--- api/bull-board/config.js
+++ api/bull-board/config.js
@@ -1,34 +1,43 @@
-const cfenv = require('cfenv');
+if (process.env.NODE_ENV === 'production') {
+  const cfenv = require('cfenv');
 
-const appEnv = cfenv.getAppEnv();
-  
-const rdsCreds = appEnv.getServiceCreds(`federalist-${process.env.APP_ENV}-rds`);
-if (rdsCreds) {
-  module.exports.postgres = {
-    database: rdsCreds.db_name,
-    host: rdsCreds.host,
-    user: rdsCreds.username,
-    password: rdsCreds.password,
-    port: rdsCreds.port,
+  const appEnv = cfenv.getAppEnv();
+
+  const rdsCreds = appEnv.getServiceCreds(`federalist-${process.env.APP_ENV}-rds`);
+  if (rdsCreds) {
+    module.exports.postgres = {
+      database: rdsCreds.db_name,
+      host: rdsCreds.host,
+      user: rdsCreds.username,
+      password: rdsCreds.password,
+      port: rdsCreds.port,
+    };
+  } else {
+    throw new Error('No database credentials found.');
+  }
+
+  const redisCreds = appEnv.getServiceCreds(`federalist-${process.env.APP_ENV}-redis`);
+  if (redisCreds) {
+    module.exports.redis = {
+      url: redisCreds.uri,
+      tls: {},
+    };
+  } else {
+    throw new Error('No Redis credentials found');
+  }
+
+  const uaaCredentials = appEnv.getServiceCreds(`app-${process.env.APP_ENV}-uaa-client`);
+
+  module.exports.passport = {
+    uaa: {
+      options: uaaCredentials,
+    },
   };
-} else {
-  throw new Error('No database credentials found.');
-}
 
-const redisCreds = appEnv.getServiceCreds(`federalist-${process.env.APP_ENV}-redis`);
-if (redisCreds) {
-  module.exports.redis = {
-    url: redisCreds.uri,
-    tls: {},
+  module.exports.app = {
+    hostname: process.env.APP_HOSTNAME || 'http://localhost:1338',
   };
+
 } else {
-  throw new Error('No Redis credentials found');
+  module.exports = require('../../config');
 }
-
-const uaaCredentials = appEnv.getServiceCreds(`app-${process.env.APP_ENV}-uaa-client`);
-
-module.exports.passport = {
-  uaa: {
-    options: uaaCredentials,
-  },
-};
