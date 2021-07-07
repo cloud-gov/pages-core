638b644c9126a3996784537157b43725f5e0ed1a
David Corwin <david.corwin@gsa.gov>
2021-07-07 14:14:19 -0700

    add option

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -8,6 +8,6 @@ cf auth
 
 cf t -o $CF_ORG -s $CF_SPACE
 
-CFVARS=$(env | grep '^CFVAR_' | sed 's/CFVAR_//')
+CFVARS=$(env | grep '^CFVAR_' | sed 's/CFVAR_/--var /')
 
 cf push $CF_APP_NAME -p $CF_PATH -f $CF_MANIFEST --strategy rolling --vars-file $CF_VARS_FILE $CFVARS
