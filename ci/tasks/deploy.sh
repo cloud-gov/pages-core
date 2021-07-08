1921775e554b0ca0ee75109db873d4e983e21080
David Corwin <david.corwin@gsa.gov>
2021-07-07 20:44:54 -0700

    interpolate w js, upgrade to node 16

--- ci/tasks/deploy.sh
+++ ci/tasks/deploy.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 set -e
-set +x
 
 cf api $CF_API
 cf auth
 
 cf t -o $CF_ORG -s $CF_SPACE
 
-CFVARS=$(env | grep '^CFVAR_' | sed 's/CFVAR_/--var /')
-
-cf push $CF_APP_NAME -p $CF_PATH -f $CF_MANIFEST --strategy rolling --vars-file $CF_VARS_FILE $CFVARS
+cf push $CF_APP_NAME \
+  --strategy rolling \
+  --path $CF_PATH \
+  --manifest $CF_MANIFEST \
+  --vars-file $CF_VARS_FILE
