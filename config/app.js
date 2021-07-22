35730f6a8ab5cf0f41751e033343e76ac2ceaf48
David Corwin <david.corwin@gsa.gov>
2021-07-22 14:12:43 -0700

    Update admin hostname for CORS to support pages

--- config/app.js
+++ config/app.js
@@ -1,4 +1,5 @@
 module.exports = {
+  adminHostname: process.env.ADMIN_HOSTNAME || 'http://localhost:3000',
   hostname: process.env.APP_HOSTNAME || 'http://localhost:1337',
   app_env: process.env.APP_ENV || 'development',
   homepageUrl: process.env.HOMEPAGE_URL || 'http://localhost:4000',
