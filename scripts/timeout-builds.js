09b2ff96aedfb37f9f9465ad06347d95c2295ad5
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2021-08-05 11:17:52 -0400

    add timoutbuilds app and update circle

---
+++ scripts/timeout-builds.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+const { timeoutBuilds } = require('../api/workers/jobProcessors/timeoutBuilds');
+
+timeoutBuilds()
+  .catch((err) => {
+    console.error(err);
+    process.exit(1);
+  });
