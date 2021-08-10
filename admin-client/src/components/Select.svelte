0be06c396c7c2bd109241f67e4779d25e0ebb018
Andrew Burnes <andrew.burnes@gsa.gov>
2021-08-10 13:59:46 -0700

    Add admin users role mgmt

---
+++ admin-client/src/components/Select.svelte
@@ -0,0 +1,43 @@
+<script>
+  export let action;
+  export let options = [];
+  export let onAction;
+  let updateable = false;
+
+  function handleSubmit() {
+    const selected = options.reduce(
+      (accumlated, current) => ({...accumlated, [current.name]: current.selected }), {});
+    return onAction(selected)
+  }
+
+  function enableUpdate() {
+    updateable = true
+  }
+
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  {#each options as opt}
+    <select
+      bind:value={opt.selected}
+      on:blur|preventDefault={enableUpdate}
+      on:change|preventDefault={enableUpdate}
+    >
+      {#each opt.values as value}
+        <option value={value.id}>{value.name}</option>
+      {/each}
+    </select>
+  {/each}
+  <button
+    class="
+      usa-button
+      usa-button--primary
+      padding-x-1
+      padding-y-05
+    "
+    disabled={!updateable}
+    type=submit
+  >
+    {action}
+  </button>
+</form>
--- admin-client/src/pages/user/Show.svelte
+++ admin-client/src/pages/user/Show.svelte
@@ -1,22 +1,32 @@
 <script>
-  import { router } from '../../stores';
-  import { fetchEvents, fetchUser, resendInvite } from '../../lib/api';
+  import { notification, router } from '../../stores';
+  import {
+    fetchEvents,
+    fetchRoles,
+    fetchUser,
+    resendInvite,
+    updateUserOrgRole,
+    removeUserOrgRole,
+  } from '../../lib/api';
   import { formatDateTime } from '../../helpers/formatter';
   import {
     Accordion,
     AccordionContent,
     Await,
+    DataTable,
     EventTable,
     GridContainer,
     LabeledItem,
     PageTitle,
+    Select,
   } from '../../components';
-  
+
   $: id = $router.params.id;
   $: userPromise = fetchUser(id);
   $: auditEventsPromise = fetchEvents({
     limit: 25, page: 1, type: 'audit', model: 'User', modelId: id,
   });
+  $: rolesPromise = fetchRoles();
 
   async function handleResendInvite(uaaEmail) {
     const { invite } = await resendInvite({ uaaEmail });
@@ -24,6 +34,38 @@
     if (invite) {
       // eslint-disable-next-line no-alert
       window.alert(`Resent invite for ${invite.email}: ${invite.link}`);
+    }
+  }
+
+  function handleUpdateUserOrgRole(organizationId) {
+    return async ({ roleId }) => {
+      const response = await updateUserOrgRole({ userId: id, organizationId, roleId });
+
+      if (response) {
+        notification.setSuccess('Organization role updated.');
+      }
+
+      if (!response) {
+        notification.setError('Error. Unable to update organization role.');
+      }
+
+      userPromise = fetchUser(id);
+    }
+  }
+
+  function handleRemoveUserOrgRole(organizationId) {
+    return async () => {
+      const response = await removeUserOrgRole({ userId: id, organizationId });
+
+      if (response) {
+        notification.setSuccess('Removed user\'s organization role.');
+      }
+
+      if (!response) {
+        notification.setError('Error. Unable to remove user\'s organization role.');
+      }
+
+      userPromise = fetchUser(id);
     }
   }
 </script>
@@ -55,7 +97,7 @@
         <div class="tablet:grid-col-fill padding-bottom-1">
           <LabeledItem label="uaa user id" value={user.UAAIdentity.userId} />
           <LabeledItem label="uaa id" value={user.UAAIdentity.uaaId} />
-          <LabeledItem label="uaa origin" value={user.UAAIdentity.origin} />        
+          <LabeledItem label="uaa origin" value={user.UAAIdentity.origin} />
           <LabeledItem label="uaa email" value={user.UAAIdentity.email} />
           <LabeledItem label="uaa username" value={user.UAAIdentity.username} />
         </div>
@@ -83,7 +125,58 @@
       {/if}
     </div>
 
-    <br/>
+    <Accordion multiselect bordered>
+      <Await on={rolesPromise} let:response={roles}>
+        <AccordionContent title="Organizations" expanded={true}>
+          {#if user.UAAIdentity}
+              <div class="grid-row">
+                <h3 class="padding-right-2">Add user to organization</h3>
+                <a
+                  class="usa-button usa-button--outline margin-y-2"
+                  href={`/users/new?uaaEmail=${user.UAAIdentity.email}`}>
+                  +
+                </a>
+              </div>
+          {/if}
+          <DataTable data={user.OrganizationRoles} borderless={true}>
+            <tr slot="header">
+              <th scope="col">Organization Name</th>
+              <th scope="col">Role</th>
+              <th scope="col">Action</th>
+            </tr>
+            <tr slot="item" let:item={orgRole}>
+              <td>
+                {orgRole.Organization.name}
+              </td>
+              <td>
+                <Select
+                  action="Update"
+                  options={[{
+                    name: 'roleId',
+                    selected: orgRole.Role.id,
+                    values: roles.data,
+                  }]}
+                  onAction={handleUpdateUserOrgRole(orgRole.Organization.id)}
+                />
+              </td>
+              <td>
+                <button
+                  class="
+                    usa-button
+                    usa-button--secondary
+                    padding-x-1
+                    padding-y-05
+                  "
+                  on:click|preventDefault={handleRemoveUserOrgRole(orgRole.Organization.id)}
+                >
+                  Remove
+                </button>
+              </td>
+            </tr>
+          </DataTable>
+        </AccordionContent>
+      </Await>
+    </Accordion>
 
     <Accordion multiselect bordered>
       <AccordionContent title="Recent Audit Activity" expanded={true}>
