b68f3b4a3704e106f59f5686adf3d1de20aa7051
Andrew Burnes <andrew.burnes@gsa.gov>
2021-08-12 11:03:58 -0700

    Switch to add/removeIndex method for constraint

--- migrations/20210812144929-unique-uaa-id.js
+++ migrations/20210812144929-unique-uaa-id.js
@@ -1,10 +1,7 @@
 const TABLE = 'uaa_identity';
 const COLUMN = 'userId';
+const INDEX_NAME = `${TABLE}_${COLUMN}_key`;
 
-exports.up = async db => db.changeColumn(TABLE, COLUMN, {
-  unique: true
-});
+exports.up = async db => db.addIndex(TABLE, INDEX_NAME, [COLUMN], true);
 
-exports.down = async db => db.changeColumn(TABLE, COLUMN, {
-  unique: false
-});
+exports.down = async db => db.removeIndex(TABLE, INDEX_NAME);
