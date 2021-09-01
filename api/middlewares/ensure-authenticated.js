a1f97f5528abfa71e61e2de8a33685c2e46c851d
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2021-09-01 12:09:23 -0400

    only show unauth message with user instructions for non-admin apps

--- api/middlewares/ensure-authenticated.js
+++ api/middlewares/ensure-authenticated.js
@@ -1,5 +1,10 @@
+const { adminHostname } = require('../../config').app;
+
 const ensureAuthenticated = (req, res, next) => {
   if (!req.isAuthenticated || !req.isAuthenticated()) {
+    if (req.hostname !== adminHostname) {
+      return res.unauthorized({ message: 'Unauthorized. If you require access to Federalist, please click <a href="https://federalist.18f.gov/documentation/access-permissions/#personal-access">here</a> to view our documentation.'})
+    }
     return res.unauthorized();
   }
   return next();
--- api/responses/unauthorized.js
+++ api/responses/unauthorized.js
@@ -1,7 +1,7 @@
 module.exports = (error = {}, { res }) => {
   res.status(401);
   return res.json({
-    message: error.message || 'Unauthorized. If you require access to Federalist, please click <a href="https://federalist.18f.gov/documentation/access-permissions/#personal-access">here</a> to view our documentation.',
+    message: error.message || 'Unauthorized',
     status: 401,
   });
 };
