e6ce953bac21fecba57925ac4ddc21dfdb270feb
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2021-09-01 11:33:25 -0400

    more detailed unauthorized error

--- api/responses/unauthorized.js
+++ api/responses/unauthorized.js
@@ -1,7 +1,7 @@
 module.exports = (error = {}, { res }) => {
   res.status(401);
   return res.json({
-    message: error.message || 'Unauthorized. If you need access to Federalist, please message #federalist-support on Slack and include your GitHub username.',
+    message: error.message || 'Unauthorized. If you require access to Federalist, please click <a href="https://federalist.18f.gov/documentation/access-permissions/#personal-access">here</a> to view our documentation.',
     status: 401,
   });
 };
