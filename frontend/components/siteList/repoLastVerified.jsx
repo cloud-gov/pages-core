e642a7c7c3265b01c8cd3fa6ed685b274219c5b0
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2021-10-04 17:16:19 -0400

    site.repoLastVerified has default value

--- frontend/components/siteList/repoLastVerified.jsx
+++ frontend/components/siteList/repoLastVerified.jsx
@@ -1,22 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-
+import moment from 'moment';
 import { dateAndTime } from '../../util/datetime';
 
 const getRepoLastVerified = (site) => {
-  let msg = 'Repository not found';
-  if (site.repoLastVerified) {
-    const formattedBuildTime = dateAndTime(site.repoLastVerified);
-    msg += `. Last seen on ${formattedBuildTime}.`;
-  }
-  return msg;
+  const formattedBuildTime = dateAndTime(site.repoLastVerified);
+  return `Repository not found. Last seen on ${formattedBuildTime}.`;
 };
 
 const RepoLastVerified = ({ site, daysNotVerified = 5, userUpdated }) => {
-  const daysAgo = fromDate => (new Date() - new Date(fromDate)) / (24 * 60 * 60 * 1000);
-  const minutesAgo = fromDate => (new Date() - new Date(fromDate)) / (60 * 1000);
-  if ((daysAgo(site.repoLastVerified || site.createdAt) > daysNotVerified)
-    && (userUpdated && (minutesAgo(userUpdated) > 4))) { // user logged in 4 mins
+  const ago = (fromDate, timeUnit = 'days') => moment().diff(moment(fromDate), timeUnit);
+  if ((ago(site.repoLastVerified) > daysNotVerified)
+    && (userUpdated && (ago(userUpdated, 'minutes') > 4))) { // user logged in 4 mins
     return (
       <p className="repo-verification">
         {getRepoLastVerified(site)}
