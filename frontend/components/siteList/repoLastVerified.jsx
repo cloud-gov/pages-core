91b7c2220c746c8f3376c664b489cd83f0c91525
Amir Reavis-Bey <amir.reavis-bey@gsa.gov>
2021-10-05 15:00:28 -0400

    rename daysNotVerified to maxDaysUnverified

--- frontend/components/siteList/repoLastVerified.jsx
+++ frontend/components/siteList/repoLastVerified.jsx
@@ -7,10 +7,10 @@ const getRepoLastVerified = (site) => {
   return `Repository not found. Last seen on ${formattedBuildTime}.`;
 };
 
-const RepoLastVerified = ({ site, daysNotVerified, userUpdated }) => {
+const RepoLastVerified = ({ site, maxDaysUnverified, userUpdated }) => {
   const daysAgo = fromDate => (new Date() - new Date(fromDate)) / (24 * 60 * 60 * 1000);
   const minutesAgo = fromDate => (new Date() - new Date(fromDate)) / (60 * 1000);
-  if ((daysAgo(site.repoLastVerified) > daysNotVerified)
+  if ((daysAgo(site.repoLastVerified) > maxDaysUnverified)
     && (userUpdated && (minutesAgo(userUpdated) > 4))) { // user logged in 4 mins
     return (
       <p className="repo-verification">
@@ -27,13 +27,13 @@ RepoLastVerified.propTypes = {
     createdAt: PropTypes.string,
     userUpdated: PropTypes.string,
   }),
-  daysNotVerified: PropTypes.number,
+  maxDaysUnverified: PropTypes.number,
   userUpdated: PropTypes.string,
 };
 
 RepoLastVerified.defaultProps = {
   site: {},
-  daysNotVerified: 5,
+  maxDaysUnverified: 5,
   userUpdated: new Date().toISOString(),
 };
 
