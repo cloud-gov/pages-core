1d9182b7abf3ade5cec9e749b09e9c28cb108d34
David Corwin <david.corwin@gsa.gov>
2022-02-15 15:29:20 -0800

    Add Github auth for queues ui, refactor to help separate queues ui, use FederalistLocal

--- api/bull-board/index.js
+++ api/bull-board/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 const app = require('./app');
 const { logger } = require('./winston');
 
-const { PORT = 1338 } = process.env;
+const { PORT } = process.env;
 
 async function run() {
   http.createServer(app).listen(PORT, () => {
