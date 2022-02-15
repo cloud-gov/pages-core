c17cc9f5ea6604282908b9d4a9a8bff00526e432
David Corwin <david.corwin@gsa.gov>
2022-02-15 15:29:20 -0800

    Add Github auth for queues ui, refactor to help separate queues ui, use FederalistLocal

---
+++ api/bull-board/cfEnv.js
@@ -0,0 +1,22 @@
+// Until this is refactored into a completely separate app and we can use a .profile file,
+// let's do this when running on CG
+const cfenv = require('cfenv');
+
+const {
+  APP_ENV,
+  PRODUCT,
+} = process.env;
+
+const appEnv = cfenv.getAppEnv();
+
+const redisCreds = appEnv.getServiceCreds(`${PRODUCT}-redis`);
+process.env.REDIS_URL = redisCreds.uri;
+process.env.REDIS_TLS = {};
+
+const uaaCredentials = appEnv.getServiceCreds(`app-${APP_ENV}-uaa-client`);
+process.env.UAA_CLIENT_ID = uaaCredentials.clientID;
+process.env.UAA_CLIENT_SECRET = uaaCredentials.clientSecret;
+
+const githubCredentials = appEnv.getServiceCreds(`${PRODUCT}-${APP_ENV}-github-queues-ui`);
+process.env.GITHUB_CLIENT_ID = githubCredentials.GITHUB_CLIENT_ID;
+process.env.GITHUB_CLIENT_SECRET = githubCredentials.GITHUB_CLIENT_SECRET;
--- api/bull-board/sessionConfig.js
+++ api/bull-board/sessionConfig.js
@@ -1,11 +1,21 @@
 const { MemoryStore } = require('express-session');
-const sessionConfig = require('../../config/session');
+
+const {
+  NODE_ENV,
+  SESSION_SECRET,
+} = process.env;
 
 module.exports = {
-  ...sessionConfig,
+  cookie: {
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000,
+    secure: NODE_ENV === 'production',
+    sameSite: 'lax',
+  },
   name: 'federalist-bull-board.sid',
-  // I think we only need `name`, but adding `key` for legacy
-  key: 'federalist-bull-board.sid',
-  secret: `${sessionConfig.secret}bull-board`,
+  secret: SESSION_SECRET,
+  proxy: true,
+  resave: true,
+  saveUninitialized: true,
   store: new MemoryStore(),
 };
