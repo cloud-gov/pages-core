f9c08f054b8006a55b372cb14ee878437a3dbe84
David Corwin <david.corwin@gsa.gov>
2022-06-09 11:24:28 -0700

    fix doc link

--- api/services/mailer/templates/uaaInvite.js
+++ api/services/mailer/templates/uaaInvite.js
@@ -19,7 +19,7 @@ function uaaInvite({ link }) {
       ${link}
       </li>
       <li>
-        <strong>Read the documentation</strong> - After you register and log in, review the <a href="https://federalist.18f.gov/documentation/customer-responsibilities">customer responsibilities </a> <sup>[4]</sup>.
+        <strong>Read the documentation</strong> - After you register and log in, review the <a href="https://cloud.gov/pages/documentation/customer-responsibilities">customer responsibilities </a> <sup>[4]</sup>.
       </li>
       <li>
       <strong>Get started</strong> - <a href="https://pages.cloud.gov">cloud.gov Pages application </a> <sup>[5]</sup>.
@@ -38,7 +38,7 @@ function uaaInvite({ link }) {
       <li>[1] cloud.gov Pages: https://cloud.gov/pages</li>
       <li>[2] 21st Century IDEA: https://digital.gov/resources/21st-century-integrated-digital-experience-act</li>
       <li>[3] Accept your invite: ${link}</li>
-      <li>[4] Customer responsibilities: https://federalist.18f.gov/documentation/customer-responsibilities</li>
+      <li>[4] Customer responsibilities: https://cloud.gov/pages/documentation/customer-responsibilities</li>
       <li>[5] Get started: https://pages.cloud.gov</li>
     </ul>
   `);
