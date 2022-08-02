1dc49873f7baf8a108d6ba0a34f2e91e4b82cfe5
Drew Bollinger <drew.bollinger@gsa.gov>
2022-08-02 15:51:14 -0400

    fix nested lint

--- admin-client/src/helpers/formatter.js
+++ admin-client/src/helpers/formatter.js
@@ -4,8 +4,8 @@ import { format, formatRelative } from 'date-fns';
 export const formatDateTime = (date, relative = false) => {
   try {
     return relative
-    ? formatRelative(new Date(date), new Date())
-    : format(new Date(date), 'yyyy-MM-dd hh:mma')
+      ? formatRelative(new Date(date), new Date())
+      : format(new Date(date), 'yyyy-MM-dd hh:mma');
   } catch (error) {
     return 'N/A';
   }
