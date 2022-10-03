75ddadf3aee8d86f7a7bc4968d1e84de242318ce
Andrew Burnes <andrew.burnes@gsa.gov>
2022-10-03 14:14:05 -0700

    Update config/templates.js
    Co-authored-by: Drew Bollinger <drewbo@users.noreply.github.com>

--- config/templates.js
+++ config/templates.js
@@ -4,7 +4,7 @@ module.exports = {
     repo: 'pages-uswds-11ty',
     branch: 'main',
     engine: 'node.js',
-    example: 'https://uswds-11y.pages.cloud.gov',
+    example: 'https://uswds-11ty.pages.cloud.gov',
     title: '11ty - U.S. Web Design System v3.0',
     description: 'A flexible multi-page setup intended to introduce an agency or program. It offers an engaging Hero section for a current priority and a landing page layout designed to provide clarity and context for first time visitors. This template also offers navigation options for internal pages.',
     thumb: '/images/pages-uswds-v3-11ty-thumbnail.png',
