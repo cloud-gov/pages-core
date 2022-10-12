b92ca7561fbea36adfa706064ca7f093a4a48166
Drew Bollinger <drew.bollinger@gsa.gov>
2022-10-12 12:55:05 -0400

    add webpack-bundle-analyzer, reduce webpack stats

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const autoprefixer = require('autoprefixer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { getFeatureFlags } = require('./webpack-utils');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
   mode: 'development',
@@ -13,6 +14,7 @@ module.exports = {
       directory: './dist',
     },
   },
+  stats: 'minimal',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
@@ -107,5 +109,6 @@ module.exports = {
       'APP_HOSTNAME',
       'PRODUCT',
     ]),
+    new BundleAnalyzerPlugin(),
   ],
 };
