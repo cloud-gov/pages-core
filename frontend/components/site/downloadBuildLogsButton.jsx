1402d4cb8b8ec212df0600e84434d4171eae22fb
Drew Bollinger <drew.bollinger@gsa.gov>
2022-12-16 14:58:35 -0500

    convert downloadBuildLogsButton to functional component

--- frontend/components/site/downloadBuildLogsButton.jsx
+++ frontend/components/site/downloadBuildLogsButton.jsx
@@ -4,23 +4,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import fileDownload from 'js-file-download';
 
-class DownloadBuildLogsButton extends React.Component {
-  constructor(props) {
-    super(props);
-    this.downloadBuildLogs = this.downloadBuildLogs.bind(this);
-  }
-
-  downloadBuildLogs() {
-    const { buildLogsData = [], buildId } = this.props;
+function DownloadBuildLogsButton(props) {
+  function downloadBuildLogs() {
+    const { buildLogsData = [], buildId } = props;
     const text = buildLogsData.map(source => `${source}\n`);
     fileDownload(text, `build-log-${buildId}.txt`);
   }
 
-  render() {
-    return (
-      <button type="button" className="usa-button" onClick={this.downloadBuildLogs}>Download logs</button>
-    );
-  }
+  return (
+    <button type="button" className="usa-button" onClick={downloadBuildLogs}>Download logs</button>
+  );
 }
 
 DownloadBuildLogsButton.propTypes = {
