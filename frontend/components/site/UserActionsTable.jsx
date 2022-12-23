8a862da5756046e652340bd95cfc499c58af9734
Drew Bollinger <drew.bollinger@gsa.gov>
2022-12-23 13:21:03 -0500

    convert UserActionsTable to functional component, update tests

--- frontend/components/site/UserActionsTable.jsx
+++ frontend/components/site/UserActionsTable.jsx
@@ -1,84 +1,64 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { USER_ACTION } from '../../propTypes';
+import React, { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from '@reach/router';
+
 import { timestampUTC } from '../../util/datetime';
 import userActions from '../../actions/userActions';
 
-class UserActionsTable extends React.Component {
-  componentDidMount() {
-    const { fetchUserActions, site } = this.props;
-    fetchUserActions(site);
-  }
-
-  renderRow(action) {
-    return (
-      <tr key={`${action.id}-${action.targetType}`}>
-        <td>{action.initiator.username}</td>
-        <td>{action.actionType.action}</td>
-        <td>{action.actionTarget.username}</td>
-        <td>{timestampUTC(action.createdAt)}</td>
-      </tr>
-    );
-  }
-
-  renderTableHead() {
-    return (
-      <thead>
-        <tr>
-          <th>
-            Initiator
-          </th>
-          <th>
-            Action
-          </th>
-          <th>
-            Target
-          </th>
-          <th>
-            Timestamp (UTC)
-          </th>
-        </tr>
-      </thead>
-    );
-  }
-
-  render() {
-    const { userActions: actions } = this.props;
-
-    if (!actions || !actions.length) {
-      return null;
-    }
-
-    return (
-      <table className="table-full-width log-table">
-        <caption>Action Log</caption>
-        {this.renderTableHead()}
-        <tbody>
-          {actions.map(this.renderRow)}
-        </tbody>
-      </table>
-    );
-  }
+function renderRow(action) {
+  return (
+    <tr key={`${action.id}-${action.targetType}`}>
+      <td>{action.initiator.username}</td>
+      <td>{action.actionType.action}</td>
+      <td>{action.actionTarget.username}</td>
+      <td>{timestampUTC(action.createdAt)}</td>
+    </tr>
+  );
 }
 
-UserActionsTable.propTypes = {
-  fetchUserActions: PropTypes.func.isRequired,
-  site: PropTypes.number.isRequired,
-  userActions: PropTypes.arrayOf(USER_ACTION),
-};
+function renderTableHead() {
+  return (
+    <thead>
+      <tr>
+        <th>
+          Initiator
+        </th>
+        <th>
+          Action
+        </th>
+        <th>
+          Target
+        </th>
+        <th>
+          Timestamp (UTC)
+        </th>
+      </tr>
+    </thead>
+  );
+}
 
-UserActionsTable.defaultProps = {
-  userActions: [],
-};
+function UserActionsTable() {
+  const { id } = useParams();
+  const actions = useSelector(state => state.userActions.data);
 
-const mapStateToProps = state => ({
-  userActions: state.userActions.data,
-});
+  useEffect(() => {
+    userActions.fetchUserActions(id);
+  }, []);
 
-const mapDispatchToProps = () => ({
-  fetchUserActions: userActions.fetchUserActions,
-});
+  if (!actions || !actions.length) {
+    return null;
+  }
+
+  return (
+    <table className="table-full-width log-table">
+      <caption>Action Log</caption>
+      {renderTableHead()}
+      <tbody>
+        {actions.map(renderRow)}
+      </tbody>
+    </table>
+  );
+}
 
 export { UserActionsTable };
-export default connect(mapStateToProps, mapDispatchToProps)(UserActionsTable);
+export default UserActionsTable;
