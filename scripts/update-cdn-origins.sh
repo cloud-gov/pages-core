12dc9a4df62df16a7defa4d0a186c4fc81e6ee82
Sven Aas <sven.aas@gsa.gov>
2023-01-17 12:02:37 -0500

    Initial work on script to update CDN origins for production domain services

---
+++ scripts/update-cdn-origins.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+display_usage() {
+  echo -e "\nUsage: $0 input.csv\n"
+}
+
+if [  $# -le 0 ]
+then
+    display_usage
+    exit 1
+fi
+
+if [[ ( $@ == "--help") ||  $@ == "-h" ]]
+then
+    display_usage
+    exit 0
+fi
+
+DOMAINS_FILE=$1
+# Expected input file is a comma-seperated-value export of the serviceName and
+# origin columns for provisioned domains in the domains table in the core DB.
+#
+# The expected format can be produced from the psql command line as follows:
+#
+# \copy (select "serviceName",origin from domain where state='provisioned') to '/output/path/for/domains.csv' CSV HEADER;
+
+# Waiting for service instance to finish being processed.
+wait_for_service_instance() {
+  local service_name=$1
+  local guid=$(cf service --guid $service_name)
+  local status=$(cf curl /v2/service_instances/${guid} | jq -r '.entity.last_operation.state')
+
+  while [ "$status" == "in progress" ]; do
+    sleep 60
+    status=$(cf curl /v2/service_instances/${guid} | jq -r '.entity.last_operation.state')
+  done
+}
+
+while IFS="," read -r service_instance current_origin
+do
+  if [[ $current_origin =~ (.*)\.app\.cloud\.gov$ ]]
+  then
+    bucket=${BASH_REMATCH[1]}
+    new_origin="$bucket.sites.pages.cloud.gov"
+
+    # For the moment, outputting instead of executing this command...
+    echo "cf update-service $service_instance -c '{\"origin\": \"$new_origin\"}'"
+
+    # ... and since we're not executing that we're not yet going to
+    # wait_for_service_instance $SERVICE_INSTANCE"
+
+    # Here's the SQL we'll need to run to update the database once the origin is updated
+    SQL="update domain set origin='$new_origin' where \"serviceName\"='$service_instance';"
+    echo $SQL
+    echo
+  fi
+done < $DOMAINS_FILE
