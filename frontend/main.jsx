152a825711167cbbb493d5592605709471a4680a
Drew Bollinger <drew.bollinger@gsa.gov>
2023-02-01 11:27:44 -0500

    upgrade to react 18

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -1,7 +1,7 @@
 /* global document:true */
 
 import '@babel/polyfill';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import React from 'react';
 import { Provider } from 'react-redux';
 import { createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom';
@@ -13,11 +13,13 @@ import './sass/styles.scss';
 
 const mainEl = document.querySelector('#js-app');
 
+const root = createRoot(mainEl);
+
 const router = createBrowserRouter(
   createRoutesFromElements(routes)
 );
 
-render((
+root.render((
   <Provider store={store}>
     <RouterProvider router={router} />
   </Provider>
