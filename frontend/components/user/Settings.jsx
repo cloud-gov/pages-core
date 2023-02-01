e20f02afec23993a85756259919bcbbbe6d34e71
Drew Bollinger <drew.bollinger@gsa.gov>
2023-02-01 12:14:53 -0500

    catch user settings load error

--- frontend/components/user/Settings.jsx
+++ frontend/components/user/Settings.jsx
@@ -37,7 +37,7 @@ const onGithubAuthFailure = (_error) => {
 function Settings({
   actions, organizations, sites, user,
 }) {
-  if (sites?.isLoading || organizations?.isLoading || !sites || !organizations) {
+  if (sites?.isLoading || organizations?.isLoading || !sites || !sites.data || !organizations) {
     return <LoadingIndicator />;
   }
 
