698d8a7b19334bbea199e52667e9f7cb68e81a62
Drew Bollinger <drew.bollinger@gsa.gov>
2023-02-24 16:17:04 -0500

    start of movement of site deletion to background queue job

---
+++ api/workers/jobProcessors/destroySite.js
@@ -0,0 +1,11 @@
+const SiteDestroyer = require('../../services/SiteDestroyer');
+const { logger } = require('../../../winston');
+
+async function destroySite() {
+  // const results = await SiteDestroyer.verifyRepos();
+  // create some message with the results
+  // log it
+  // logger.info(msg.join('\n'));
+}
+
+module.exports = destroySite;
