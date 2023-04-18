534a3d81f6f3b9953e9f201f87b4950455228ac2
Andrew Burnes <andrew.burnes@gsa.gov>
2023-04-18 12:48:32 -0700

    Format webhook info

--- admin-client/src/components/SiteFormWebhook.svelte
+++ admin-client/src/components/SiteFormWebhook.svelte
@@ -1,23 +1,42 @@
 <script>
   import Form from './Form.svelte';
+  import DataTable from './DataTable.svelte';
 
   export let site;
+  export let hooks = [];
   export let onSubmit;
   export let onSuccess;
   export let onFailure;
+
+  const hookData = hooks.map((hook) => ({
+    url: hook?.config?.url,
+    status: hook?.last_response?.status,
+    message: hook?.last_response?.message,
+  }));
 </script>
 
 <div class="grid-row">
+  <div class="grid-col-12">
+    <DataTable data={hookData} borderless>
+      <tr slot="header">
+        <th class="center">URL</th>
+        <th>Status</th>
+        <th>Message</th>
+      </tr>
+      <tr slot="item" let:item={hook}>
+        <td>{hook.url}</td>
+        <td>{hook.status}</td>
+        <td>{hook.message}</td>
+      </tr>
+    </DataTable>
+  </div>
   <div class="grid-col-8 grid-offset-4">
     <Form
-      action="Save"
+      action="Add Pages Webhook"
       onSubmit={() => onSubmit(site.id)}
       {onSuccess}
       {onFailure}
     >
-      <fieldset class="usa-fieldset">
-        <legend class="usa-legend usa-legend">Site Webhook</legend>
-      </fieldset>
     </Form>
   </div>
 </div>
