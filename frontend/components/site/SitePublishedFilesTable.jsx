d826b4ce1b165e9aa8b53574d85deb197c685e64
Sven Aas <sven.aas@gsa.gov>
2023-05-03 15:29:57 -0400

    Add another check to see whether publishedFiles.data has been initialized.

--- frontend/components/site/SitePublishedFilesTable.jsx
+++ frontend/components/site/SitePublishedFilesTable.jsx
@@ -127,7 +127,7 @@ function SitePublishedFilesTable() {
     }
   }, [publishedFiles]);
 
-  if (publishedFiles.isLoading) {
+  if (publishedFiles.isLoading || !publishedFiles.data) {
     return <LoadingIndicator />;
   }
 
