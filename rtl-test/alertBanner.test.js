a7a05c30731cdf466b0bd024dc595ccafa272ea2
Drew Bollinger <drew.bollinger@gsa.gov>
2023-08-11 15:35:48 -0400

    one test suite

--- rtl-test/alertBanner.test.js
+++ rtl-test/alertBanner.test.js
@@ -9,39 +9,34 @@ import AlertBanner from '../frontend/components/alertBanner';
 const statusTypes = ['error', 'info'];
 
 describe('<AlertBanner/>', () => {
-  test('outputs null when a message isn\'t supplied', () => {
+  test('it does not appear without a message', () => {
     render(<AlertBanner />);
     expect(screen.queryByRole('alert')).not.toBeInTheDocument();
   });
 
-  // statusTypes.forEach((status) => {
-  //   it(`outputs an ${status} banner when the status is ${status}`, () => {
-  //     const component = shallow(<AlertBanner message="hello" status={status} />);
+  statusTypes.forEach((status) => {
+    test(`it has the correct status when the status is ${status}`, () => {
+      render(<AlertBanner message="hello" status={status} />);
+      expect(screen.queryByRole('alert')).toHaveClass(`usa-alert-${status}`);
+    });
+  });
 
-  //     expect(component.find(`.usa-alert-${status}`).length).not.to.equal(0);
-  //   });
-  // });
+  test('it falls back to an info banner when status is not provided', () => {
+    render(<AlertBanner message="hello" />);
+    expect(screen.queryByRole('alert')).toHaveClass('usa-alert-info');
+  });
 
-  // it('falls back to an info banner when status is not provided', () => {
-  //   const component = shallow(<AlertBanner message="hello" />);
-  //   expect(component.find('.usa-alert-info').length).not.to.equal(0);
-  // });
+  test('it can render a component as a message', () => {
+    const componentText = 'Hey there';
+    const child = <button type="button">{componentText}</button>;
+    render(<AlertBanner message={child} />);
+    expect(screen.queryByRole('button')).toHaveTextContent(componentText);
+  });
 
-  // it('can render a component as a message', () => {
-  //   const componentText = 'Hey there';
-  //   const child = <h1>{componentText}</h1>;
-  //   const wrapper = shallow(<AlertBanner message={child} />);
-  //   const childInstance = wrapper.find('h1');
+  test('it can opt out of displaying `role="alert"`', () => {
+    render(<AlertBanner message="hi" alertRole={false} />);
 
-  //   expect(childInstance).to.have.length(1);
-  //   expect(childInstance.text()).to.equal(componentText);
-  // });
-
-  // it('can opt out of displaying `role="alert"`', () => {
-  //   const wrapperWithRole = shallow(<AlertBanner message="wow" />);
-  //   const wrapperWithoutRole = shallow(<AlertBanner message="hi" alertRole={false} />);
-
-  //   expect(wrapperWithRole.find('div').at(0).prop('role')).to.equal('alert');
-  //   expect(wrapperWithoutRole.find('div').at(0).prop('role')).to.equal(undefined);
-  // });
+    expect(screen.queryByRole('alert')).not.toBeInTheDocument();
+    expect(screen.queryByText('hi')).toBeInTheDocument();
+  });
 });
