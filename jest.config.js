eaac6c265a66b5908936de86b77664e94c237fc8
Drew Bollinger <drew.bollinger@gsa.gov>
2023-08-14 09:25:01 -0400

    fix svg mock resolution

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   moduleNameMapper: {
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-    'rtl-test/__mocks__/fileMock.js',
-    '\\.(css|less)$': 'rtl-test/__mocks__/styleMock.js',
+    '<rootDir>/rtl-test/__mocks__/fileMock.js',
+    '\\.(css|less)$': '<rootDir>/rtl-test/__mocks__/styleMock.js',
   },
+  testEnvironment: 'jsdom',
 };
