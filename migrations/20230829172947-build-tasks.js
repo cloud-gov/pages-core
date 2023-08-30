91e7b8aff74c681d80ff74cd8a073a8efa271f3d
Drew Bollinger <drew.bollinger@gsa.gov>
2023-08-30 11:39:44 -0400

    add build task table migration

---
+++ migrations/20230829172947-build-tasks.js
@@ -0,0 +1,55 @@
+
+const TYPE_TABLE_NAME = "build_task_type";
+const TYPE_TABLE_SCHEMA = {
+  id: { type: "int", primaryKey: true, autoIncrement: true },
+  name: { type: "string", notNull: true },
+  description: { type: "string", notNull: true },
+  metadata: { type: "jsonb", allowNull: true },
+};
+
+const TABLE_NAME = "build_task";
+const TABLE_INDEX_NAME = "build_task_build_id_type_index";
+const TABLE_SCHEMA = {
+  id: { type: "int", primaryKey: true, autoIncrement: true },
+  buildId: {
+    type: "int",
+    notNull: true,
+    foreignKey: {
+      name: "build_task_build_id_fk",
+      table: "build",
+      rules: {
+        onDelete: "RESTRICT",
+        onUpdate: "RESTRICT"
+      },
+      mapping: "id"
+    }
+  },
+  buildTaskTypeId: {
+    type: "int",
+    notNull: true,
+    foreignKey: {
+      name: "build_task_build_task_type_id_fk",
+      table: "build_task_type",
+      rules: {
+        onDelete: "RESTRICT",
+        onUpdate: "RESTRICT"
+      },
+      mapping: "id"
+    }
+  },
+  name: { type: "string", notNull: true },
+  status: { type: "string", notNull: true, default: "created" },
+  artifact: { type: "string", allowNull: true },
+};
+
+exports.up = async (db) => {
+  await db.createTable(TYPE_TABLE_NAME, TYPE_TABLE_SCHEMA);
+  await db.createTable(TABLE_NAME, TABLE_SCHEMA)
+  await db.addIndex(TABLE_NAME, TABLE_INDEX_NAME, ["buildId", "buildTaskTypeId"], true);
+};
+
+exports.down = async (db) => {
+  await db.dropTable(TABLE_NAME);
+  return db.dropTable(TYPE_TABLE_NAME);
+};
+
