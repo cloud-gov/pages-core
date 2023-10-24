58332b2a6f47ecc81435c9ffafd24a5445c72f9f
Drew Bollinger <drew.bollinger@gsa.gov>
2023-10-24 19:25:30 -0400

    chore: update slack release notes format and bump pattern matcher

--- ci/tasks/get-changelog-version.sh
+++ ci/tasks/get-changelog-version.sh
@@ -6,10 +6,11 @@ version=`cat .git/ref`
 
 awk -v version=$version '/^## / { if (p) { exit }; if ($2 == version) { p=1; next } } p && NF' CHANGELOG.md > releaselog.md
 
+cat releaselog.md | sed -E 's/### (.*)/\n*\1*/g' | sed -E 's/^-/•/g' > templog.md
+
 echo ":tada: New cloud.gov Pages Release :tada:
 
 https://github.com/cloud-gov/pages-core/releases/tag/$version
-
-`cat releaselog.md`
+`cat templog.md`
 " > slackrelease.md
 
