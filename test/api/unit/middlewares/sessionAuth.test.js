40d6d30425932eb1617c1547e66b2b8f89f04129
Drew Bollinger <drew.bollinger@gsa.gov>
2024-03-28 15:15:57 -0700

    fix: update reauthenication logic

--- test/api/unit/middlewares/sessionAuth.test.js
+++ test/api/unit/middlewares/sessionAuth.test.js
@@ -1,16 +1,14 @@
 const { expect } = require('chai');
 const sinon = require('sinon');
 const moment = require('moment');
-const proxyquire = require('proxyquire').noCallThru();
 
 const config = require('../../../../config');
+const sessionAuth = require('../../../../api/middlewares/session-auth');
 
 describe('sessionAuth', () => {
   let mockReq;
   let mockRes;
   let mockNext;
-  let sessionAuth;
-  let validateUserSpy;
 
   beforeEach(() => {
     mockReq = {
@@ -20,9 +18,6 @@ describe('sessionAuth', () => {
         authenticatedAt: new Date(),
         destroy: sinon.spy(),
       },
-      user: {
-        githubAccessToken: 'good-token',
-      },
       logout: sinon.spy(),
     };
 
@@ -31,19 +26,6 @@ describe('sessionAuth', () => {
     mockRes = {
       forbidden: sinon.spy(),
     };
-
-    validateUserSpy = sinon.spy((token) => {
-      if (token === 'good-token') {
-        return Promise.resolve();
-      }
-      return Promise.reject(new Error('user not valid'));
-    });
-
-    sessionAuth = proxyquire('../../../../api/middlewares/session-auth', {
-      '../services/GitHub': {
-        validateUser: validateUserSpy,
-      },
-    });
   });
 
   it('calls res.forbidden when not authenticated', (done) => {
@@ -62,45 +44,18 @@ describe('sessionAuth', () => {
     done();
   });
 
-  it('revalidates the user if authenticatedAt is missing', (done) => {
+  it('logs out the user if authenticatedAt is missing', () => {
     delete mockReq.session.authenticatedAt;
-    sessionAuth(mockReq, mockRes, mockNext)
-      .then(() => {
-        expect(moment(mockReq.session.authenticatedAt).isSame(moment(), 'second')).to.equal(true);
-        expect(validateUserSpy.calledOnce).to.equal(true);
-        expect(mockNext.calledOnce).to.equal(true);
-        expect(mockReq.session.authRedirectPath).to.equal(undefined);
-        done();
-      })
-      .catch(done);
+    sessionAuth(mockReq, mockRes, mockNext);
+    expect(moment(mockReq.session.authenticatedAt).isSame(moment(), 'second')).to.equal(true);
+    expect(mockReq.logout.calledOnce).to.equal(true);
+    expect(mockReq.session.authRedirectPath).to.equal(undefined);
   });
 
-  it('revalidates the user if authenticatedAt is too old', (done) => {
+  it('logs out the user if authenticatedAt is too old', () => {
     mockReq.session.authenticatedAt = moment().subtract(config.policies.authRevalidationMinutes + 5, 'minutes').toDate();
-    sessionAuth(mockReq, mockRes, mockNext)
-      .then(() => {
-        expect(moment(mockReq.session.authenticatedAt).isSame(moment(), 'second')).to.equal(true);
-        expect(validateUserSpy.calledOnce).to.equal(true);
-        expect(mockNext.calledOnce).to.equal(true);
-        expect(mockReq.session.authRedirectPath).to.equal(undefined);
-        done();
-      })
-      .catch(done);
-  });
-
-  it('ends the session and calls res.forbidden if the user is no longer valid', (done) => {
-    mockReq.session.authenticatedAt = moment().subtract(config.policies.authRevalidationMinutes + 5, 'minutes').toDate();
-    mockReq.user.githubAccessToken = 'bad-user-token';
-
-    sessionAuth(mockReq, mockRes, mockNext)
-      .then(() => {
-        expect(validateUserSpy.calledOnce).to.equal(true);
-        expect(mockReq.logout.calledOnce).to.equal(true);
-        expect(mockReq.session.destroy.calledOnce).to.equal(true);
-        expect(mockNext.notCalled).to.equal(true);
-        expect(mockRes.forbidden.calledOnce).to.equal(true);
-        done();
-      })
-      .catch(done);
+    sessionAuth(mockReq, mockRes, mockNext);
+    expect(mockReq.logout.calledOnce).to.equal(true);
+    expect(mockReq.session.authRedirectPath).to.equal(undefined);
   });
 });
