bf55e5e1af0a917edece0fe4506a468193325488
Drew Bollinger <drew.bollinger@gsa.gov>
2024-10-23 16:03:04 -0400

    chore: implement nested folder route mapping

---
+++ frontend/pages/sites/new/AddRepoSiteForm.jsx
@@ -0,0 +1,115 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Field, reduxForm } from 'redux-form';
+
+import GitHubRepoUrlField from '@shared/Fields/GitHubRepoUrlField';
+import AlertBanner from '@shared/alertBanner';
+import { validAddRepoSiteForm } from '@util/validators';
+import UserOrgSelect from '@shared/UserOrgSelect';
+import SelectSiteEngine from '@shared/SelectSiteEngine';
+
+import globals from '@globals';
+import { ORGANIZATIONS } from '@propTypes';
+
+const showNewSiteAlert = () => {
+  const message = (
+    <span>
+      Looks like this site is completely new to
+      {` ${globals.APP_NAME}!`}
+      <br />
+      Please fill out these additional fields to complete the process.
+    </span>
+  );
+
+  return <AlertBanner status="info" header="New Site" message={message} />;
+};
+
+export const AddRepoSiteForm = ({
+  // even though initialValues is not directly used, it is used
+  // by reduxForm, and we want PropType validation on it, so we'll
+  // keep it here but disable the eslint rule below
+  initialValues, // eslint-disable-line no-unused-vars
+  pristine,
+  handleSubmit,
+  organizations,
+  showAddNewSiteFields,
+}) => (
+  <form onSubmit={handleSubmit}>
+    <div className="form-group margin-y-3">
+      <GitHubRepoUrlField
+        label="Already have a GitHub repo for your site? Paste the URL here."
+        name="repoUrl"
+        id="repoUrl"
+        placeholder="https://github.com/owner/repository"
+        readOnly={showAddNewSiteFields}
+      />
+    </div>
+    <div className="form-group margin-y-3">
+      <Field
+        name="repoOrganizationId"
+        type="select"
+        component={p => (
+          <UserOrgSelect
+            id="repoOrganizationId"
+            name="repoOrganizationId"
+            value={p.input.value}
+            onChange={p.input.onChange}
+            orgData={organizations.data}
+            mustChooseOption
+            {...p.meta}
+          />
+        )}
+      />
+    </div>
+    {
+      showAddNewSiteFields && (
+      <div className="add-repo-site-additional-fields">
+        {showNewSiteAlert()}
+        <div className="form-group margin-y-2">
+          {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+          <label className="usa-label text-bold" htmlFor="engine">Site engine</label>
+          <Field
+            name="engine"
+            component={p => (
+              <SelectSiteEngine
+                name="engine"
+                id="engine"
+                value={p.input.value}
+                onChange={p.input.onChange}
+              />
+            )}
+          />
+        </div>
+      </div>
+      )
+}
+    <button
+      type="submit"
+      className="usa-button usa-button--primary inline-block"
+      disabled={pristine}
+    >
+      Add repository-based site
+    </button>
+  </form>
+);
+
+AddRepoSiteForm.propTypes = {
+  organizations: ORGANIZATIONS.isRequired,
+  showAddNewSiteFields: PropTypes.bool,
+  initialValues: PropTypes.shape({
+    engine: PropTypes.string.isRequired,
+  }).isRequired,
+  // the following props are from reduxForm:
+  handleSubmit: PropTypes.func.isRequired,
+  pristine: PropTypes.bool.isRequired,
+};
+
+AddRepoSiteForm.defaultProps = {
+  showAddNewSiteFields: false,
+};
+
+// create a higher-order component with reduxForm and export that
+export default reduxForm({
+  form: 'addRepoSite',
+  validate: validAddRepoSiteForm,
+})(AddRepoSiteForm);
--- frontend/routes.js
+++ frontend/routes.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import { Route, redirect } from 'react-router-dom';
 
+// TODO: we're currently mimicking a file based router using a component based router
 import App from '@pages/app';
-import * as Organizations from '@pages/organizations';
-import * as Sites from '@pages/sites';
-import * as CustomDomains from '@pages/customDomains';
-import * as Settings from '@pages/settings';
+import OrganizationList from '@pages/organizations';
+import OrganizationEdit from '@pages/organizations/edit';
+import Sites from '@pages/sites';
+import AddSite from '@pages/sites/new';
+import SiteContainer from '@pages/sites/$siteId';
+import SiteSettings from '@pages/sites/$siteId/settings';
+import SiteBuildList from '@pages/sites/$siteId/builds';
+import BuildLogs from '@pages/sites/$siteId/builds/$buildId/logs';
+import PublishedBranchesTable from '@pages/sites/$siteId/published';
+import PublishedFilesTable from '@pages/sites/$siteId/published/$name';
+import DomainList from '@pages/sites/$siteId/custom-domains';
+import NewCustomDomain from '@pages/sites/$siteId/custom-domains/new';
+import EditCustomDomain from '@pages/sites/$siteId/custom-domains/$domainId/edit';
+import Reports from '@pages/sites/$siteId/reports';
+import Settings from '@pages/settings';
 import NotFound from '@pages/NotFound';
 import Error from '@pages/Error';
 
@@ -27,26 +39,26 @@ const fetchInitialData = () => {
 
 export default (
   <Route path="/" element={<App onEnter={fetchInitialData} />} errorElement={ErrorElement}>
-    <Route path="organizations" element={<Organizations.List />} />
-    <Route path="organizations/:id" element={<Organizations.Edit />} />
-    <Route path="sites" element={<Sites.List />} />
-    <Route path="sites/new" element={<Sites.New />} />
-    <Route path="sites/:id" element={<Sites.Wrapper />}>
+    <Route path="organizations" element={<OrganizationList />} />
+    <Route path="organizations/:id" element={<OrganizationEdit />} />
+    <Route path="sites" element={<Sites />} />
+    <Route path="sites/new" element={<AddSite />} />
+    <Route path="sites/:id" element={<SiteContainer />}>
       <Route path="" loader={() => redirect('builds')} />
-      <Route path="settings" element={<Sites.Settings />} />
-      <Route path="published" element={<Sites.PublishedBranchesTable />} />
-      <Route path="published/:name" element={<Sites.PublishedFilesTable />} />
-      <Route path="builds" element={<Sites.Builds />} />
-      <Route path="custom-domains" element={<CustomDomains.List />} />
-      <Route path="custom-domains/new" element={<CustomDomains.New />} />
-      <Route path="custom-domains/:domainId/edit" element={<CustomDomains.Edit />} />
-      <Route path="builds/:buildId/logs" element={<Sites.BuildLogs />} />
+      <Route path="settings" element={<SiteSettings />} />
+      <Route path="published" element={<PublishedBranchesTable />} />
+      <Route path="published/:name" element={<PublishedFilesTable />} />
+      <Route path="builds" element={<SiteBuildList />} />
+      <Route path="custom-domains" element={<DomainList />} />
+      <Route path="custom-domains/new" element={<NewCustomDomain />} />
+      <Route path="custom-domains/:domainId/edit" element={<EditCustomDomain />} />
+      <Route path="builds/:buildId/logs" element={<BuildLogs />} />
       <Route path="scans" loader={() => redirect('../reports')} />
       {(process.env.FEATURE_BUILD_TASKS === 'active') && (
-        <Route path="reports" element={<Sites.Reports />} />
+        <Route path="reports" element={<Reports />} />
       )}
     </Route>
-    <Route path="settings" element={<Settings.UserSettings />} />
+    <Route path="settings" element={<Settings />} />
     <Route path="*" element={<NotFound />} />
   </Route>
 );
