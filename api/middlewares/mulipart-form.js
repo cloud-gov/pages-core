0120ba1f9734bc0af5b23c589a5c7161e30e58e3
afishman-2025 <anna.fishman@gsa.gov>
2026-05-12 11:00:00 -0500

    test

--- api/middlewares/mulipart-form.js
+++ api/middlewares/mulipart-form.js
@@ -1,9 +1,17 @@
 const multer = require('multer');
+const { logger } = require('../../winston');
 
 const multipartForm = multer({
-  storage: multer.diskStorage({ destination: '/tmp' }),
+  storage: multer.diskStorage({
+    destination: (req, file, cb) => {
+      logger.info('multer writing to /tmp');
+      cb(null, '/tmp');
+    },
+  }),
   // eslint-disable-next-line sonarjs/content-length
   limits: { fileSize: 300000000, files: 1 },
 });
+
+logger.info('multipart form');
 
 module.exports = multipartForm;
