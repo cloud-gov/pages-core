d7ccd78ea45824feb351aafd4d11bcfab1ed54bd
afishman-2025 <anna.fishman@gsa.gov>
2026-05-13 16:42:12 -0500

    test

--- ci/clamav-rest-image/deploy.sh
+++ ci/clamav-rest-image/deploy.sh
@@ -22,12 +22,26 @@ cf set-env ${CLAMAV_REST_HOSTNAME} MAX_FILE_SIZE 250M
 cf set-env ${CLAMAV_REST_HOSTNAME} MAX_SCAN_SIZE 250M
 cf restage ${CLAMAV_REST_HOSTNAME}
 
-cf ssh pages-clamav-rest-dev -c "
-  sed -i 's/MaxFileSize.*/MaxFileSize 250M/' /etc/clamav/clamd.conf
-  sed -i 's/MaxScanSize.*/MaxScanSize 250M/' /etc/clamav/clamd.conf
-  sed -i 's/StreamMaxLength.*/StreamMaxLength 250M/' /etc/clamav/clamd.conf
-"
+#cf ssh pages-clamav-rest-dev -c "
+#  sed -i 's/MaxFileSize.*/MaxFileSize 250M/' /etc/clamav/clamd.conf
+#  sed -i 's/MaxScanSize.*/MaxScanSize 250M/' /etc/clamav/clamd.conf
+#  sed -i 's/StreamMaxLength.*/StreamMaxLength 250M/' /etc/clamav/clamd.conf
+#"
 
-cf ssh pages-clamav-rest-dev -c "grep -E 'MaxFileSize|MaxScanSize|StreamMaxLength' /etc/clamav/clamd.conf"
+for i in 0 1; do
+  cf ssh pages-clamav-rest-dev -i $i -c "
+    sed -i 's/MaxFileSize.*/MaxFileSize 250M/' /etc/clamav/clamd.conf
+    sed -i 's/MaxScanSize.*/MaxScanSize 250M/' /etc/clamav/clamd.conf
+    sed -i 's/StreamMaxLength.*/StreamMaxLength 250M/' /etc/clamav/clamd.conf
+    pkill clamd
+    sleep 2
+    clamd &
+  "
+done
 
-cf ssh pages-clamav-rest-dev -c "cat /etc/clamav/clamd.conf"
+for i in 0 1; do
+  echo "=== Instance $i ==="
+  cf ssh pages-clamav-rest-dev -i $i -c "grep -E 'MaxFileSize|MaxScanSize|StreamMaxLength' /etc/clamav/clamd.conf"
+  cf ssh pages-clamav-rest-dev -i $i -c "cat /etc/clamav/clamd.conf"
+done
+
